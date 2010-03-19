7852804b9afd8c3d1ad692643b47bdd058d3a6e7
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2010-03-19 19:48:20 +0000

    wip

--- objeck-lang/wip/interactive/interactive.cpp
+++ objeck-lang/wip/interactive/interactive.cpp
@@ -1,31 +1,108 @@
-#include <iostream>
-#include <string>
-#include <vector>
-
-using namespace std;
+#include "interactive.h"
 
 int main() {
-	cout << "Welcome to Objeck! v0.9.7" << endl;
+  cout << "Welcome to Objeck! v0.9.7" << endl;
+  
+  
+  SourceMethod* method = new SourceMethod("function : Main(args : String[]), Nil {");
 
-	vector<string> source;
 	string line;
-	do {	
-		cout << "<#> ";
+  bool quit = false;
+	do {
+    cout << "=> ";
 		getline(cin, line);
+    line = trim(line);
 
+    // clear program
 		if(line == "clear" || line == "c") {
-			source.clear();
-			cout << "program cleared!" << endl;
+			method->Clear();
+			cout << "method cleared!" << endl;
 		}
+    // list program
 		else if(line == "list" || line == "l") {
-			for(int i = 0; i < source.size(); i++ ) {
-				cout << i << ": " << source[i] << endl;
-			}
+      method->List();
 		}
-		else {
-			source.push_back(line);
-		}	
+    // insert line
+		else if(line == "insert" || line == "i") {
+			cout << "  line #? ";
+      getline(cin, line);
+      line = trim(line);
+      int unsigned index = atoi(line.c_str());
+
+      cout << "  insert? '";
+      getline(cin, line);
+      line = trim(line);
+      
+      if(!method->InsertLine(line, index - 1)) {
+        cout << "!invalid line number" << endl;
+      }
+		}
+    // replace line
+		else if(line == "replace" || line == "r") {
+			cout << "  line #? ";
+      getline(cin, line);
+      line = trim(line);
+      int unsigned index = atoi(line.c_str());
+
+      cout << "  replace? '";
+      getline(cin, line);
+      line = trim(line);
+      
+      if(!method->DeleteLine(index - 1)) {
+        cout << "!invalid line number" << endl;
+      }
+      else {
+        method->InsertLine(line, index - 1);
+      }
+		}
+    // replace line
+		else if(line == "delete" || line == "d") {
+			cout << "  line #? ";
+      getline(cin, line);
+      line = trim(line);
+      int unsigned index = atoi(line.c_str());
+
+      if(!method->DeleteLine(index - 1)) {
+        cout << "!invalid line number" << endl;
+      }
+    }
+    // add line to method
+    else if(line.size() > 0 && line[0] == '\'') {
+      method->AddLine(line.substr(1));
+    }
+    // load library
+		else if(line == "load lib" || line == "ll") {
+      cout << "TODO: load lib" << endl;
+    }
+    // execute a program
+		else if(line == "execute" || line == "x") {
+      cout << "TODO: execute" << endl;
+    }
+    // quit session
+		else if(line == "quit" || line == "q") {
+			quit = true;
+		}
+    // change current function/method
+		else if(line == "change method" || line == "cm") {
+      cout << "  method/function? ";
+      getline(cin, line);
+      line = trim(line);
+
+      cout << "now editing: " << line << "..." << endl;
+		}
+    // change current function/method
+		else if(line == "new method" || line == "nm") {
+      cout << "  declaration? ";
+      getline(cin, line);
+      line = trim(line);
+
+      cout << "now editing: " << line << "..." << endl;
+		}
+    // invalid command
+    else {
+      cout << "!invalid command" << endl;
+    }
 	}
-	while(line != "quit" && line != "q"); 
-	cout << "Goodbye!" << endl;
+	while(!quit);
+	cout << "Goodbye..." << endl;
 }
