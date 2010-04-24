79c2bf64312fddf304f5833d7b030d3d8ee4972c
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2010-04-24 16:05:24 +0000

    adding debugger support

---
+++ src/vm/debugger/debugger.h
@@ -0,0 +1,64 @@
+/***************************************************************************
+ * Language scanner.
+ *
+ * Copyright (c) 2008-2010 Randy Hollines
+ * All rights reserved.
+ *
+ * Reistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in
+ * the documentation and/or other materials provided with the distribution.
+ * - Neither the name of the StackVM Team nor the names of its
+ * contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ***************************************************************************/
+
+#ifndef __DEBUGGER_H__
+#define __DEBUGGER_H__
+
+#include "../common.h"
+
+using namespace std;
+
+/********************************
+ * Interactive command line
+ * debugger
+ ********************************/
+class Debugger {
+  static Debugger* instance;
+  int break_num;
+  bool next_instr;
+  
+  Debugger() {
+    break_num = -2;
+    next_instr = false;
+  }
+  
+ public:
+  static void Initialize(StackProgram* p);
+  static Debugger* Instance();
+  
+  ~Debugger() {
+  }
+  
+  void ProcessInstruction(StackInstr* instr, long ip, StackFrame** call_stack,
+			  long call_stack_pos, StackFrame* frame);
+};
+
+#endif
