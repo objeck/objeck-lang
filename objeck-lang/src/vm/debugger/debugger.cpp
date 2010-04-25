79ac051215e413f02896703289745ca1ba4aaf33
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2010-04-25 16:24:51 +0000

    adding debugger support

--- objeck-lang/src/vm/debugger/debugger.cpp
+++ objeck-lang/src/vm/debugger/debugger.cpp
@@ -1,7 +1,7 @@
 /**************************************************************************
  * Runtime debugger
  *
- * Copyright (c) 2008-2010 Randy Hollines
+ * Copyright (c) 2010 Randy Hollines
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
