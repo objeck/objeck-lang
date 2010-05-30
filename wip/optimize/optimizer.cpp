aa6c67c94f2481c982e74f4f6dc143dec1df3291
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2010-05-30 15:53:26 +0000

    working on jit optimization prototype

--- wip/optimize/optimizer.cpp
+++ wip/optimize/optimizer.cpp
@@ -2,4 +2,10 @@
 
 void Optimizer::Optimize() {
   root = new CodeBlock();
+  root->AddSegment(new CodeSegment(new CodeElement(INT_VAR, 0), new CodeElement(INT_LIT, 13)));
+  root->AddSegment(new CodeSegment(new CodeElement(INT_VAR, 1), 
+				   new CodeElement(INT_VAR, 0),
+				   new CodeElement(ADD_OPER),
+				   new CodeElement(INT_VAR, 0)));
+  root->AddSegment(new CodeSegment(new CodeElement(INT_VAR, 0), new CodeElement(INT_VAR, 1)));
 }
