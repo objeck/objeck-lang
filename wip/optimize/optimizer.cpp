cc903b0def4e97b05a334becac14b0e8cd45ef83
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2010-05-31 13:25:22 +0000

    working on jit optimization prototype

--- wip/optimize/optimizer.cpp
+++ wip/optimize/optimizer.cpp
@@ -11,18 +11,18 @@ CodeElementFactory* CodeElementFactory::Instance() {
 
 void Optimizer::Optimize() {
   root = new CodeBlock();
-  root->AddSegment(new CodeSegment(CodeElementFactory::Instance()->MakeCodeElement(INT_VAR, 0), 
-				   CodeElementFactory::Instance()->MakeCodeElement(INT_LIT, 13)));
-  root->AddSegment(new CodeSegment(CodeElementFactory::Instance()->MakeCodeElement(INT_VAR, 1), 
-				   CodeElementFactory::Instance()->MakeCodeElement(INT_VAR, 0),
-				   CodeElementFactory::Instance()->MakeCodeElement(ADD_OPER),
-				   CodeElementFactory::Instance()->MakeCodeElement(INT_VAR, 0)));
-  root->AddSegment(new CodeSegment(CodeElementFactory::Instance()->MakeCodeElement(INT_VAR, 0), 
-				   CodeElementFactory::Instance()->MakeCodeElement(INT_VAR, 1)));
-  root->AddSegment(new CodeSegment(CodeElementFactory::Instance()->MakeCodeElement(INT_VAR, 1), 
-				   CodeElementFactory::Instance()->MakeCodeElement(INT_LIT, 25)));
-  root->AddSegment(new CodeSegment(CodeElementFactory::Instance()->MakeCodeElement(INT_VAR, 1), 
-				   CodeElementFactory::Instance()->MakeCodeElement(INT_LIT, 74),
-				   CodeElementFactory::Instance()->MakeCodeElement(ADD_OPER),
-				   CodeElementFactory::Instance()->MakeCodeElement(INT_VAR, 0)));
+  root->AddSegment(new CodeSegment(MakeCodeElement(INT_VAR, 0), 
+				   MakeCodeElement(INT_LIT, 13)));
+  root->AddSegment(new CodeSegment(MakeCodeElement(INT_VAR, 1), 
+				   MakeCodeElement(INT_VAR, 0), 
+				   MakeCodeElement(ADD_OPER),
+				   MakeCodeElement(INT_VAR, 0)));
+  root->AddSegment(new CodeSegment(MakeCodeElement(INT_VAR, 0), 
+				   MakeCodeElement(INT_VAR, 1)));
+  root->AddSegment(new CodeSegment(MakeCodeElement(INT_VAR, 1), 
+				   MakeCodeElement(INT_LIT, 25)));
+  root->AddSegment(new CodeSegment(MakeCodeElement(INT_VAR, 1), 
+				   MakeCodeElement(INT_LIT, 74),
+				   MakeCodeElement(ADD_OPER),
+				   MakeCodeElement(INT_VAR, 0)));
 }
