f9a4aa0f7f97039f1fbbe035dffd3b29b245d104
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2010-08-30 20:39:28 +0000

    added support for "random"

--- objeck-lang/src/compiler/scanner.cpp
+++ objeck-lang/src/compiler/scanner.cpp
@@ -120,6 +120,7 @@ void Scanner::LoadKeywords()
   ident_map["LOG_FLOAT"] = LOG_FLOAT;
   ident_map["POW_FLOAT"] = POW_FLOAT;
   ident_map["SQRT_FLOAT"] = SQRT_FLOAT;
+  ident_map["RAND_FLOAT"] = RAND_FLOAT;
   ident_map["LOAD_CLS_INST_ID"] = LOAD_CLS_INST_ID;
   ident_map["LOAD_INST_UID"] = LOAD_INST_UID;
   ident_map["LOAD_ARY_SIZE"] = LOAD_ARY_SIZE;
@@ -232,6 +233,7 @@ void Scanner::CheckIdentifier(int index)
   case LOG_FLOAT:
   case POW_FLOAT:
   case SQRT_FLOAT:
+  case RAND_FLOAT:
   case LOAD_CLS_INST_ID:
   case LOAD_INST_UID:
   case STD_OUT_BOOL:
