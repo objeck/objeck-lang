47849e249999fdeb049a09d84a1b1b9f91c6174d
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2010-11-22 13:38:34 +0000

    fixed bug related to the collection of static memory

--- src/vm/os/posix/memory.h
+++ src/vm/os/posix/memory.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 2007, 2008, Randy Hollines
  * All rights reserved.
- *CollectMemory(o
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
