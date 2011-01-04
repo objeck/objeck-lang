5e50f74d35b2deb03ccc08b91f6a6b83d04bd611
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2011-01-04 18:20:50 +0000

    adding support c dlls

--- src/vm/lib/linux64/test_dll.cpp
+++ src/vm/lib/linux64/test_dll.cpp
@@ -9,6 +9,8 @@ extern "C" {
 		int size = DLLTools_GetArraySize(data);
 		cout << size << endl;
 		cout << DLLTools_GetIntValue(data, 1) << endl;
+		cout << DLLTools_GetFloatValue(data, 2) << endl;
+		DLLTools_SetFloatValue(data, 2, 13.5);
 		DLLTools_SetIntValue(data, 0, 20);
 	}
 }
