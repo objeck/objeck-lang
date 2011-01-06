721b706e2f6f7af856abcab8fb78ac740a0d6a38
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2011-01-06 20:19:56 +0000

    adding gtk window support for linux

--- src/vm/lib/linux64/build64.sh
+++ src/vm/lib/linux64/build64.sh
@@ -1,4 +1,5 @@
 #/bin/sh
 rm -rf *.o
-g++ -fPIC -g -c -Wno-deprecated -Wall -D_X64 $1.cpp
-g++ -shared -fPIC -Wl,-soname,$1.so.1 -D_X64 -o $1.so.1.0.1 $1.o -lc
+# g++ -shared -fPIC `pkg-config --cflags --libs gtk+-2.0` -c $1.o $1.cpp 
+# g++ -fPIC -g -c -Wno-deprecated -Wall -D_X64 $1.cpp
+g++ -shared -fPIC `pkg-config --cflags --libs gtk+-2.0` -Wl,-soname,$1.so.1 -D_X64 -o $1.so.1.0.1 $1.cpp -lc
