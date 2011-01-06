00df3ba7184297f0a9482da3c8ce472a54d2f6a2
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2011-01-06 07:06:07 +0000

    working on support for C shared libraries

--- src/vm/lib/linux64/build.sh
+++ src/vm/lib/linux64/build.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 rm -rf *.o
-g++ -fPIC -g -c -Wno-deprecated -Wall $1.cpp
-g++ -shared -fPIC -Wl,-soname,$1.so.1 -o $1.so.1.0.1 $1.o -lc
+g++ -fPIC -g -c -Wno-deprecated -Wall -D_X64 $1.cpp
+g++ -shared -fPIC -Wl,-soname,$1.so.1 -D_X64 -o $1.so.1.0.1 $1.o -lc
