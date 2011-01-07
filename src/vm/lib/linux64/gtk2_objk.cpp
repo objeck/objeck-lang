d100a01bc4d91cb96d5ac2999bf4beecd2e0fe24
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2011-01-07 08:09:29 +0000

    adding gtk window support for linux

--- src/vm/lib/linux64/gtk2_objk.cpp
+++ src/vm/lib/linux64/gtk2_objk.cpp
@@ -37,12 +37,11 @@ extern "C" {
 
 	void signal_connect(long* data_array, long* op_stack, long* stack_pos, DLLTools_MethodCall_Ptr callback) {
 
-		GtkWidget* widget = (GtkWidget*)DLLTools_GetIntValue(data_array, 0);
+		long* self = (long*)DLLTools_GetIntValue(data_array, 0);
 		int signal = DLLTools_GetIntValue(data_array, 1);
-		long* self = (long*)DLLTools_GetIntValue(data_array, 2);
-		int cls_id = DLLTools_GetIntValue(data_array, 3);
-		int mthd_id = DLLTools_GetIntValue(data_array, 4);
-//    cout << "@@@ " << signal << ", " << cls_id << ", " << mthd_id << " @@@" << endl;
+		int cls_id = DLLTools_GetIntValue(data_array, 2);
+		int mthd_id = DLLTools_GetIntValue(data_array, 3);
+
 		callback_data* cbd = new callback_data;
 		cbd->cls_id = cls_id;
 		cbd->mthd_id = mthd_id;
@@ -51,11 +50,11 @@ extern "C" {
 		cbd->self = self;
 		cbd->callback = callback;
 
-		cout << "@@@ " << cbd << " @@@" << endl;
+	// cout << "@@@ " << widget << ", " << (long*)self[0] << ", " << self << " @@@" << endl;
 
 		switch(signal) {
 		case -100:
-			g_signal_connect(widget, "destroy", G_CALLBACK(destroy_callback_handler), (gpointer)cbd);
+			g_signal_connect((GtkWidget*)self, "destroy", G_CALLBACK(destroy_callback_handler), cbd);
 			break;
 		}
    }
@@ -66,8 +65,12 @@ extern "C" {
 
    void destroy_callback_handler(GtkWidget* widget, gpointer data) {
 		callback_data* cbd = (callback_data*)data;
+
 		DLLTools_MethodCall_Ptr callback = cbd->callback;
 		DLLTools_PushInt(cbd->op_stack, cbd->stack_pos, (long)cbd->self);
 		(*callback)(cbd->op_stack, cbd->stack_pos, NULL, cbd->cls_id, cbd->mthd_id);
+
+		delete cbd;
+		cbd = NULL;
 	}
 }
