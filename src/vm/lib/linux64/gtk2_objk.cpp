f9ebc3936081cb0718f4ae92366dce0759fb006c
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2011-01-07 07:51:22 +0000

    adding gtk window support for linux

--- src/vm/lib/linux64/gtk2_objk.cpp
+++ src/vm/lib/linux64/gtk2_objk.cpp
@@ -11,6 +11,7 @@ extern "C" {
 		int mthd_id;
 		long* op_stack;
 		long* stack_pos;
+		long* self;
 		DLLTools_MethodCall_Ptr callback;
 	} callback_data;
 
@@ -37,16 +38,17 @@ extern "C" {
 	void signal_connect(long* data_array, long* op_stack, long* stack_pos, DLLTools_MethodCall_Ptr callback) {
 
 		GtkWidget* widget = (GtkWidget*)DLLTools_GetIntValue(data_array, 0);
-//		char* name = DLLTools_GetStringValue(data_array, 1);
 		int signal = DLLTools_GetIntValue(data_array, 1);
-		int cls_id = DLLTools_GetIntValue(data_array, 2);
-		int mthd_id = DLLTools_GetIntValue(data_array, 3);
+		long* self = (long*)DLLTools_GetIntValue(data_array, 2);
+		int cls_id = DLLTools_GetIntValue(data_array, 3);
+		int mthd_id = DLLTools_GetIntValue(data_array, 4);
 //    cout << "@@@ " << signal << ", " << cls_id << ", " << mthd_id << " @@@" << endl;
 		callback_data* cbd = new callback_data;
 		cbd->cls_id = cls_id;
 		cbd->mthd_id = mthd_id;
 		cbd->op_stack = op_stack;
 		cbd->stack_pos = stack_pos;
+		cbd->self = self;
 		cbd->callback = callback;
 
 		cout << "@@@ " << cbd << " @@@" << endl;
@@ -65,9 +67,7 @@ extern "C" {
    void destroy_callback_handler(GtkWidget* widget, gpointer data) {
 		callback_data* cbd = (callback_data*)data;
 		DLLTools_MethodCall_Ptr callback = cbd->callback;
-		DLLTools_PushInt(cbd->op_stack, cbd->stack_pos, 0);
-		DLLTools_PushInt(cbd->op_stack, cbd->stack_pos, 0);
-		DLLTools_PushInt(cbd->op_stack, cbd->stack_pos, 0);
+		DLLTools_PushInt(cbd->op_stack, cbd->stack_pos, (long)cbd->self);
 		(*callback)(cbd->op_stack, cbd->stack_pos, NULL, cbd->cls_id, cbd->mthd_id);
 	}
 }
