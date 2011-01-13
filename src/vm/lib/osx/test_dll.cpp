30a6d3a8b5bb83b00da43ebb491b3090726608c2
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2011-01-13 09:31:32 +0000

    working on support for C shared libraries

--- src/vm/lib/osx/test_dll.cpp
+++ src/vm/lib/osx/test_dll.cpp
@@ -5,20 +5,24 @@
 using namespace std;
 
 extern "C" {
-	void foo(long* data_array, long* op_stack, long *stack_pos, 
-            DLLTools_MethodCall_Ptr callback) {
-		int size = DLLTools_GetArraySize(data_array);
-		cout << size << endl;
-		cout << DLLTools_GetIntValue(data_array, 1) << endl;
-		cout << DLLTools_GetFloatValue(data_array, 2) << endl;
-		DLLTools_SetFloatValue(data_array, 2, 13.5);
-		DLLTools_SetIntValue(data_array, 0, 20);
-
-		cout << "---0---" << endl;
-
-		DLLTools_PushFloat(op_stack, stack_pos, 1112.11);
-		(*callback)(op_stack, stack_pos, NULL, 4, 15);
-
-		cout << "---1---" << endl;
-	}
+  void load_lib() {}
+  void unload_lib() {}
+  
+  void foo(long* data_array, long* op_stack, long *stack_pos, 
+	   DLLTools_MethodCall_Ptr callback) {
+    int size = DLLTools_GetArraySize(data_array);
+    cout << size << endl;
+    cout << DLLTools_GetIntValue(data_array, 1) << endl;
+    cout << DLLTools_GetFloatValue(data_array, 2) << endl;
+    DLLTools_SetFloatValue(data_array, 2, 13.5);
+    DLLTools_SetIntValue(data_array, 0, 20);
+    
+    cout << "---0---" << endl;
+    
+    DLLTools_PushInt(op_stack, stack_pos, 13);
+    DLLTools_PushFloat(op_stack, stack_pos, 1112.11);
+    DLLTools_CallMethod(callback, op_stack, stack_pos, NULL, 4, 15);
+    
+    cout << "---1---" << endl;
+  }
 }
