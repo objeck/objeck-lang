88f6bdf7496ad8857c878322af3b3907040221df
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2011-01-21 18:17:59 +0000

    working on support for C shared libraries

--- src/vm/lib/osx/qt/test.cpp
+++ src/vm/lib/osx/qt/test.cpp
@@ -1,12 +1,23 @@
 #include <QtGui>
+#include <iostream>
+#include <string>
+#include "../../../../shared/dll_tools.h"
 
- int main(int argc, char *argv[])
- {
-     QApplication app(argc, argv);
-     QWidget window;
-     window.resize(320, 240);
-     window.show();
-     window.setWindowTitle(
-         QApplication::translate("toplevel", "Top-level widget"));
-     return app.exec();
- }
+using namespace std;
+
+extern "C" {
+  void load_lib() {}
+  void unload_lib() {}
+
+  void foo(long* data_array, long* op_stack, long *stack_pos, DLLTools_MethodCall_Ptr callback) {
+	 int argc = 0;
+	 char* argv[0];
+
+    QApplication app(argc, argv);
+    QWidget window;
+    window.resize(320, 240);
+    window.show();
+    window.setWindowTitle(QApplication::translate("toplevel", "Top-level widget"));
+    app.exec();
+  }
+}
