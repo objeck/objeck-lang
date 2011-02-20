b877e1cb53b0564d44331fffe9b1a84fa2af76e7
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2011-02-20 12:36:29 +0000

    working on api support

---
+++ src/vm/callback.h
@@ -0,0 +1,50 @@
+/***************************************************************************
+ * API header file
+ *
+ * Copyright (c) 2008-2011, Randy Hollines
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in
+ * the documentation and/or other materials provided with the distribution.
+ * - Neither the name of the Objeck Team nor the names of its
+ * contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ***************************************************************************/
+
+#ifndef __CALLBACK_H__
+#define __CALLBACK_H__
+
+#include "common.h"
+
+#ifdef _WIN32
+#include "os/windows/memory.h"
+#else
+#include "os/posix/memory.h"
+#endif
+
+// function declaration for native C callbacks
+typedef void(*DLLTools_MethodCall_Ptr) (long* op_stack, long *stack_pos, long *instance, const char* cls_id, const char* mthd_id);
+struct Callbacks {
+	DLLTools_MethodCall_Ptr method_call;
+	MemoryManager* mem_mgr;
+};
+
+#endif
