e07bca19206717e4de69b85d44c51fee3b818c8c
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2011-05-27 14:32:45 +0000

    don't roll up inlined method into main

--- src/compiler/optimization.h
+++ src/compiler/optimization.h
@@ -118,8 +118,8 @@ class ItermediateOptimizer {
     std::string net_prefix("API."); std::string intro_prefix("Introspection.");
     // check general properties
     if(!called->IsVirtual() && called->GetInstructionCount() < 16 &&
-     !(current_method->GetClass()->GetId() == program->GetStartClassId() && 
-       current_method->GetId() == program->GetStartMethodId()) &&  
+       !(current_method->GetClass()->GetId() == program->GetStartClassId() && 
+	 current_method->GetId() == program->GetStartMethodId()) &&  
        current_method->GetSpace() <= 128 &&
        // check bundles
        method_name.compare(0, new_cls_prefix.size(), new_cls_prefix) != 0 &&
@@ -158,7 +158,7 @@ class ItermediateOptimizer {
     return false;
   }
   
-public:
+ public:
   ItermediateOptimizer(IntermediateProgram* p, string optimize) {
     program = p;
     inline_end = -1;
