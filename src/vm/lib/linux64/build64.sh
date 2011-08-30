2cfa3c0a4e5f66fb400ee41342fb62a0f6822467
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2011-08-30 01:16:37 +0000

    odbc support or die "thug life!"

--- src/vm/lib/linux64/build64.sh
+++ src/vm/lib/linux64/build64.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 rm -rf *.o
 # g++ -shared -fPIC `pkg-config --cflags --libs gtk+-2.0` -c $1.o $1.cpp 
-# g++ -fPIC -g -c -Wno-deprecated -Wall -D_X64 $1.cpp
-g++ -g -shared -fPIC `pkg-config --cflags --libs gtk+-2.0` -Wl,-soname,$1.so.1 -D_DEBUG -D_X64 -o $1.so.1.0.1 $1.cpp -lc
+g++ -g -shared -fPIC -Wl,-soname,$1.so.1 -D_DEBUG -D_X64 -o $1.so $1.cpp -lc
+# g++ -g -shared -fPIC `pkg-config --cflags --libs gtk+-2.0` -Wl,-soname,$1.so.1 -D_DEBUG -D_X64 -o $1.so.1.0.1 $1.cpp -lc
