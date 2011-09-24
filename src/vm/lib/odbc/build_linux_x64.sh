c9d939d31a5ac38c880f01e151d98a50a0fe3488
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2011-09-24 16:20:50 +0000

    odbc v0.5.0

--- src/vm/lib/odbc/build_linux_x64.sh
+++ src/vm/lib/odbc/build_linux_x64.sh
@@ -1,5 +1,8 @@
 #/bin/sh
 rm -rf *.o
-# g++ -shared -fPIC `pkg-config --cflags --libs gtk+-2.0` -c $1.o $1.cpp 
-# g++ -fPIC -g -c -Wno-deprecated -Wall -D_X64 $1.cpp
-g++ -g -shared -fPIC `pkg-config --cflags --libs gtk+-2.0` -Wl,-soname,$1.so.1 -D_DEBUG -D_X64 -o $1.so.1.0.1 $1.cpp -lc
+gcc -Wall -fPIC -c *$1.cpp
+gcc -shared -lodbc -Wl,-soname,$1.so.1 -o $1.so *.o
+# g++ -shared -fPIC $1.o $1.cpp 
+# g++ -fPIC -O3 -c -Wno-deprecated -Wall -D_X64 $1.cpp -o $1.so
+# g++ -shared -fPIC -O3 -c -Wno-deprecated -Wall $1.cpp -o $1.so
+# g++ -g -shared -fPIC `pkg-config --cflags --libs gtk+-2.0` -Wl,-soname,$1.so.1 -D_DEBUG -D_X64 -o $1.so.1.0.1 $1.cpp -lc
