7e0cc17ab5dc8845ce560ede6420601a816b0037
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2011-11-21 21:26:31 +0000

    remote object support - wii

--- wip/remote/client.cpp
+++ wip/remote/client.cpp
@@ -34,7 +34,26 @@ public:
   }
   
   bool Connect() {
-    if(sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP) < 0) {
+    sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(sock < 0) {
+      sock = 0;
+      return false;
+    }
+    
+    struct hostent* host_info = gethostbyname(address.c_str()); 
+    long host_addr;
+    memcpy(&host_addr, host_info->h_addr, host_info->h_length);
+    
+    struct sockaddr_in ip_addr; 
+    ip_addr.sin_addr.s_addr = host_addr;;
+    ip_addr.sin_port=htons(port);
+    ip_addr.sin_family = AF_INET;
+    
+    if(connect(sock, (struct sockaddr*)&ip_addr,sizeof(ip_addr))) {
+
+      cout << "...." << endl;
+
+      sock = 0;
       return false;
     }
     
@@ -129,68 +148,24 @@ public:
   }
 };
 
-int main() {
-  string address = "www.du.edu";
-  int port = 80;
-
+int main(int argc, char **argv) {
 #ifdef _WIN32
   WSADATA data;
   int version = MAKEWORD(2, 2);
   WSAStartup(version, &data);
 #endif
   
-  int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);;
-  if(sock > -1) {
-    struct hostent* host_info = gethostbyname(address.c_str());	
-    long host_addr;
-    memcpy(&host_addr, host_info->h_addr, host_info->h_length);
-		
-    struct sockaddr_in ip_addr;	
-    ip_addr.sin_addr.s_addr = host_addr;;
-    ip_addr.sin_port=htons(port);
-    ip_addr.sin_family = AF_INET;
-
-    if(!connect(sock, (struct sockaddr*)&ip_addr,sizeof(ip_addr))) {
-      string get_string = "GET ";
-      get_string += "/";
-      get_string += " HTTP/1.1";
-      get_string += CRLF;
-      get_string += "Host: ";
-      get_string += "www.du.edu";
-      get_string += CRLF;
-      get_string += CRLF;
-		
-#ifdef _WIN32
-      int write_num = send(sock, get_string.c_str(), get_string.size(), 0);
-#else
-      int write_num = write(sock, get_string.c_str(), get_string.size());
-#endif
-      cout << "write_num: " << write_num << endl;
-	
-      char buffer[BUFFER_SIZE];
-#ifdef _WIN32
-      int read_num;
-      do {
-        read_num = recv(sock, buffer, BUFFER_SIZE, 0);
-        cout << "read_num: " << read_num << ": |" << buffer << "|" << endl;
-      }
-      while(read_num > 0);
-#else
-      int read_num;
-      do {
-	read_num = read(sock, buffer, BUFFER_SIZE);
-        cout << "read_num: " << read_num << ": |" << buffer << "|" << endl;
-      }
-      while(read_num > 0);
-#endif
-			
-#ifdef _WIN32
-      closesocket(sock);
-      WSACleanup();
-#else
-      close(sock);
-#endif
+  if(argc == 3) {
+    cout << "trying: " << argv[1] << " (" << argv[2] << ")" << endl;
+    RemoteClient client(argv[1], atoi(argv[2]));
+    if(client.Connect()) {
+      client.Send(42);
     }
   }
+  
+#ifdef _WIN32
+  WSACleanup();
+#endif
+  
   return 0;	
 }
--- wip/remote/server.cpp
+++ wip/remote/server.cpp
@@ -48,8 +48,55 @@ void *get_in_addr(struct sockaddr *sa)
   return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-int main(void)
+void error(const char *msg)
 {
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char **argv)
+{
+  int sockfd, newsockfd, portno;
+  socklen_t clilen;
+  struct sockaddr_in serv_addr, cli_addr;
+  int n;
+  if (argc < 2) {
+    fprintf(stderr,"ERROR, no port provided\n");
+    exit(1);
+  }
+  sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (sockfd < 0) 
+    error("ERROR opening socket");
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  portno = atoi(argv[1]);
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(portno);
+  if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+    error("ERROR on binding");
+  listen(sockfd,5);
+  clilen = sizeof(cli_addr);
+  newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+  if (newsockfd < 0) 
+    error("ERROR on accept");
+  
+  int value;
+  n = read(newsockfd,&value,255);
+  if (n < 0) error("ERROR reading from socket");
+  
+  printf("Here is the message: %d\n",value);
+  
+  /*
+    n = write(newsockfd,"I got your message",18);
+    if (n < 0) error("ERROR writing to socket");
+  */
+  
+  close(newsockfd);
+  close(sockfd);
+  
+  return 0; 
+  
+  /*
   int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
   struct addrinfo hints, *servinfo, *p;
   struct sockaddr_storage their_addr; // connector's address information
@@ -127,7 +174,6 @@ int main(void)
 	      s, sizeof s);
     printf("server: got connection from %s\n", s);
 
-    /*
     if (!fork()) { // this is the child process
       close(sockfd); // child doesn't need the listener
       if (send(new_fd, "Hello, world!", 13, 0) == -1)
@@ -135,10 +181,11 @@ int main(void)
       close(new_fd);
       exit(0);
     }
-    */
+
     pthread_t handle;
     pthread_create(&handle, NULL, talker, (void *)new_fd);
   }
 
   return 0;
+  */
 }
