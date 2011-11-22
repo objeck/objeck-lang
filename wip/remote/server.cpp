14ea719a9466e878d015ecfc1a8456241fa178a7
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2011-11-22 02:52:51 +0000

    remote object support - wip

--- wip/remote/server.cpp
+++ wip/remote/server.cpp
@@ -1,6 +1,4 @@
-/*
-** server.c -- a stream socket server demo
-*/
+// 
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,27 +18,56 @@
 
 using namespace std;
 
-void error(const char *msg)
+long server_sock = 0;
+
+void error(const char* msg)
 {
-    perror(msg);
-    exit(1);
+  perror(msg);
+  exit(1);
 }
+
+void* request_handler(void* data) 
+{
+  long client_sock = (long)data;
+
+  int value;  
+  if(read(client_sock, &value, sizeof(value)) < 0) {
+    error("ERROR reading from socket");
+  }
+  cout << "value=" << value << endl;  
+  
+  close(client_sock);  
+  return NULL;
+}
+
+void terminate(int value)
+{
+  cout << "shutting down server..." << endl;
+  close(server_sock);
+  exit(0);
+}
+
+// TODO...
+// 1) new object instance
+// 2) function/method call
+// 3) free object instance
+// 4) class level memory???
 
 int main(int argc, char **argv)
 {
-  if (argc < 2) {
+  if(argc < 2) {
     fprintf(stderr,"ERROR, no port provided\n");
     exit(1);
   }
   
-  int server_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  server_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
   if (server_sock < 0) {
     error("ERROR opening socket");
   }
   
   int port = atoi(argv[1]);
   struct sockaddr_in serv_addr;
-  bzero((char *) &serv_addr, sizeof(serv_addr));
+  bzero((char*)&serv_addr, sizeof(serv_addr));
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_addr.s_addr = INADDR_ANY;
   serv_addr.sin_port = htons(port);
@@ -50,118 +77,19 @@ int main(int argc, char **argv)
   }
   
   listen(server_sock, SOMAXCONN);
+  signal(SIGINT, terminate);
   
   struct sockaddr_in cli_addr;
   socklen_t cli_len = sizeof(cli_addr);  
   while(true) {
-    int client_sock = accept(server_sock, (struct sockaddr*) &cli_addr, &cli_len);
+    long client_sock = accept(server_sock, (struct sockaddr*) &cli_addr, &cli_len);
     if(client_sock < 0) {
       error("ERROR on accept");
     }
     
-    int value;
-    int n = read(client_sock, &value, sizeof(value));
-    if (n < 0) {
-      error("ERROR reading from socket");
-    }
-    cout << "value=" << value << endl;
-    
-    close(client_sock);
-  }
-  
-  close(server_sock);  
-  return 0; 
-  
-  /*
-  int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
-  struct addrinfo hints, *servinfo, *p;
-  struct sockaddr_storage their_addr; // connector's address information
-  socklen_t sin_size;
-  struct sigaction sa;
-  int yes=1;
-  char s[INET6_ADDRSTRLEN];
-  int rv;
-
-  memset(&hints, 0, sizeof hints);
-  hints.ai_family = AF_UNSPEC;
-  hints.ai_socktype = SOCK_STREAM;
-  hints.ai_flags = AI_PASSIVE; // use my IP
-
-  if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
-    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-    return 1;
-  }
-
-  // loop through all the results and bind to the first we can
-  for(p = servinfo; p != NULL; p = p->ai_next) {
-    if ((sockfd = socket(p->ai_family, p->ai_socktype,
-			 p->ai_protocol)) == -1) {
-      perror("server: socket");
-      continue;
-    }
-
-    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-		   sizeof(int)) == -1) {
-      perror("setsockopt");
-      exit(1);
-    }
-
-    if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-      close(sockfd);
-      perror("server: bind");
-      continue;
-    }
-
-    break;
-  }
-
-  if (p == NULL)  {
-    fprintf(stderr, "server: failed to bind\n");
-    return 2;
-  }
-
-  freeaddrinfo(servinfo); // all done with this structure
-
-  if (listen(sockfd, BACKLOG) == -1) {
-    perror("listen");
-    exit(1);
-  }
-
-  sa.sa_handler = sigchld_handler; // reap all dead processes
-  sigemptyset(&sa.sa_mask);
-  sa.sa_flags = SA_RESTART;
-  if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-    perror("sigaction");
-    exit(1);
-  }
-
-  printf("server: waiting for connections...\n");
-
-  while(1) {  // main accept() loop
-    sin_size = sizeof their_addr;
-    new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-    if (new_fd == -1) {
-      perror("accept");
-      continue;
-    }
-
-    inet_ntop(their_addr.ss_family,
-	      get_in_addr((struct sockaddr *)&their_addr),
-	      s, sizeof s);
-    printf("server: got connection from %s\n", s);
-
-    if (!fork()) { // this is the child process
-      close(sockfd); // child doesn't need the listener
-      if (send(new_fd, "Hello, world!", 13, 0) == -1)
-	perror("send");
-      close(new_fd);
-      exit(0);
-    }
-
     pthread_t handle;
-    pthread_create(&handle, NULL, talker, (void *)new_fd);
+    pthread_create(&handle, NULL, request_handler, (void*)client_sock);
   }
-
+  
   return 0;
-  */
 }
