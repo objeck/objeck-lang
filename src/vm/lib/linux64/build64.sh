0713652c38867605b82a94924ac4b682cc8dcd7f
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2012-04-15 20:36:36 +0000

    gtk3 support

--- src/vm/lib/linux64/build64.sh
+++ src/vm/lib/linux64/build64.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 rm -rf *.o
-# g++ -shared -fPIC `pkg-config --cflags --libs gtk+-2.0` -c $1.o $1.cpp 
+# g++ -shared -fPIC `pkg-config --cflags --libs gtk+-3.0` -c $1.o $1.cpp 
 g++ -g -shared -fPIC -Wl,-soname,$1.so.1 -D_DEBUG -D_X64 -o $1.so $1.cpp -lc
-# g++ -g -shared -fPIC `pkg-config --cflags --libs gtk+-2.0` -Wl,-soname,$1.so.1 -D_DEBUG -D_X64 -o $1.so.1.0.1 $1.cpp -lc
+# g++ -g -shared -fPIC `pkg-config --cflags --libs gtk+-3.0` -Wl,-soname,$1.so.1 -D_DEBUG -D_X64 -o $1.so.1.0.1 $1.cpp -lc
