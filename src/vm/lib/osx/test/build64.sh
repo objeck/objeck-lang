13a5a8fb229abc86e65924515e6bff9d7dbcc1dd
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2012-06-02 19:25:31 +0000

    code clean up

--- src/vm/lib/osx/test/build64.sh
+++
@@ -1,4 +0,0 @@
-#/bin/sh
-rm -rf *.o *.dylib
-g++ -D_X64 -shared -D_X64 -fPIC -g -c -Wno-deprecated -Wall $1.cpp
-g++ -D_X64 -dynamiclib -D_X64 -Wl,-headerpad_max_install_names,-undefined,dynamic_lookup,-compatibility_version,1.0,-current_version,1.0 -o $1.dylib $1.o
