bb3891bef082b2e02c7a082baddd37a14e1e3c21
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2012-06-21 20:37:13 +0000

    updates to odbc code

--- src/vm/lib/odbc/build_linux_x64.sh
+++ src/vm/lib/odbc/build_linux_x64.sh
@@ -1,8 +1,8 @@
 #/bin/sh
 rm -rf *.o
 rm -rf *.so
-g++ -O3 -Wall -fPIC -c *$1.cpp
-g++ -O3 -shared -D_DEBUG -lodbc -Wl,-soname,$1.so.1 -o $1.so *.o
+g++ -O3 -Wall -fPIC -c *$1.cpp -lodbc
+g++ -O3 -shared -D_DEBUG -Wl,-soname,$1.so.1 -o $1.so *.o -lodbc
 # g++ -shared -fPIC $1.o $1.cpp 
 # g++ -fPIC -O3 -c -Wno-deprecated -Wall -D_X64 $1.cpp -o $1.so
 # g++ -shared -fPIC -O3 -c -Wno-deprecated -Wall $1.cpp -o $1.so
