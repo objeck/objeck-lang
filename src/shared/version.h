9b6546d964fe8ac3a6e7e1205a931c1316637690
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2012-07-08 15:51:31 +0000

    v3.1.2_2 * interpreter tweaks * more clgb examples

--- src/shared/version.h
+++ src/shared/version.h
@@ -34,8 +34,8 @@
 
 #define MAGIC_NUM_LIB 0xddde
 #define MAGIC_NUM_EXE 0xdddd
-#define VER_NUM 3121
+#define VER_NUM 3122
 
-#define VERSION_STRING "v3.1.2_1"
+#define VERSION_STRING "v3.1.2_2"
 
 #endif
