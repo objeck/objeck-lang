63210ec99af0ab30c227be176d1086152cd5d369
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2012-08-17 05:46:29 +0000

    updating windows code

--- src/vm/os/windows/memory.h
+++ src/vm/os/windows/memory.h
@@ -40,8 +40,8 @@
 using namespace stx;
 
 // basic vm tuning parameters
-// #define MEM_MAX 1024 * 512
-#define MEM_MAX 1048576 * 8
+#define MEM_MAX 1024
+// #define MEM_MAX 1048576 * 8
 #define UNCOLLECTED_COUNT 4
 #define COLLECTED_COUNT 8
 
