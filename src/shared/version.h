244570fe396461c45471c3b28cb16c7e0aed4d45
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2012-09-08 03:56:37 +0000

    support for mult-dim array 'Size' method

--- src/shared/version.h
+++ src/shared/version.h
@@ -34,8 +34,8 @@
 
 #define MAGIC_NUM_LIB 0xddde
 #define MAGIC_NUM_EXE 0xdddd
-#define VER_NUM 3142
+#define VER_NUM 3143
 
-#define VERSION_STRING "v3.1.4_2"
+#define VERSION_STRING "v3.1.4_3"
 
 #endif
