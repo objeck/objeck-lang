fe623b39c6d71fb483cde863d53397d8bbb8a3a4
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2012-12-26 23:06:48 +0000

    adding support for openssl

--- src/shared/version.h
+++ src/shared/version.h
@@ -35,8 +35,8 @@
 #define MAGIC_NUM_WEB 0xdddf
 #define MAGIC_NUM_LIB 0xddde
 #define MAGIC_NUM_EXE 0xdddd
-#define VER_NUM 3162
+#define VER_NUM 3170
 
-#define VERSION_STRING "v3.1.6_2"
+#define VERSION_STRING "v3.1.7_0"
 
 #endif
