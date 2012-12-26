0b5d9a7441f744adcbcd73677d73bf80b98c07a8
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2012-12-26 21:16:08 +0000

    working on openssl support

--- src/objeck/deploy.cmd
+++ src/objeck/deploy.cmd
@@ -3,5 +3,6 @@ mkdir deploy
 devenv /rebuild Release objeck.sln
 copy Release\*.exe deploy
 copy ..\compiler\*.obl deploy
+copy ..\vm\lib\openssl\win32\bin\*.dll deploy
 xcopy /e ..\compiler\rc\* deploy\examples\
 cd deploy
