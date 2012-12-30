2d291c7a2524f2b55a1c1c743000cc5dec5af6d4
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2012-12-30 17:40:32 +0000

    v3.1.7_0

--- src/objeck/deploy.cmd
+++ src/objeck/deploy.cmd
@@ -1,24 +1,35 @@
+set LATEX_BIN="C:\Program Files\MiKTeX 2.9\miktex\bin\x64"
+
 rmdir /s /q deploy
 mkdir deploy
 REM build binaries
 devenv /rebuild Release objeck.sln
-copy Release\*.exe deploy
-copy ..\compiler\*.obl deploy
-del deploy\fcgi.obl
+mkdir deploy\bin
+copy Release\*.exe deploy\bin
+copy ..\compiler\*.obl deploy\bin
+del deploy\bin\fcgi.obl
 REM openssl support
-mkdir deploy\lib\openssl
+mkdir deploy\bin\lib\openssl
 cd ..\vm\lib\openssl\openssl
 devenv /rebuild Release openssl.sln
-copy Release\*.dll ..\..\..\..\objeck\deploy\lib\openssl
-copy ..\win32\bin\*.dll ..\..\..\..\objeck\deploy
+copy Release\*.dll ..\..\..\..\objeck\deploy\bin\lib\openssl
+copy ..\win32\bin\*.dll ..\..\..\..\objeck\deploy\bin
 cd ..\..\..\..\objeck
 REM odbc support
-mkdir deploy\lib\odbc
+mkdir deploy\bin\lib\odbc
 cd ..\vm\lib\odbc
 devenv /rebuild Release odbc.sln
-copy Release\*.dll ..\..\..\objeck\deploy\lib\odbc
-copy ..\win32\bin\*.dll ..\..\..\objeck\deploy
+copy Release\*.dll ..\..\..\objeck\deploy\bin\lib\odbc
+copy ..\win32\bin\*.dll ..\..\..\objeck\deploy\bin
 cd ..\..\..\objeck
 REM copy examples
 xcopy /e ..\compiler\rc\* deploy\examples\
+REM build and update docs
+mkdir deploy\doc
+%LATEX_BIN%\pdflatex ..\..\docs\guide\objeck_lang.tex
+copy ..\..\docs\guide\objeck_lang.pdf deploy\doc
+mkdir deploy\doc\syntax
+copy ..\..\docs\syntax\* deploy\doc\syntax
+copy ..\..\docs\readme.rtf deploy
+REM finished
 cd deploy
