cac60ee148a413e4c8f780f7b37a8ea2c82fc75a
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2013-01-27 19:21:39 +0000

    deployment script for 64-bit Linux

--- src/objeck/deploy_linux32.sh
+++ src/objeck/deploy_linux32.sh
@@ -9,27 +9,59 @@ mkdir deploy/doc
 mkdir deploy/examples
 # build compiler
 cd ../compiler
-cp Makefile.32 Makefile
+
+if [ ! -z "$1" ] && [ "$1" = "32" ]; then
+	cp Makefile.32 Makefile
+else
+	cp Makefile.64 Makefile
+fi
+
 make clean; make -j3
 cp obc ../objeck/deploy/bin
 cp *.obl ../objeck/deploy/bin
 rm ../objeck/deploy/fcgi.obl
 # build utilities
 cd ../utilities
-cp Makefile.32 Makefile
+
+if [ ! -z "$1" ] && [ "$1" = "32" ]; then
+	cp Makefile.32 Makefile
+else
+	cp Makefile.64 Makefile
+fi
+
 make clean; make -j3
 cp obu ../objeck/deploy/bin
 # build VM
 cd ../vm
-cp Makefile.32 Makefile
-cp os/posix/Makefile.32 os/posix/Makefile
-cp jit/ia32/Makefile.32 jit/ia32/Makefile
+
+if [ ! -z "$1" ] && [ "$1" = "32" ]; then
+	cp Makefile.32 Makefile
+	cp os/posix/Makefile.32 os/posix/Makefile
+	cp jit/ia32/Makefile.32 jit/ia32/Makefile
+else
+	cp Makefile.64 Makefile
+	cp os/posix/Makefile.64 os/posix/Makefile
+	cp jit/amd64/Makefile.64 jit/amd64/Makefile
+fi
+
 make clean; make -j3
 cp obr ../objeck/deploy/bin
 # build debugger
-cp Makefile.obd32 Makefile
+
+if [ ! -z "$1" ] && [ "$1" = "32" ]; then
+	cp Makefile.obd32 Makefile
+else
+	cp Makefile.obd64 Makefile
+fi
+
 cd debugger
-cp Makefile.32 Makefile
+
+if [ ! -z "$1" ] && [ "$1" = "32" ]; then
+	cp Makefile.32 Makefile
+else
+	cp Makefile.64 Makefile
+fi
+
 make clean; make -j3
 cp obd ../../objeck/deploy/bin
 # build libaries
@@ -45,8 +77,9 @@ cp docs/guide/objeck_lang.pdf src/objeck/deploy/doc
 cp docs/readme.rtf src/objeck/deploy
 # copy examples
 cp -rf src/compiler/rc/* src/objeck/deploy/examples
+
 # deploy
-if [ ! -z "$1" ] && [ "$1" = "deploy" ]; then
+if [ ! -z "$2" ] && [ "$2" = "deploy" ]; then
 	rm -rf ~/Desktop/objeck-lang
 	cp -rf src/objeck/deploy ~/Desktop/objeck-lang
 	cd ~/Desktop
