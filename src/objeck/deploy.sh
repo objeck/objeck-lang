80b4eb694d2a78623be929bed78d65c0d66aa6d7
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2013-03-06 21:55:38 +0000

    updated the deploy script to support mingw

--- src/objeck/deploy.sh
+++ src/objeck/deploy.sh
@@ -5,12 +5,16 @@ rm -rf deploy
 mkdir deploy
 mkdir deploy/bin
 mkdir deploy/bin/lib
+mkdir deploy/bin/lib/odbc
+mkdir deploy/bin/lib/openssl
 mkdir deploy/doc
 
 # build compiler
 cd ../compiler
 if [ ! -z "$1" ] && [ "$1" = "32" ]; then
 	cp Makefile.32 Makefile
+elif [ ! -z "$1" ] && [ "$1" = "mingw" ]; then
+	cp Makefile.MINGW Makefile
 else
 	cp Makefile.64 Makefile
 fi
@@ -23,6 +27,8 @@ rm ../objeck/deploy/fcgi.obl
 cd ../utilities
 if [ ! -z "$1" ] && [ "$1" = "32" ]; then
 	cp Makefile.32 Makefile
+elif [ ! -z "$1" ] && [ "$1" = "mingw" ]; then
+	cp Makefile.MINGW Makefile
 else
 	cp Makefile.64 Makefile
 fi
@@ -37,8 +43,12 @@ if [ ! -z "$1" ] && [ "$1" = "32" ]; then
 	cp jit/ia32/Makefile.32 jit/ia32/Makefile
 elif [ ! -z "$1" ] && [ "$1" = "osx" ]; then
 	cp Makefile.OSX.64 Makefile
-	cp os/posix/Makefile..OSX.64 os/posix/Makefile
+	cp os/posix/Makefile.OSX.64 os/posix/Makefile
 	cp jit/amd64/Makefile.64 jit/amd64/Makefile
+elif [ ! -z "$1" ] && [ "$1" = "mingw" ]; then
+	cp Makefile.MINGW Makefile
+	cp os/windows/Makefile.MINGW os/posix/Makefile
+	cp jit/ia32/Makefile.MINGW jit/ia32/Makefile
 else 
 	cp Makefile.64 Makefile
 	cp os/posix/Makefile.64 os/posix/Makefile
@@ -50,14 +60,19 @@ cp obr ../objeck/deploy/bin
 # build debugger
 if [ ! -z "$1" ] && [ "$1" = "32" ]; then
 	cp Makefile.obd32 Makefile
+elif [ ! -z "$1" ] && [ "$1" = "mingw" ]; then
+	cp Makefile.obd.MINGW Makefile
 else
 	cp Makefile.obd64 Makefile
 fi
+
 cd debugger
 if [ ! -z "$1" ] && [ "$1" = "32" ]; then
 	cp Makefile.32 Makefile
 elif [ ! -z "$1" ] && [ "$1" = "osx" ]; then
 	cp Makefile.OSX.obd64 Makefile
+elif [ ! -z "$1" ] && [ "$1" = "mingw" ]; then
+	cp Makefile.MINGW Makefile
 else
 	cp Makefile.64 Makefile
 fi
@@ -68,18 +83,26 @@ cp obd ../../objeck/deploy/bin
 cd ../lib/odbc
 if [ ! -z "$1" ] && [ "$1" = "osx" ]; then
 	./build_osx_x64.sh odbc
-	cp odbc.dylib ../../../objeck/deploy/bin/lib
+	cp odbc.dylib ../../../objeck/deploy/bin/lib/odbc
+elif [ ! -z "$1" ] && [ "$1" = "mingw" ]; then
+	./build_win32.sh odbc
+	cp odbc.so ../../../objeck/deploy/bin/lib/odbc
 else
 	./build_linux.sh odbc
-	cp odbc.so ../../../objeck/deploy/bin/lib
+	cp odbc.so ../../../objeck/deploy/bin/lib/odbc
 fi
+
 cd ../openssl
+
 if [ ! -z "$1" ] && [ "$1" = "osx" ]; then
 	./build_osx_x64.sh openssl
-	cp openssl.dylib ../../../objeck/deploy/bin/lib
+	cp openssl.dylib ../../../objeck/deploy/bin/lib/openssl
+elif [ ! -z "$1" ] && [ "$1" = "mingw" ]; then
+	./build_win32.sh openssl
+	cp openssl.so ../../../objeck/deploy/bin/lib/openssl
 else
 	./build_linux.sh openssl
-	cp openssl.so ../../../objeck/deploy/bin/lib
+	cp openssl.so ../../../objeck/deploy/bin/lib/openssl
 fi
 
 # copy guide
@@ -91,12 +114,22 @@ svn export src/compiler/rc src/objeck/deploy/examples
 
 # deploy
 if [ ! -z "$2" ] && [ "$2" = "deploy" ]; then
-	rm -rf ~/Desktop/objeck-lang
-	cp -rf src/objeck/deploy ~/Desktop/objeck-lang
-	cd ~/Desktop
+	if [ ! -z "$1" ] && [ "$1" = "mingw" ]; then
+		rm -rf "C:\Documents and Settings\Administrator\Desktop\objeck-lang"
+		cp -rf src/objeck/deploy "C:\Documents and Settings\Administrator\Desktop\objeck-lang"
+		cd "C:\Documents and Settings\Administrator\Desktop"
+	else
+		rm -rf ~/Desktop/objeck-lang
+		cp -rf src/objeck/deploy ~/Desktop/objeck-lang
+		cd ~/Desktop
+	fi;
+
 	if [ ! -z "$1" ] && [ "$1" = "osx" ]; then
 		rm -f objeck.zip
-		zip -r objeck-lang objeck-lang/*
+		zip -r objeck-lang/* objeck-lang 
+	elif [ ! -z "$1" ] && [ "$1" = "mingw" ]; then
+		rm -f objeck.zip
+		"C:\Program Files\7-Zip\7z.exe" a -tzip objeck-lang
 	else 
 		rm -f objeck.tar objeck.tgz
 		tar cf objeck.tar objeck-lang
