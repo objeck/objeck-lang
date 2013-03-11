c47ae31674a39b240ee9892d39fd18d622a81253
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2013-03-11 08:54:19 +0000

    code clean up

--- src/objeck/deploy.sh
+++ src/objeck/deploy.sh
@@ -108,7 +108,8 @@ fi
 # copy guide
 cd ../../../..
 cp docs/guide/objeck_lang.pdf src/objeck/deploy/doc
-cp -r docs/syntax src/objeck/deploy/doc
+svn export docs/syntax src/objeck/deploy/doc/syntax
+
 cp docs/readme.rtf src/objeck/deploy
 # copy examples
 svn export src/compiler/rc src/objeck/deploy/examples
