267986263afb49750c5860e914b35d5617a2fa87
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2013-05-07 15:34:45 +0000

    v3.2.0_2

--- src/shared/version.h
+++ src/shared/version.h
@@ -35,8 +35,8 @@
 #define MAGIC_NUM_WEB 0xdddf
 #define MAGIC_NUM_LIB 0xddde
 #define MAGIC_NUM_EXE 0xdddd
-#define VER_NUM 3200
+#define VER_NUM 3202
 
-#define VERSION_STRING L"v3.2.0_1"
+#define VERSION_STRING L"v3.2.0_2"
 
 #endif
