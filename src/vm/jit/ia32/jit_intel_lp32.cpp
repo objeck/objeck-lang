b8edcb7c86269662104baf911a82097d4778cedf
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2013-05-27 04:10:16 +0000

    v3.2.2_0

--- src/vm/jit/ia32/jit_intel_lp32.cpp
+++ src/vm/jit/ia32/jit_intel_lp32.cpp
@@ -4,7 +4,7 @@
  * Copyright (c) 2008-2013, Randy Hollines
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with%G or without 
+ * Redistribution and use in source and binary forms, with or without 
  * modification, are permitted provided that the following conditions are met:
  *
  * - Redistributions of source code must retain the above copyright 
