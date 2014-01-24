f50321307eba96d1cbe800abd23d0c9289db728e
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2014-01-24 15:36:45 +0000

    adding find/replace

--- wip/wxWidgets/stc/stc_test/edit.cpp
+++ wip/wxWidgets/stc/stc_test/edit.cpp
@@ -116,6 +116,9 @@ Edit::Edit (wxWindow *parent, wxWindowID id,
     m_DividerID = 1;
     m_FoldingID = 2;
 
+    m_dlgFind = NULL;
+    m_dlgReplace = NULL;
+
     // initialize language
     m_language = NULL;
 
@@ -212,15 +215,112 @@ void Edit::OnEditPaste (wxCommandEvent &WXUNUSED(event)) {
 }
 
 void Edit::OnFind (wxCommandEvent &WXUNUSED(event)) {
+  if(m_dlgFind)
+  {
+    wxDELETE(m_dlgFind);
+  }
+  else
+  {
+    m_dlgFind = new wxFindReplaceDialog
+      (
+      this,
+      &m_findData,
+      wxT("Find dialog"),
+      // just for testing
+      wxFR_NOWHOLEWORD
+      );
+
+    m_dlgFind->Show(true);
+  }
 }
 
 void Edit::OnFindNext (wxCommandEvent &WXUNUSED(event)) {
 }
 
 void Edit::OnReplace (wxCommandEvent &WXUNUSED(event)) {
+  if(m_dlgReplace)
+  {
+    wxDELETE(m_dlgReplace);
+  }
+  else
+  {
+    m_dlgReplace = new wxFindReplaceDialog
+      (
+      this,
+      &m_findData,
+      wxT("Find and replace dialog"),
+      wxFR_REPLACEDIALOG
+      );
+
+    m_dlgReplace->Show(true);
+  }
 }
 
 void Edit::OnReplaceNext (wxCommandEvent &WXUNUSED(event)) {
+}
+
+void Edit::OnFindDialog(wxFindDialogEvent& event)
+{
+
+  wxEventType type = event.GetEventType();
+
+  /*
+  if(type == wxEVT_FIND || type == wxEVT_FIND_NEXT)
+  {
+    wxLogMessage(wxT("Find %s'%s' (flags: %s)"),
+      type == wxEVT_FIND_NEXT ? wxT("next ") : wxT(""),
+      event.GetFindString().c_str(),
+      DecodeFindDialogEventFlags(event.GetFlags()).c_str());
+  }
+  else if(type == wxEVT_FIND_REPLACE ||
+    type == wxEVT_FIND_REPLACE_ALL)
+  {
+    wxLogMessage(wxT("Replace %s'%s' with '%s' (flags: %s)"),
+      type == wxEVT_FIND_REPLACE_ALL ? wxT("all ") : wxT(""),
+      event.GetFindString().c_str(),
+      event.GetReplaceString().c_str(),
+      DecodeFindDialogEventFlags(event.GetFlags()).c_str());
+  }
+  else if(type == wxEVT_FIND_CLOSE)
+  {
+    wxFindReplaceDialog *dlg = event.GetDialog();
+
+    int idMenu;
+    const wxChar *txt;
+    if(dlg == m_dlgFind)
+    {
+      txt = wxT("Find");
+      idMenu = wxID_FIND;
+      m_dlgFind = NULL;
+    }
+    else if(dlg == m_dlgReplace)
+    {
+      txt = wxT("Replace");
+      idMenu = wxID_REPLACE;
+      m_dlgReplace = NULL;
+    }
+    else
+    {
+      txt = wxT("Unknown");
+      idMenu = wxID_ANY;
+
+      wxFAIL_MSG(wxT("unexpected event"));
+    }
+
+    wxLogMessage(wxT("%s dialog is being closed."), txt);
+
+    if(idMenu != wxID_ANY)
+    {
+      GetMenuBar()->Check(idMenu, false);
+    }
+
+    dlg->Destroy();
+  }
+  else
+  {
+    wxLogError(wxT("Unknown find dialog event!"));
+  }
+  */
 }
 
 void Edit::OnBraceMatch (wxCommandEvent &WXUNUSED(event)) {
@@ -592,238 +692,3 @@ bool Edit::Modified () {
     return (GetModify() && !GetReadOnly());
 }
 
-//----------------------------------------------------------------------------
-// EditProperties
-//----------------------------------------------------------------------------
-
-EditProperties::EditProperties (Edit *edit,
-                                long style)
-        : wxDialog (edit, wxID_ANY, wxEmptyString,
-                    wxDefaultPosition, wxDefaultSize,
-                    style | wxDEFAULT_DIALOG_STYLE | wxRESIZE_BORDER) {
-
-    // sets the application title
-    SetTitle (_("Properties"));
-    wxString text;
-
-    // fullname
-    wxBoxSizer *fullname = new wxBoxSizer (wxHORIZONTAL);
-    fullname->Add (10, 0);
-    fullname->Add (new wxStaticText (this, wxID_ANY, _("Full filename"),
-                                     wxDefaultPosition, wxSize(80, wxDefaultCoord)),
-                   0, wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL);
-    fullname->Add (new wxStaticText (this, wxID_ANY, edit->GetFilename()),
-                   0, wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL);
-
-    // text info
-    wxGridSizer *textinfo = new wxGridSizer (4, 0, 2);
-    textinfo->Add (new wxStaticText (this, wxID_ANY, _("Language"),
-                                     wxDefaultPosition, wxSize(80, wxDefaultCoord)),
-                   0, wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL|wxLEFT, 4);
-    textinfo->Add (new wxStaticText (this, wxID_ANY, edit->m_language->name),
-                   0, wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL|wxRIGHT, 4);
-    textinfo->Add (new wxStaticText (this, wxID_ANY, _("Lexer-ID: "),
-                                     wxDefaultPosition, wxSize(80, wxDefaultCoord)),
-                   0, wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL|wxLEFT, 4);
-    text = wxString::Format (wxT("%d"), edit->GetLexer());
-    textinfo->Add (new wxStaticText (this, wxID_ANY, text),
-                   0, wxALIGN_RIGHT|wxALIGN_CENTER_VERTICAL|wxRIGHT, 4);
-    wxString EOLtype = wxEmptyString;
-    switch (edit->GetEOLMode()) {
-        case wxSTC_EOL_CR: {EOLtype = wxT("CR (Unix)"); break; }
-        case wxSTC_EOL_CRLF: {EOLtype = wxT("CRLF (Windows)"); break; }
-        case wxSTC_EOL_LF: {EOLtype = wxT("CR (Macintosh)"); break; }
-    }
-    textinfo->Add (new wxStaticText (this, wxID_ANY, _("Line endings"),
-                                     wxDefaultPosition, wxSize(80, wxDefaultCoord)),
-                   0, wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL|wxLEFT, 4);
-    textinfo->Add (new wxStaticText (this, wxID_ANY, EOLtype),
-                   0, wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL|wxRIGHT, 4);
-
-    // text info box
-    wxStaticBoxSizer *textinfos = new wxStaticBoxSizer (
-                     new wxStaticBox (this, wxID_ANY, _("Informations")),
-                     wxVERTICAL);
-    textinfos->Add (textinfo, 0, wxEXPAND);
-    textinfos->Add (0, 6);
-
-    // statistic
-    wxGridSizer *statistic = new wxGridSizer (4, 0, 2);
-    statistic->Add (new wxStaticText (this, wxID_ANY, _("Total lines"),
-                                     wxDefaultPosition, wxSize(80, wxDefaultCoord)),
-                    0, wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL|wxLEFT, 4);
-    text = wxString::Format (wxT("%d"), edit->GetLineCount());
-    statistic->Add (new wxStaticText (this, wxID_ANY, text),
-                    0, wxALIGN_RIGHT|wxALIGN_CENTER_VERTICAL|wxRIGHT, 4);
-    statistic->Add (new wxStaticText (this, wxID_ANY, _("Total chars"),
-                                     wxDefaultPosition, wxSize(80, wxDefaultCoord)),
-                    0, wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL|wxLEFT, 4);
-    text = wxString::Format (wxT("%d"), edit->GetTextLength());
-    statistic->Add (new wxStaticText (this, wxID_ANY, text),
-                    0, wxALIGN_RIGHT|wxALIGN_CENTER_VERTICAL|wxRIGHT, 4);
-    statistic->Add (new wxStaticText (this, wxID_ANY, _("Current line"),
-                                     wxDefaultPosition, wxSize(80, wxDefaultCoord)),
-                    0, wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL|wxLEFT, 4);
-    text = wxString::Format (wxT("%d"), edit->GetCurrentLine());
-    statistic->Add (new wxStaticText (this, wxID_ANY, text),
-                    0, wxALIGN_RIGHT|wxALIGN_CENTER_VERTICAL|wxRIGHT, 4);
-    statistic->Add (new wxStaticText (this, wxID_ANY, _("Current pos"),
-                                     wxDefaultPosition, wxSize(80, wxDefaultCoord)),
-                    0, wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL|wxLEFT, 4);
-    text = wxString::Format (wxT("%d"), edit->GetCurrentPos());
-    statistic->Add (new wxStaticText (this, wxID_ANY, text),
-                    0, wxALIGN_RIGHT|wxALIGN_CENTER_VERTICAL|wxRIGHT, 4);
-
-    // char/line statistics
-    wxStaticBoxSizer *statistics = new wxStaticBoxSizer (
-                     new wxStaticBox (this, wxID_ANY, _("Statistics")),
-                     wxVERTICAL);
-    statistics->Add (statistic, 0, wxEXPAND);
-    statistics->Add (0, 6);
-
-    // total pane
-    wxBoxSizer *totalpane = new wxBoxSizer (wxVERTICAL);
-    totalpane->Add (fullname, 0, wxEXPAND | wxLEFT | wxRIGHT | wxTOP, 10);
-    totalpane->Add (0, 6);
-    totalpane->Add (textinfos, 0, wxEXPAND | wxLEFT | wxRIGHT, 10);
-    totalpane->Add (0, 10);
-    totalpane->Add (statistics, 0, wxEXPAND | wxLEFT | wxRIGHT, 10);
-    totalpane->Add (0, 6);
-    wxButton *okButton = new wxButton (this, wxID_OK, _("OK"));
-    okButton->SetDefault();
-    totalpane->Add (okButton, 0, wxALIGN_CENTER | wxALL, 10);
-
-    SetSizerAndFit (totalpane);
-
-    ShowModal();
-}
-
-#if wxUSE_PRINTING_ARCHITECTURE
-
-//----------------------------------------------------------------------------
-// EditPrint
-//----------------------------------------------------------------------------
-
-EditPrint::EditPrint (Edit *edit, const wxChar *title)
-              : wxPrintout(title) {
-    m_edit = edit;
-    m_printed = 0;
-
-}
-
-bool EditPrint::OnPrintPage (int page) {
-
-    wxDC *dc = GetDC();
-    if (!dc) return false;
-
-    // scale DC
-    PrintScaling (dc);
-
-    // print page
-    if (page == 1) m_printed = 0;
-    m_printed = m_edit->FormatRange (1, m_printed, m_edit->GetLength(),
-                                     dc, dc, m_printRect, m_pageRect);
-
-    return true;
-}
-
-bool EditPrint::OnBeginDocument (int startPage, int endPage) {
-
-    if (!wxPrintout::OnBeginDocument (startPage, endPage)) {
-        return false;
-    }
-
-    return true;
-}
-
-void EditPrint::GetPageInfo (int *minPage, int *maxPage, int *selPageFrom, int *selPageTo) {
-
-    // initialize values
-    *minPage = 0;
-    *maxPage = 0;
-    *selPageFrom = 0;
-    *selPageTo = 0;
-
-    // scale DC if possible
-    wxDC *dc = GetDC();
-    if (!dc) return;
-    PrintScaling (dc);
-
-    // get print page informations and convert to printer pixels
-    wxSize ppiScr;
-    GetPPIScreen (&ppiScr.x, &ppiScr.y);
-    wxSize page = g_pageSetupData->GetPaperSize();
-    page.x = static_cast<int> (page.x * ppiScr.x / 25.4);
-    page.y = static_cast<int> (page.y * ppiScr.y / 25.4);
-    m_pageRect = wxRect (0,
-                         0,
-                         page.x,
-                         page.y);
-
-    // get margins informations and convert to printer pixels
-    wxPoint pt = g_pageSetupData->GetMarginTopLeft();
-    int left = pt.x;
-    int top = pt.y;
-    pt = g_pageSetupData->GetMarginBottomRight();
-    int right = pt.x;
-    int bottom = pt.y;
-
-    top = static_cast<int> (top * ppiScr.y / 25.4);
-    bottom = static_cast<int> (bottom * ppiScr.y / 25.4);
-    left = static_cast<int> (left * ppiScr.x / 25.4);
-    right = static_cast<int> (right * ppiScr.x / 25.4);
-
-    m_printRect = wxRect (left,
-                          top,
-                          page.x - (left + right),
-                          page.y - (top + bottom));
-
-    // count pages
-    while (HasPage (*maxPage)) {
-        m_printed = m_edit->FormatRange (0, m_printed, m_edit->GetLength(),
-                                       dc, dc, m_printRect, m_pageRect);
-        *maxPage += 1;
-    }
-    if (*maxPage > 0) *minPage = 1;
-    *selPageFrom = *minPage;
-    *selPageTo = *maxPage;
-}
-
-bool EditPrint::HasPage (int WXUNUSED(page)) {
-
-    return (m_printed < m_edit->GetLength());
-}
-
-bool EditPrint::PrintScaling (wxDC *dc){
-
-    // check for dc, return if none
-    if (!dc) return false;
-
-    // get printer and screen sizing values
-    wxSize ppiScr;
-    GetPPIScreen (&ppiScr.x, &ppiScr.y);
-    if (ppiScr.x == 0) { // most possible guess 96 dpi
-        ppiScr.x = 96;
-        ppiScr.y = 96;
-    }
-    wxSize ppiPrt;
-    GetPPIPrinter (&ppiPrt.x, &ppiPrt.y);
-    if (ppiPrt.x == 0) { // scaling factor to 1
-        ppiPrt.x = ppiScr.x;
-        ppiPrt.y = ppiScr.y;
-    }
-    wxSize dcSize = dc->GetSize();
-    wxSize pageSize;
-    GetPageSizePixels (&pageSize.x, &pageSize.y);
-
-    // set user scale
-    float scale_x = (float)(ppiPrt.x * dcSize.x) /
-                    (float)(ppiScr.x * pageSize.x);
-    float scale_y = (float)(ppiPrt.y * dcSize.y) /
-                    (float)(ppiScr.y * pageSize.y);
-    dc->SetUserScale (scale_x, scale_y);
-
-    return true;
-}
-
-#endif // wxUSE_PRINTING_ARCHITECTURE
