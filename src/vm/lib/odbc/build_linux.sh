6b0500d804b2f27809e77cd195a5a00a24f8b5a2
objeck <objeck@09746e85-98b3-41f0-ac33-d31734d27783>
2014-05-20 04:37:58 +0000

    refactoring odbc code

--- src/vm/lib/odbc/build_linux.sh
+++ src/vm/lib/odbc/build_linux.sh
@@ -1,10 +1,10 @@
 #/bin/sh
 rm -rf *.o
 rm -rf *.so
-# g++ -O3 -Wall -fPIC -c *$1.cpp -lodbc
-# g++ -O3 -shared -D_DEBUG -Wl,-soname,$1.so.1 -o $1.so *.o -lodbc
-g++ -g -Wall -fPIC -c *$1.cpp -lodbc
-g++ -g -shared -D_DEBUG -Wl,-soname,$1.so.1 -o $1.so *.o -lodbc
+g++ -O3 -Wall -fPIC -c *$1.cpp -lodbc
+g++ -O3 -shared -Wl,-soname,$1.so.1 -o $1.so *.o -lodbc
+# g++ -g -Wall -fPIC -c *$1.cpp -lodbc
+# g++ -g -shared -D_DEBUG -Wl,-soname,$1.so.1 -o $1.so *.o -lodbc
 # g++ -shared -fPIC $1.o $1.cpp 
 # g++ -fPIC -O3 -c -Wno-deprecated -Wall -D_X64 $1.cpp -o $1.so
 # g++ -shared -fPIC -O3 -c -Wno-deprecated -Wall $1.cpp -o $1.so
