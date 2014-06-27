f84f448fb2970e5a656e6d9a778f6ed2e1e9ba55
Randy Hollines <objeck@objeck.org>
2014-06-27 07:29:15 -0700

    ide demo - getting ready to add find/replace support

--- wip/ide/edit.h
+++ wip/ide/edit.h
@@ -55,51 +55,10 @@ public:
 
   void OpenFile(wxString& fn);
 
-  //! pages
+  // event handlers
+  void OnEdit(wxCommandEvent &event);
   void OnPageClose(wxAuiNotebookEvent& event);
   void OnPageChanged(wxAuiNotebookEvent& event);
-  //! edit
-  void OnEditRedo(wxCommandEvent &event);
-  void OnEditUndo(wxCommandEvent &event);
-  void OnEditClear(wxCommandEvent &event);
-  void OnEditCut(wxCommandEvent &event);
-  void OnEditCopy(wxCommandEvent &event);
-  void OnEditPaste(wxCommandEvent &event);
-  //! find
-  void OnFind(wxCommandEvent &event);
-  void OnFindNext(wxCommandEvent &event);
-  void OnReplace(wxCommandEvent &event);
-  void OnReplaceNext(wxCommandEvent &event);
-  void OnBraceMatch(wxCommandEvent &event);
-  void OnGoto(wxCommandEvent &event);
-  void OnEditIndentInc(wxCommandEvent &event);
-  void OnEditIndentRed(wxCommandEvent &event);
-  void OnEditSelectAll(wxCommandEvent &event);
-  void OnEditSelectLine(wxCommandEvent &event);
-  //! view
-  void OnHilightLang(wxCommandEvent &event);
-  void OnDisplayEOL(wxCommandEvent &event);
-  void OnIndentGuide(wxCommandEvent &event);
-  void OnLineNumber(wxCommandEvent &event);
-  void OnLongLineOn(wxCommandEvent &event);
-  void OnWhiteSpace(wxCommandEvent &event);
-  void OnFoldToggle(wxCommandEvent &event);
-  void OnSetOverType(wxCommandEvent &event);
-  void OnSetReadOnly(wxCommandEvent &event);
-  void OnWrapmodeOn(wxCommandEvent &event);
-  void OnUseCharset(wxCommandEvent &event);
-  //! annotations
-  void OnAnnotationAdd(wxCommandEvent& event);
-  void OnAnnotationRemove(wxCommandEvent& event);
-  void OnAnnotationClear(wxCommandEvent& event);
-  void OnAnnotationStyle(wxCommandEvent& event);
-  //! extra
-  void OnChangeCase(wxCommandEvent &event);
-  void OnConvertEOL(wxCommandEvent &event);
-  //! stc
-  void OnMarginClick(wxStyledTextEvent &event);
-  void OnCharAdded(wxStyledTextEvent &event);
-  void OnKey(wxStyledTextEvent &event);
   
   DECLARE_EVENT_TABLE()
 };
@@ -115,6 +74,16 @@ class Edit : public wxStyledTextCtrl {
       margin_id_fold,
       MARGIN_FOLD
     };
+
+    wxString m_filename;
+    LanguageInfo const* m_language;
+    int m_LineNrID;
+    int m_LineNrMargin;
+    int m_FoldingID;
+    int m_FoldingMargin;
+    int m_DividerID;
+
+    DECLARE_EVENT_TABLE()
 
 public:
     //! constructor
@@ -132,42 +101,64 @@ public:
     ~Edit ();
 
     // event handlers
-    // common
-    void OnSize(wxSizeEvent& event);
-    void OnEditSelectLine(wxCommandEvent &event);
-    void OnLineNumber(wxCommandEvent &event);
+    void OnSize(wxSizeEvent &event);
+    // edit
+    void OnEditRedo(wxCommandEvent &event);
+    void OnEditUndo(wxCommandEvent &event);
+    void OnEditClear(wxCommandEvent &event);
+    void OnEditCut(wxCommandEvent &event);
+    void OnEditCopy(wxCommandEvent &event);
+    void OnEditPaste(wxCommandEvent &event);
+    // find
+    void OnFind(wxCommandEvent &event);
+    void OnFindNext(wxCommandEvent &event);
+    void OnReplace(wxCommandEvent &event);
+    void OnReplaceNext(wxCommandEvent &event);
     void OnBraceMatch(wxCommandEvent &event);
+    void OnGoto(wxCommandEvent &event);
+    void OnEditIndentInc(wxCommandEvent &event);
+    void OnEditIndentRed(wxCommandEvent &event);
+    void OnEditSelectAll(wxCommandEvent &event);
+    void OnEditSelectLine(wxCommandEvent &event);
+    //! view
+    void OnHilightLang(wxCommandEvent &event);
+    void OnDisplayEOL(wxCommandEvent &event);
+    void OnIndentGuide(wxCommandEvent &event);
+    void OnLineNumber(wxCommandEvent &event);
+    void OnLongLineOn(wxCommandEvent &event);
+    void OnWhiteSpace(wxCommandEvent &event);
+    void OnFoldToggle(wxCommandEvent &event);
+    void OnSetOverType(wxCommandEvent &event);
+    void OnSetReadOnly(wxCommandEvent &event);
+    void OnWrapmodeOn(wxCommandEvent &event);
+    void OnUseCharset(wxCommandEvent &event);
+    // annotations
+    void OnAnnotationAdd(wxCommandEvent& event);
+    void OnAnnotationRemove(wxCommandEvent& event);
+    void OnAnnotationClear(wxCommandEvent& event);
+    void OnAnnotationStyle(wxCommandEvent& event);
+    //! extra
+    void OnChangeCase(wxCommandEvent &event);
+    void OnConvertEOL(wxCommandEvent &event);
+    // stc
+    void OnMarginClick(wxStyledTextEvent &event);
+    void OnCharAdded(wxStyledTextEvent &event);
+    void OnKey(wxStyledTextEvent &event);
     
+    //! load/save file
+    bool LoadFile();
+    bool LoadFile(const wxString &filename);
+    bool SaveFile();
+    bool SaveFile(const wxString &filename);
+    bool Modified();
+    wxString GetFilename() { return m_filename; };
+    void SetFilename(const wxString &filename) { m_filename = filename; };
+
     //! language/lexer
     wxString DeterminePrefs (const wxString &filename);
     bool InitializePrefs (const wxString &filename);
     bool UserSettings (const wxString &filename);
     LanguageInfo const* GetLanguageInfo () {return m_language;};
-
-    //! load/save file
-    bool LoadFile ();
-    bool LoadFile (const wxString &filename);
-    bool SaveFile ();
-    bool SaveFile (const wxString &filename);
-    bool Modified ();
-    wxString GetFilename () {return m_filename;};
-    void SetFilename (const wxString &filename) {m_filename = filename;};
-
-private:
-    // file
-    wxString m_filename;
-
-    // lanugage properties
-    LanguageInfo const* m_language;
-
-    // margin variables
-    int m_LineNrID;
-    int m_LineNrMargin;
-    int m_FoldingID;
-    int m_FoldingMargin;
-    int m_DividerID;
-
-    DECLARE_EVENT_TABLE()
 };
 
 //----------------------------------------------------------------------------
