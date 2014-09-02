d756d3cb87d5f4b3624e4289148adb3dcd3d7cf4
Randy Hollines <objeck@gmail.com>
2014-09-01 21:28:57 -0700

    updated copyright

--- src/vm/posix_main.cpp
+++ src/vm/posix_main.cpp
@@ -1,7 +1,7 @@
 /***************************************************************************
  * Starting point for the VM.
  *
- * Copyright (c) 2008-2010 Randy Hollines
+ * Copyright (c) 2008-2014 Randy Hollines
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
