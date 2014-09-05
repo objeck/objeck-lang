921df01ce2f5c1330b0df0ec155cfa4a495e4619
Randy Hollines <objeck@gmail.com>
2014-09-04 22:32:47 -0700

    working on setting support for ide demo

--- wip/ide/ide.cpp
+++ wip/ide/ide.cpp
@@ -77,8 +77,9 @@ END_EVENT_TABLE()
 MyFrame::MyFrame(wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style) : 
     wxFrame(parent, id, title, pos, size, style) 
 {
+  m_iniManager = new InIManager(wxT("ide.ini"));
   m_newPageCount = 1;
-
+  
   // setup window manager
   aui_manager.SetManagedWindow(this);
   aui_manager.AddPane(CreateTreeCtrl(), wxAuiPaneInfo().Left().PaneBorder(false));
@@ -96,19 +97,13 @@ MyFrame::MyFrame(wxWindow* parent, wxWindowID id, const wxString& title, const w
   
   // set tool bar
   aui_manager.AddPane(CreateToolBar(), wxAuiPaneInfo().
-    Name(wxT("toolbar")).Caption(wxT("Toolbar 3")).
-    ToolbarPane().Top().Row(1).Position(1));
-
-  // wxPersistenceManager& persistenceManager = wxPersistenceManager::Get();
-
+                      Name(wxT("toolbar")).Caption(wxT("Toolbar 3")).
+                      ToolbarPane().Top().Row(1).Position(1));
+  
+  // set global options dialog
   m_globalOptions = new GlobalOptions(this, 0);
   m_globalOptions->SetName("My Global Options");
-
-  bool flag = wxPersistenceManager::Get().RegisterAndRestore(m_globalOptions);
-  if(flag) {
-
-  }
-
+  
   // update
   m_notebook->SetFocus();
   aui_manager.Update();
@@ -116,6 +111,10 @@ MyFrame::MyFrame(wxWindow* parent, wxWindowID id, const wxString& title, const w
 
 MyFrame::~MyFrame() 
 {
+  if(m_iniManager) {
+    delete m_iniManager;
+    m_iniManager = NULL;
+  }
   aui_manager.UnInit();
 }
 
@@ -215,7 +214,6 @@ void MyFrame::OnFileClose(wxCommandEvent &WXUNUSED(event))
 void MyFrame::OnOptions(wxCommandEvent &WXUNUSED(event))
 {
   m_globalOptions->ShowModal();
-  wxPersistenceManager::Get().Save(m_globalOptions);
 }
 
 wxMenuBar* MyFrame::CreateMenuBar()
@@ -273,7 +271,7 @@ wxMenuBar* MyFrame::CreateMenuBar()
   return menu_bar;
 }
 
-wxAuiToolBar* MyFrame::CreateToolBar()
+wxAuiToolBar* MyFrame::DoCreateToolBar()
 {
   wxAuiToolBarItemArray prepend_items;
   wxAuiToolBarItemArray append_items;
@@ -391,12 +389,266 @@ wxAuiNotebook* MyFrame::CreateInfoCtrl()
 }
 
 //----------------------------------------------------------------------------
+// InIManager
+//----------------------------------------------------------------------------
+/******************************
+ * Load file into memory
+ ******************************/
+wstring InIManager::LoadFile(wstring filename) {
+  char* buffer;
+
+  string fn(filename.begin(), filename.end());
+  ifstream in(fn.c_str(), ios_base::in | ios_base::binary | ios_base::ate);
+  if(in.good()) {
+    // get file size
+    in.seekg(0, ios::end);
+    size_t buffer_size = (size_t)in.tellg();
+    in.seekg(0, ios::beg);
+    buffer = (char*)calloc(buffer_size + 1, sizeof(char));
+    in.read(buffer, buffer_size);
+    // close file
+    in.close();
+  }
+  else {
+    wcerr << L"Unable to read file: " << filename << endl;
+    exit(1);
+  }  
+  wstring out = BytesToUnicode(buffer);
+    
+  free(buffer);
+  return out;
+}
+ 
+/******************************
+ * Write file
+ ******************************/
+bool InIManager::WriteFile(const wstring &filename, const wstring &output) {
+  string fn(filename.begin(), filename.end());
+  ofstream out(fn.c_str(), ios_base::out | ios_base::binary);
+  if(out.good()) {
+    const string bytes = UnicodeToBytes(output);
+    out.write(bytes.c_str(), bytes.size());      
+    // close file
+    out.close();
+    return true;
+  }
+  else {
+    wcerr << L"Unable to write file: " << filename << endl;
+    exit(1);
+  }  
+    
+  return false;
+}
+ 
+/******************************
+ * Next parse token
+ ******************************/
+void InIManager::NextChar() {
+  if(cur_pos < input.size()) {
+    cur_char = input[cur_pos++];
+    if(cur_pos < input.size()) {
+      next_char = input[cur_pos];
+    }
+    else {
+      next_char = L'\0';
+    }
+  }
+  else {
+    cur_char = next_char = L'\0';
+  }
+}
+  
+/******************************
+ * Clear sections and names/values
+ ******************************/
+void InIManager::Clear() {
+  map<const wstring, map<const wstring, const wstring>*>::iterator iter;
+  for(iter = section_map.begin(); iter != section_map.end(); ++iter) {
+    map<const wstring, const wstring>* value_map = iter->second;
+    value_map->clear();
+    // free map
+    delete value_map;
+    value_map = NULL;
+  }
+
+  if(!section_map.empty()) {
+    section_map.clear();
+  }
+}
+  
+/******************************
+ * Serializes internal structures 
+ ******************************/
+wstring InIManager::Serialize() {
+  wstring out;
+  // sections
+  map<const wstring, map<const wstring, const wstring>*>::iterator section_iter;
+  for(section_iter = section_map.begin(); section_iter != section_map.end(); ++section_iter) {
+    out += L"[";
+    out += section_iter->first;
+    out += L"]\r\n";
+    // name/value pairs
+    map<const wstring, const wstring>::iterator value_iter;
+    for(value_iter = section_iter->second->begin(); value_iter != section_iter->second->end(); ++value_iter) {
+      out += value_iter->first;
+      out += L"=";
+      out += value_iter->second;
+      out += L"\r\n";
+    }
+  }
+
+  return out;
+}
+
+/******************************
+ * Parses setions and name/value
+ * pairs and loads internal 
+ * structures 
+ ******************************/
+void InIManager::Deserialize() {
+  map<const wstring, const wstring>* value_map = NULL;
+    
+  NextChar();    
+  while(cur_char != L'\0') {
+    // ignore white space
+    while(cur_char == L' ' || cur_char == L'\t' || cur_char == L'\r' || cur_char == L'\n') {
+      NextChar();
+    }
+      
+    // parse section
+    size_t start;
+    if(cur_char == L'[') {
+      start = cur_pos;
+      while(cur_pos < input.size() && iswprint(cur_char) && cur_char != L']') {
+        NextChar();
+      }
+      const wstring section = input.substr(start, cur_pos - start - 1);
+      if(cur_char == L']') {
+        NextChar();
+      }        
+      value_map = new map<const wstring, const wstring>;
+      section_map.insert(pair<const wstring, map<const wstring, const wstring>*>(section, value_map));
+    }
+    // comment
+    else if(cur_char == L'#') {
+      while(cur_pos < input.size() && cur_char != L'\r' && cur_char != L'\n') {
+        NextChar();
+      }
+    }
+    // key/value
+    else if(iswalpha(cur_char)) {
+      start = cur_pos - 1;
+      while(cur_pos < input.size() && iswprint(cur_char) && cur_char != L'=') {
+        NextChar();
+      }
+      const wstring key = input.substr(start, cur_pos - start - 1);
+      NextChar();
+        
+      wstring value;
+      start = cur_pos - 1;
+      while(cur_pos < input.size() && iswprint(cur_char) && cur_char != L'\r' && cur_char != L'\n') {
+        if(cur_char == L'\\') {
+          switch(next_char) {
+          case L'n':
+            value += L'\n';
+            NextChar();
+            break;
+          case L'r':
+            value += L'\r';
+            NextChar();
+            break;
+          default:
+            value += L'\\';
+            break;
+          }
+        }
+        else {
+          value += cur_char;
+        }
+          
+        NextChar();
+      }
+      
+      // add key/value pair
+      if(value_map) {
+        value_map->insert(pair<const wstring, const wstring>(key, value));
+      }
+    } 
+  }
+}
+
+/******************************
+ * Constructor/deconstructor
+ ******************************/
+InIManager::InIManager(const wstring &f) {
+  filename = f;
+  cur_char = next_char = L'\0';
+  cur_pos = 0;
+
+  Read();
+}
+  
+InIManager::~InIManager() {
+  Write();
+  Clear();
+}
+  
+/******************************
+ * Fetch value per section and key
+ ******************************/
+wstring InIManager::GetValue(const wstring &sec, const wstring &key) {
+  map<const wstring, map<const wstring, const wstring>*>::iterator section = section_map.find(sec);
+  if(section != section_map.end()) {
+    map<const wstring, const wstring>::iterator value = section->second->find(key);
+    if(value != section->second->end()) {
+      return value->second;
+    }
+  }
+    
+  return L"";
+}
+  
+/******************************
+ * Fetch value per section and key
+ ******************************/
+void InIManager::SetValue(const wstring &sec, const wstring &key, const wstring &value) {
+  map<const wstring, map<const wstring, const wstring>*>::iterator section = section_map.find(sec);
+  if(section != section_map.end()) {
+    section->second->insert(pair<const wstring, const wstring>(key, value));      
+  }
+}
+  
+/******************************
+ * Write contentes of memory
+ * to file
+ ******************************/
+void InIManager::Read() {
+  Clear();
+    
+  input = LoadFile(filename);
+  if(input.size() > 0) {
+    Deserialize();
+  }
+}
+
+/******************************
+ * Write contentes of memory
+ * to file
+ ******************************/
+void InIManager::Write() {
+  const wstring output = Serialize();
+  if(output.size() > 0) {
+    WriteFile(filename, output);
+  }
+}
+
+//----------------------------------------------------------------------------
 // GlobalOptions
 //----------------------------------------------------------------------------
 
-GlobalOptions::GlobalOptions(wxWindow* parent, long style) :
+GlobalOptions::GlobalOptions(wxWindow* parent, InIManager* ini, long style) :
   wxDialog(parent, wxID_ANY, wxT("Settings"), wxDefaultPosition, wxDefaultSize, style | wxDEFAULT_DIALOG_STYLE | wxRESIZE_BORDER) {
-
+  m_IniManager = ini;
   SetSizeHints(wxDefaultSize, wxDefaultSize);
 
   wxBoxSizer* bSizer1 = new wxBoxSizer(wxVERTICAL);
@@ -406,16 +658,15 @@ GlobalOptions::GlobalOptions(wxWindow* parent, long style) :
   wxStaticText* staticText4 = new wxStaticText(this, wxID_ANY, wxT("Objeck Path"), wxDefaultPosition, wxDefaultSize, 0);
   staticText4->Wrap(-1);
   bSizer3->Add(staticText4, 0, wxALL, 5);
-
-  m_textCtrl4 = new wxTextCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0);
+  
+  wxString path_string = m_IniManager->GetValue(wxT("Options"), wxT("path"));
+  m_textCtrl4 = new wxTextCtrl(this, wxID_ANY, path_string, wxDefaultPosition, wxDefaultSize, 0);
   bSizer3->Add(m_textCtrl4, 1, wxALL, 5);
 
   m_pathButton = new wxButton(this, wxID_ANY, wxT("..."), wxDefaultPosition, wxDefaultSize, wxBU_EXACTFIT);
   bSizer3->Add(m_pathButton, 0, wxALL, 5);
-
-
+  
   bSizer1->Add(bSizer3, 0, wxEXPAND, 5);
-
   wxStaticBoxSizer* sbSizer1 = new wxStaticBoxSizer(new wxStaticBox(this, wxID_ANY, wxT("Editor")), wxVERTICAL);
 
   wxFlexGridSizer* fgSizer1;
@@ -488,12 +739,19 @@ GlobalOptions::GlobalOptions(wxWindow* parent, long style) :
   m_sdbSizer1->Realize();
 
   bSizer1->Add(m_sdbSizer1, 1, wxEXPAND, 5);
-
-
+  
   SetSizer(bSizer1);
   Layout();
 
   Centre(wxBOTH);
+}
+
+void GlobalOptions::DoShow() {
+  ShowModal();
+  
+  // write out values
+  const wstring path_string = m_textCtrl4->GetValue().ToStdWstring();
+  m_IniManager->SetValue(wxT("Options"), wxT("path"), path_string);
 }
 
 
