53438180a56fd5f4bbaee8cc4abf101da90fa921
Randy Hollines <objeck@gmail.com>
2014-09-08 12:22:09 -0700

    working on project support

--- wip/ide/config/stc_sync.sh
+++ wip/ide/config/stc_sync.sh
@@ -2,7 +2,7 @@ CONFIG_DIR=~/Documents/Code/objeck-lang/wip/ide/config
 WXWIDGETS_ROOT=~/Documents/Code/wxWidgets-3.0.1
 
 if [ ! -z "$1" ]  && [ "$1" = "deploy" ]; then
-	cp Makefile $WXWIDGETS_ROOT
+#	cp Makefile $WXWIDGETS_ROOT
 	cp Catalogue.cxx $WXWIDGETS_ROOT/src/stc/scintilla/src/
 	cp LexObjeck.cxx $WXWIDGETS_ROOT/src/stc/scintilla/lexers
 	cp edit.h $WXWIDGETS_ROOT/samples/stc
