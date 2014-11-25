4e404414c341a676d142910e804bc9ad448cd508
Randy Hollines <objeck@objeck.org>
2014-11-25 12:04:18 -0800

    getting ready for release

--- src/objeck/code_doc.cmd
+++ src/objeck/code_doc.cmd
@@ -1,8 +1,12 @@
 cd deploy\bin
-rmdir /s /q ..\html
 mkdir ..\html
 copy ..\examples\doc\templates\index.html ..\html
-obc -src ..\examples\doc\doc_html.obs,..\examples\doc\doc_parser.obs -lib collect.obl -dest ..\code_doc.obe
-obr ..\code_doc.obe ..\examples\doc\templates ..\..\..\compiler\lib_src\lang.obs ..\..\..\compiler\lib_src\collect.obs ..\..\..\compiler\lib_src\regex.obs ..\..\..\compiler\lib_src\json.obs
-explorer ..\html\index.html
+obc -src ..\examples\doc\doc_html.obs,..\examples\doc\doc_parser.obs -lib collect.obl -dest ..\..\code_doc.obe
+obr ..\..\code_doc.obe ..\examples\doc\templates ..\..\..\compiler\lib_src\lang.obs ..\..\..\compiler\lib_src\collect.obs ..\..\..\compiler\lib_src\regex.obs ..\..\..\compiler\lib_src\json.obs
+REM explorer ..\html\index.html
+rmdir /s /q ..\doc\api
+mkdir ..\doc\api
+xcopy /e ..\html\* ..\doc\api
+rmdir /s /q ..\html
+explorer ..\doc\api\index.html
 cd ..\..
