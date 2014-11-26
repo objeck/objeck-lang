4bddbdcbc3d44232b28be8cf24dbbcf305a382e8
Randy Hollines <objeck@objeck.org>
2014-11-25 16:43:38 -0800

    getting ready for release

--- src/objeck/code_doc.cmd
+++ src/objeck/code_doc.cmd
@@ -3,7 +3,6 @@ mkdir ..\html
 copy ..\examples\doc\templates\index.html ..\html
 obc -src ..\examples\doc\doc_html.obs,..\examples\doc\doc_parser.obs -lib collect.obl -dest ..\..\code_doc.obe
 obr ..\..\code_doc.obe ..\examples\doc\templates ..\..\..\compiler\lib_src\lang.obs ..\..\..\compiler\lib_src\collect.obs ..\..\..\compiler\lib_src\regex.obs ..\..\..\compiler\lib_src\json.obs
-REM explorer ..\html\index.html
 rmdir /s /q ..\doc\api
 mkdir ..\doc\api
 xcopy /e ..\html\* ..\doc\api
