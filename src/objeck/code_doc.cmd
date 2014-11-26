fd583283c39acb04fe7f231b5c1e5cdc39920af6
Randy Hollines <objeck@objeck.org>
2014-11-25 18:01:27 -0800

    getting ready for release

--- src/objeck/code_doc.cmd
+++ src/objeck/code_doc.cmd
@@ -1,14 +1,15 @@
+set ZIP_BIN="C:\Program Files\7-Zip"
+
 cd deploy\bin
+rmdir /s /q ..\html
 mkdir ..\html
-copy ..\examples\doc\templates\index.html ..\html
 obc -src ..\examples\doc\doc_html.obs,..\examples\doc\doc_parser.obs -lib collect.obl -dest ..\..\code_doc.obe
-obr ..\..\code_doc.obe ..\examples\doc\templates ..\..\..\compiler\lib_src\lang.obs ..\..\..\compiler\lib_src\collect.obs ..\..\..\compiler\lib_src\regex.obs ..\..\..\compiler\lib_src\json.obs
+obr ..\..\code_doc.obe ..\examples\doc\templates ..\..\..\compiler\lib_src\lang.obs ..\..\..\compiler\lib_src\collect.obs ..\..\..\compiler\lib_src\regex.obs ..\..\..\compiler\lib_src\json.obs ..\..\..\compiler\lib_src\xml.obs ..\..\..\compiler\lib_src\encrypt.obs
 rmdir /s /q ..\doc\api
 mkdir ..\doc\api
+copy ..\..\..\compiler\rc\doc\templates\index.html ..\doc\api
 xcopy /e ..\html\* ..\doc\api
 rmdir /s /q ..\html
-explorer ..\doc\api\index.html
-cd ..\doc\
-"C:\Program Files\7-Zip\7z.exe" a -tzip api "api\*.html"
+%ZIP_BIN%\7z.exe a -tzip api "..\doc\api\*.html"
 copy api.zip ..\..\..\..\docs
 cd ..\..
