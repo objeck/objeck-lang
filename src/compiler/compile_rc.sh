69ca5604516fbb39599fa24ecca0a2ee94ecd4ee
Randy Hollines <objeck@gmail.com>
2014-11-27 13:50:28 -0800

    getting ready for release

--- src/compiler/compile_rc.sh
+++ src/compiler/compile_rc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh  
-for i in rc/*.obs  
+for i in $1/*.obs  
 	do  
-	./obc -src "$i" -lib encrypt.obl,collect.obl,regex.obl,xml.obl,json.obl -dest a.obe 
+	./obc -src "$i" -lib odbc.obl,encrypt.obl,collect.obl,regex.obl,xml.obl,json.obl -dest a.obe 
 done  
