f6ba6e8747b1c617dd0cad50f77b1bb364138acb
Randy Hollines <objeck@objeck.org>
2015-01-18 20:38:51 -0800

    better version support for windows

--- src/objeck/update_version.ps1
+++ src/objeck/update_version.ps1
@@ -1,5 +1,10 @@
 $version = "3.3.5.2"
 
 $version_number = $version.Replace(".", "")
+$version_windows = $version.Replace(".", ",")
 (Get-Content ..\..\debian\build.in) | ForEach-Object { $_ -replace "@VERSION@", $version } | Set-Content ..\..\debian\build.sh
 (Get-Content ..\shared\version.in) | ForEach-Object { $_ -replace "@VERSION@", $version } | ForEach-Object { $_ -replace "@VERSION_NUMBER@", $version_number } | Set-Content ..\shared\version.h
+(Get-Content ..\compiler\compiler\objeck.in) | ForEach-Object { $_ -replace "@VERSION@", $version } | ForEach-Object { $_ -replace "@VERSION_WINDOWS@", $version_windows } | Set-Content ..\compiler\compiler\objeck.rc
+(Get-Content ..\vm\vm\objeck.in) | ForEach-Object { $_ -replace "@VERSION@", $version } | ForEach-Object { $_ -replace "@VERSION_WINDOWS@", $version_windows } | Set-Content ..\vm\vm\objeck.rc
+(Get-Content ..\utilities\utilities\objeck.in) | ForEach-Object { $_ -replace "@VERSION@", $version } | ForEach-Object { $_ -replace "@VERSION_WINDOWS@", $version_windows } | Set-Content ..\utilities\utilities\objeck.rc
+(Get-Content ..\vm\debugger\debugger\objeck.in) | ForEach-Object { $_ -replace "@VERSION@", $version } | ForEach-Object { $_ -replace "@VERSION_WINDOWS@", $version_windows } | Set-Content ..\vm\debugger\debugger\objeck.rc
