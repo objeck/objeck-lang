2603ccb675ba7732ea11314a68f8f2a7f9e45f58
Randy Hollines <objeck@objeck.org>
2015-01-18 22:38:12 -0800

    better version support for posix

--- src/objeck/update_version.ps1
+++ src/objeck/update_version.ps1
@@ -1,10 +1,19 @@
+# version string
 $version = "3.3.5.2"
 
+# alternative strings
 $version_number = $version.Replace(".", "")
+$version_posix = $version -replace "\.\d+$", ("-" + ($version.SubString($version.LastIndexOf(".") + 1)))
 $version_windows = $version.Replace(".", ",")
-(Get-Content ..\..\debian\build.in) | ForEach-Object { $_ -replace "@VERSION@", $version } | Set-Content ..\..\debian\build.sh
+
+# update source version header
 (Get-Content ..\shared\version.in) | ForEach-Object { $_ -replace "@VERSION@", $version } | ForEach-Object { $_ -replace "@VERSION_NUMBER@", $version_number } | Set-Content ..\shared\version.h
+
+# update debain build script
+(Get-Content ..\..\debian\build.in) | ForEach-Object { $_ -replace "@VERSION@", $version_posix } | Set-Content ..\..\debian\build.sh
+
+# update window resource files
 (Get-Content ..\compiler\compiler\objeck.in) | ForEach-Object { $_ -replace "@VERSION@", $version } | ForEach-Object { $_ -replace "@VERSION_WINDOWS@", $version_windows } | Set-Content ..\compiler\compiler\objeck.rc
 (Get-Content ..\vm\vm\objeck.in) | ForEach-Object { $_ -replace "@VERSION@", $version } | ForEach-Object { $_ -replace "@VERSION_WINDOWS@", $version_windows } | Set-Content ..\vm\vm\objeck.rc
-(Get-Content ..\utilities\utilities\objeck.in) | ForEach-Object { $_ -replace "@VERSION@", $version } | ForEach-Object { $_ -replace "@VERSION_WINDOWS@", $version_windows } | Set-Content ..\utilities\utilities\objeck.rc
 (Get-Content ..\vm\debugger\debugger\objeck.in) | ForEach-Object { $_ -replace "@VERSION@", $version } | ForEach-Object { $_ -replace "@VERSION_WINDOWS@", $version_windows } | Set-Content ..\vm\debugger\debugger\objeck.rc
+(Get-Content ..\utilities\utilities\objeck.in) | ForEach-Object { $_ -replace "@VERSION@", $version } | ForEach-Object { $_ -replace "@VERSION_WINDOWS@", $version_windows } | Set-Content ..\utilities\utilities\objeck.rc
