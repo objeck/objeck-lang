4186d82a1ab3f5fa36446f8ed8eee06cc689ca8c
Randy Hollines <objeck@gmail.com>
2015-01-20 16:17:33 -0800

    v3.3.5-2

--- src/objeck/update_version.ps1
+++ src/objeck/update_version.ps1
@@ -13,10 +13,10 @@ $version_windows = $version.Replace(".", ",")
 # update debain build files
 (Get-Content ..\..\debian\build.in) | ForEach-Object { $_ -replace "@VERSION@", $version_posix } | Set-Content ..\..\debian\build.sh
 (Get-Content ..\..\debian\files\changelog.in) | ForEach-Object { $_ -replace "@VERSION_LONG@", $version_posix_long } | Set-Content ..\..\debian\files\changelog
-(Get-Content ..\..\doc\man\obc.in) | ForEach-Object { $_ -replace "@VERSION@", $version_posix_long } | Set-Content ..\..\doc\man\obc.1
-(Get-Content ..\..\doc\man\obr.in) | ForEach-Object { $_ -replace "@VERSION@", $version_posix_long } | Set-Content ..\..\doc\man\obr.1
-(Get-Content ..\..\doc\man\obd.in) | ForEach-Object { $_ -replace "@VERSION@", $version_posix_long } | Set-Content ..\..\doc\man\obd.1
-(Get-Content ..\..\doc\man\obu.in) | ForEach-Object { $_ -replace "@VERSION@", $version_posix_long } | Set-Content ..\..\doc\man\obu.1
+(Get-Content ..\..\docs\man\obc.in) | ForEach-Object { $_ -replace "@VERSION@", $version_posix_long } | Set-Content ..\..\docs\man\obc.1
+(Get-Content ..\..\docs\man\obr.in) | ForEach-Object { $_ -replace "@VERSION@", $version_posix_long } | Set-Content ..\..\docs\man\obr.1
+(Get-Content ..\..\docs\man\obd.in) | ForEach-Object { $_ -replace "@VERSION@", $version_posix_long } | Set-Content ..\..\docs\man\obd.1
+(Get-Content ..\..\docs\man\obu.in) | ForEach-Object { $_ -replace "@VERSION@", $version_posix_long } | Set-Content ..\..\docs\man\obu.1
 
 # update window resource files
 (Get-Content ..\compiler\compiler\objeck.in) | ForEach-Object { $_ -replace "@VERSION@", $version } | ForEach-Object { $_ -replace "@VERSION_WINDOWS@", $version_windows } | Set-Content ..\compiler\compiler\objeck.rc
