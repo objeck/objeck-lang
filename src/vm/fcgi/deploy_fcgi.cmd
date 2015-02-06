d0c42787224d389418db5ac2e173d42764fbbc73
Randy Hollines <objeck@objeck.org>
2015-02-05 20:56:13 -0800

    code clean up

--- src/vm/fcgi/deploy_fcgi.cmd
+++ src/vm/fcgi/deploy_fcgi.cmd
@@ -1,5 +1,7 @@
 echo off
 
+set VS_ROOT="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC"
+
 rmdir /s /q deploy
 mkdir deploy
 mkdir deploy\bin
@@ -8,7 +10,7 @@ mkdir deploy\lib\objeck-lang
 if "%1" == "debug" (
 	copy Debug\obr_fcgi.exe deploy\bin
 	copy Debug\*.pdb deploy\bin
-	copy "D:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\Debug_NonRedist\x86\Microsoft.VC120.DebugCRT\*.dll" deploy\bin
+	copy %VS_ROOT%\redist\Debug_NonRedist\x86\Microsoft.VC120.DebugCRT\*.dll deploy\bin
 	copy Debug\fcgi_lib.dll deploy\lib\objeck-lang
 ) else (
 	copy Release\obr_fcgi.exe deploy\bin
