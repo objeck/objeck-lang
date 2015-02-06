05a2e72618dc7a0fed1fcc2fbe01e706af8035c7
Randy Hollines <objeck@gmail.com>
2015-02-06 10:33:47 -0800

    code for fastcgi

--- src/vm/fcgi/deploy_fcgi.cmd
+++ src/vm/fcgi/deploy_fcgi.cmd
@@ -9,12 +9,14 @@ mkdir deploy\lib\objeck-lang
 
 if "%1" == "debug" (
 	copy Debug\obr_fcgi.exe deploy\bin
+	copy Debug\fcgi_lib.dll deploy\lib\objeck-lang
 	copy Debug\*.pdb deploy\bin
 	copy %VS_ROOT%\redist\Debug_NonRedist\x86\Microsoft.VC120.DebugCRT\*.dll deploy\bin
-	copy Debug\fcgi_lib.dll deploy\lib\objeck-lang
 ) else (
 	copy Release\obr_fcgi.exe deploy\bin
 	copy Release\fcgi_lib.dll deploy\lib\objeck-lang
+	copy %VS_ROOT%\redist\x86\Microsoft.VC120.CRT\*.dll deploy\bin	
+	REM copy Release\*.pdb deploy\bin
 )
 
 copy ..\..\compiler\*.obl deploy\bin
