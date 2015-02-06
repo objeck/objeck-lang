9cd34c2f51480e0d7aa46894f5246bddaa422a5b
Randy Hollines <objeck@gmail.com>
2015-02-06 08:25:00 -0800

    code refactor

--- src/vm/fcgi_main.cpp
+++ src/vm/fcgi_main.cpp
@@ -103,11 +103,11 @@ int main(const int argc, const char* argv[])
   FCGX_Stream*in; FCGX_Stream* out; FCGX_Stream* err;
   FCGX_ParamArray envp;
 
-  while(mthd && (FCGX_Accept(&in, &out, &err, &envp) >= 0)) {
-    // execute method
-    long* op_stack = new long[CALC_STACK_SIZE];
-    long* stack_pos = new long;
+  // execute method
+  long* op_stack = new long[CALC_STACK_SIZE];
+  long* stack_pos = new long;
 
+  while(mthd && (FCGX_Accept(&in, &out, &err, &envp) >= 0)) {
     // create request and response
     long* req_obj = MemoryManager::AllocateObject(L"FastCgi.Request",  op_stack, *stack_pos, false);
     long* res_obj = MemoryManager::AllocateObject(L"FastCgi.Response", op_stack, *stack_pos, false);
@@ -128,7 +128,7 @@ int main(const int argc, const char* argv[])
       intpr.Execute((long*)op_stack, (long*)stack_pos, 0, mthd, NULL, false);
     }
     else {
-      wcerr << L">>> DLL call: Unable to allocate FastCgi.Request or FastCgi.Response <<<" << endl;
+      wcerr << L">>> Shared library call: Unable to allocate FastCgi.Request or FastCgi.Response <<<" << endl;
       return 1;
     }
 
@@ -141,18 +141,18 @@ int main(const int argc, const char* argv[])
     }
 #endif
 
-    // clean up
-    delete[] op_stack;
-    op_stack = NULL;
-
-    delete stack_pos;
-    stack_pos = NULL;
-
 #ifdef _DEBUG
     PrintEnv(out, "Request environment", envp);
     PrintEnv(out, "Initial environment", environ);
 #endif
   }
+
+  // clean up
+  delete[] op_stack;
+  op_stack = NULL;
+
+  delete stack_pos;
+  stack_pos = NULL;
 
   return 0;
 }
@@ -166,362 +166,4 @@ void PrintEnv(FCGX_Stream* out, const char* label, char** envp)
   for(; *envp != NULL; envp++) {
     wcout << L"\t" << BytesToUnicode(*envp) << endl;
   }
-}
-
-
-/******************************
- * Load file into memory
- ******************************/
-wstring IniManager::LoadFile(const wstring &fn) {
-  char* buffer;
-
-  string file(fn.begin(), fn.end());
-  ifstream in(file.c_str(), ios_base::in | ios_base::binary | ios_base::ate);
-  if (in.good()) {
-    // get file size
-    in.seekg(0, ios::end);
-    size_t buffer_size = (size_t)in.tellg();
-    in.seekg(0, ios::beg);
-    buffer = (char*)calloc(buffer_size + 1, sizeof(char));
-    in.read(buffer, buffer_size);
-    // close file
-    in.close();
-  }
-  else {
-    wcerr << L"Unable to read file: " << fn << endl;
-    exit(1);
-  }
-  wstring out = BytesToUnicode(buffer);
-
-  free(buffer);
-  return out;
-}
-
-/******************************
- * Write file
- ******************************/
-bool IniManager::WriteFile(const wstring &fn, const wstring &buffer) {
-  string file(fn.begin(), fn.end());
-  ofstream out(file.c_str(), ios_base::out | ios_base::binary);
-  if (out.good()) {
-    const string bytes = UnicodeToBytes(buffer);
-    out.write(bytes.c_str(), bytes.size());
-    // close file
-    out.close();
-    return true;
-  }
-  else {
-    wcerr << L"Unable to write file: " << fn << endl;
-    exit(1);
-  }
-
-  return false;
-}
-
-/******************************
- * Next parse token
- ******************************/
-void IniManager::NextChar() {
-  if (cur_pos < input.size()) {
-    cur_char = input[cur_pos++];
-    if (cur_pos < input.size()) {
-      next_char = input[cur_pos];
-    }
-    else {
-      next_char = L'\0';
-    }
-  }
-  else {
-    cur_char = next_char = L'\0';
-  }
-}
-
-/******************************
- * Clear sections and names/values
- ******************************/
-void IniManager::Clear() {
-  map<const wstring, map<const wstring, wstring>*>::iterator iter;
-  for (iter = section_map.begin(); iter != section_map.end(); ++iter) {
-    map<const wstring, wstring>* value_map = iter->second;
-    value_map->clear();
-    // free map
-    delete value_map;
-    value_map = NULL;
-  }
-
-  if (!section_map.empty()) {
-    section_map.clear();
-  }
-  cur_pos = 0;
-}
-
-/******************************
- * Serializes internal structures
- ******************************/
-wstring IniManager::Serialize() {
-  wstring out;
-  // sections
-  map<const wstring, map<const wstring, wstring>*>::iterator section_iter;
-  for (section_iter = section_map.begin(); section_iter != section_map.end(); ++section_iter) {
-    out += L"[";
-    out += section_iter->first;
-    out += L"]\r\n";
-    // name/value pairs
-    map<const wstring, wstring>::iterator value_iter;
-    for (value_iter = section_iter->second->begin(); value_iter != section_iter->second->end(); ++value_iter) {
-      out += value_iter->first;
-      out += L"=";
-      out += value_iter->second;
-      out += L"\r\n";
-    }
-  }
-
-  return out;
-}
-
-/******************************
- * Parses setions and name/value
- * pairs and loads internal
- * structures
- ******************************/
-void IniManager::Deserialize() {
-  map<const wstring, wstring>* value_map = NULL;
-
-  NextChar();
-  while (cur_char != L'\0') {
-    // ignore white space
-    while (cur_char == L' ' || cur_char == L'\t' || cur_char == L'\r' || cur_char == L'\n') {
-      NextChar();
-    }
-
-    // parse section
-    size_t start;
-    if (cur_char == L'[') {
-      start = cur_pos;
-      while (cur_pos < input.size() && iswprint(cur_char) && cur_char != L']') {
-        NextChar();
-      }
-      const wstring section = input.substr(start, cur_pos - start - 1);
-      if (cur_char == L']') {
-        NextChar();
-      }
-      value_map = new map<const wstring, wstring>;
-      section_map.insert(pair<const wstring, map<const wstring, wstring>*>(section, value_map));
-    }
-    // comment
-    else if (cur_char == L'#') {
-      while (cur_pos < input.size() && cur_char != L'\r' && cur_char != L'\n') {
-        NextChar();
-      }
-    }
-    // key/value
-    else if (iswalpha(cur_char)) {
-      start = cur_pos - 1;
-      while (cur_pos < input.size() && iswprint(cur_char) && cur_char != L'=') {
-        NextChar();
-      }
-      const wstring key = input.substr(start, cur_pos - start - 1);
-      NextChar();
-
-      wstring value;
-      start = cur_pos - 1;
-      while (cur_pos < input.size() && iswprint(cur_char) && cur_char != L'\r' && cur_char != L'\n') {
-        if (cur_char == L'\\') {
-          switch (next_char) {
-          case L'n':
-            value += L'\n';
-            NextChar();
-            break;
-          case L'r':
-            value += L'\r';
-            NextChar();
-            break;
-          default:
-            value += L'\\';
-            break;
-          }
-        }
-        else {
-          value += cur_char;
-        }
-
-        NextChar();
-      }
-
-      // add key/value pair
-      if (value_map) {
-        value_map->insert(pair<const wstring, wstring>(key, value));
-      }
-    }
-  }
-}
-
-/******************************
- * Constructor/deconstructor
- ******************************/
-IniManager::IniManager(const wstring &fn)
-{
-  filename = fn;
-  cur_char = next_char = L'\0';
-  cur_pos = 0;
-  locked = false;
-
-  Load();
-}
-
-IniManager::~IniManager() {
-  Save();
-  Clear();
-}
-
-/******************************
- * Fetch value per section and key
- ******************************/
-wstring IniManager::GetValue(const wstring &sec, const wstring &key) {
-  if(locked) {
-    return L"";
-  }
-  
-  locked = true;
-  map<const wstring, map<const wstring, wstring>*>::iterator section = section_map.find(sec);
-  if (section != section_map.end()) {
-    map<const wstring, wstring>::iterator value = section->second->find(key);
-    if (value != section->second->end()) {
-      locked = false;
-      return value->second;
-    }
-  }
-
-  locked = false;
-  return L"";
-}
-
-/******************************
- * Fetch value per section and key
- ******************************/
-bool IniManager::SetValue(const wstring &sec, const wstring &key, const wstring &value) {
-  if(locked) {
-    return false;
-  }
-
-  locked = true;
-  map<const wstring, map<const wstring, wstring>*>::iterator section = section_map.find(sec);
-  if (section != section_map.end()) {
-    (*section->second)[key] = value;
-    locked = false;
-    return true;
-  }
-
-  locked = false;
-  return false;
-}
-
-vector<wstring> IniManager::GetListValues(const wstring &sec, const wstring &key)
-{
-  vector<wstring> values;  
-  
-  const wstring raw_value = GetValue(sec, key);
-  Tokenize(raw_value, values, L";");
-
-  return values;
-}
-
-bool IniManager::AddListValue(const wstring &sec, const wstring &key, const wstring &val)
-{
-  // put values into a set
-  set<wstring> values_set;
-  vector<wstring> list_values = GetListValues(sec, key);
-  for(size_t i  = 0; i < list_values.size(); ++i) {
-    const wstring value = list_values[i];
-    if(value.size() > 0) {
-      values_set.insert(value);
-    }
-  }
-
-  // check for existing entry
-  set<wstring>::iterator iter = values_set.find(val);
-  if(iter != values_set.end()) {
-    // found
-    return false;
-  }
-  else {
-    values_set.insert(val);
-  }
-  
-  // rebuilt and save value
-  wstring new_value;
-  for(iter = values_set.begin(); iter != values_set.end(); ++iter) {
-    new_value += *iter;
-    new_value += L";";
-  }
-  
-  return SetValue(sec, key, new_value);
-}
-
-bool IniManager::RemoveListValue(const wstring &sec, const wstring &key, const wstring &val)
-{
-  // put values into a set
-  set<wstring> values_set;
-  vector<wstring> list_values = GetListValues(sec, key);
-  for(size_t i  = 0; i < list_values.size(); ++i) {
-    const wstring value = list_values[i];
-    if(value.size() > 0) {
-      values_set.insert(value);
-    }
-  }
-  
-  // rebuilt and save value
-  wstring new_value;
-  for(set<wstring>::iterator iter = values_set.begin(); iter != values_set.end(); ++iter) {
-    const wstring list_value = *iter;
-    if(list_value != val) {
-      new_value += list_value;
-      new_value += L";";
-    }
-  }
-  
-  return SetValue(sec, key, new_value);
-}
-
-/******************************
- * Write contentes of memory
- * to file
- ******************************/
-bool IniManager::Load() {
-  if(locked) {
-    return false;
-  }
-  
-  locked = true;
-  Clear();
-  input = LoadFile(filename);
-  if (input.size() > 0) {
-    Deserialize();
-    locked = false;
-    return true;
-  }
-  
-  locked = false;
-  return false;
-}
-
-/******************************
- * Write contentes of memory
- * to file
- ******************************/
-bool IniManager::Save() {
-  if(locked) {
-    return false;
-  }
-  
-  locked = true;
-  const wstring out = Serialize();
-  if (out.size() > 0) {
-    bool wrote = WriteFile(filename, out);
-    locked = false;
-    return wrote;
-  }
-  
-  locked = false;
-  return false;
 }
