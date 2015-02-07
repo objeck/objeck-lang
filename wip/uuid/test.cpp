4b1d812885e89eb2c4944c8af3e70fdf992df763
Randy Hollines <objeck@gmail.com>
2015-02-06 23:18:17 -0800

    uuid code

--- wip/uuid/test.cpp
+++ wip/uuid/test.cpp
@@ -9,18 +9,40 @@ using namespace std;
 
 #ifdef _WIN32
 #include <windows.h>
+#pragma comment(lib, "Rpcrt4.lib")
 #else
 #include <uuid/uuid.h>
 #endif
 
-int main() {
-	uuid_t t;
-	uuid_generate(t);
+string CreateUUID() {  
+  uuid_t uuid;
+  string uuid_str;
 
-	char ch[36];
-	memset(ch, 0, 36);
-	uuid_unparse(t, ch);
-	string uuid(ch);
-  
-  cout << uuid.size() << ": " << uuid << endl;
+#ifdef _WIN32
+  RPC_CSTR buffer = NULL;
+  CoCreateGuid(&uuid);
+  UuidToString(&uuid, &buffer);
+  for(int i = 0; i < 36; i++) {
+    if(buffer[i] != '-') {
+      uuid_str += buffer[i];
+    }
+  }
+  RpcStringFree(&buffer);
+#else
+  char buffer[36];
+  bzero(buffer, 36);
+  uuid_generate(uuid);
+  uuid_unparse(uuid, buffer);
+  for(int i = 0; i < 36; i++) {
+    if(buffer[i] != '-') {
+      uuid_str += buffer[i];
+    }
+  }
+#endif
+
+  return uuid_str;
+}
+
+int main() {
+  cout << CreateUUID() << endl;
 }
