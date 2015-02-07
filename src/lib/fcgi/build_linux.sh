1f398dd3e289aa6be73eff9046a91e43c62a07d5
Randy Hollines <objeck@objeck.org>
2015-02-07 08:31:12 -0800

    fastcgi support

--- src/lib/fcgi/build_linux.sh
+++ src/lib/fcgi/build_linux.sh
@@ -2,7 +2,7 @@
 rm -rf *.o
 rm -rf *.so
 g++ -g -D_DEBUG -Wall -fPIC -c *$1.cpp 
-g++ -g -shared -D_DEBUG -lfcgi -Wl,-soname,$1.so.1 -o $1.so *.o 
+g++ -g -shared -D_DEBUG -luuid -lfcgi -Wl,-soname,$1.so.1 -o $1.so *.o 
 # g++ -shared -fPIC $1.o $1.cpp 
 # g++ -fPIC -O3 -c -Wno-deprecated -Wall -D_X64 $1.cpp -o $1.so
 # g++ -shared -fPIC -O3 -c -Wno-deprecated -Wall $1.cpp -o $1.so
