66e580087ced176eba5d0d948ddbf6f2c5e30232
Randy Hollines <objeck@gmail.com>
2015-06-04 23:17:14 -0700

    adding fcgi to main build

--- src/objeck/deploy.cmd
+++ src/objeck/deploy.cmd
@@ -2,6 +2,7 @@ set LATEX_BIN="D:\Program Files\MiKTeX 2.9\miktex\bin\x64"
 
 rmdir /s /q deploy
 mkdir deploy
+mkdir fcgi_deploy
 
 REM update version information
 powershell.exe -executionpolicy remotesigned -file  update_version.ps1
@@ -12,6 +13,7 @@ copy Release\*.exe deploy\bin
 copy ..\compiler\*.obl deploy\bin
 del deploy\bin\fcgi.obl
 del deploy\bin\gtk2.obl
+del /q deploy\bin\a.*
 REM openssl support
 mkdir deploy\lib
 mkdir deploy\lib\objeck-lang
@@ -36,6 +38,10 @@ mkdir deploy\doc\syntax
 copy ..\..\docs\syntax\* deploy\doc\syntax
 copy ..\..\docs\readme.htm deploy
 call code_doc.cmd
+REM create and build fcgi
+xcopy /e deploy\* fcgi_deploy
+del fcgi_deploy\bin\obr.exe
+
 REM finished
 if [%1] NEQ [deploy] goto end
 	rmdir /s /q "%HOMEDRIVE%%HOMEPATH%\Desktop\objeck-lang"
