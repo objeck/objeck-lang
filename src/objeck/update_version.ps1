cf7c484e6b1cc8b371df2b378a7c18b40b069d67
Randy Hollines <objeck@objeck.org>
2015-09-24 05:24:16 -0700

    bumped version

--- src/objeck/update_version.ps1
+++ src/objeck/update_version.ps1
@@ -1,5 +1,5 @@
 # version string
-$version = "3.3.8.2"
+$version = "3.3.8.3"
 
 # alternative strings
 $version_number = $version.Replace(".", "")
--- src/shared/version.h
+++ src/shared/version.h
@@ -35,8 +35,8 @@
 #define MAGIC_NUM_WEB 0xdddf
 #define MAGIC_NUM_LIB 0xddde
 #define MAGIC_NUM_EXE 0xdddd
-#define VER_NUM 3382
+#define VER_NUM 3383
 
-#define VERSION_STRING L"3.3.8.2"
+#define VERSION_STRING L"3.3.8.3"
 
 #endif
