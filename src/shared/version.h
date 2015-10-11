7913cea6d19b2bf4697e8f14e9d2fbb314f1be28
Randy Hollines <objeck@objeck.org>
2015-10-11 02:51:56 -0700

    bumping version

--- src/shared/version.h
+++ src/shared/version.h
@@ -35,8 +35,8 @@
 #define MAGIC_NUM_WEB 0xdddf
 #define MAGIC_NUM_LIB 0xddde
 #define MAGIC_NUM_EXE 0xdddd
-#define VER_NUM 3383
+#define VER_NUM 3384
 
-#define VERSION_STRING L"3.3.8.3"
+#define VERSION_STRING L"3.3.8.4"
 
 #endif
