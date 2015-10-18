aed1040c53c6f8a8aebc20bc09b1a72321487ee0
Randy Hollines <objeck@objeck.org>
2015-10-18 09:07:50 -0700

    working on sdl support

--- src/objeck/sdl_debug.cmd
+++ src/objeck/sdl_debug.cmd
@@ -4,8 +4,8 @@ rmdir /s /q ..\vm\lib
 del ..\vm\*.dll
 
 cd deploy\bin
-obc -src 'C:\Users\Randy\Documents\Code\objeck-lang\src\compiler\lib_src\sdl.obs' -lib collect.obl -tar lib -dest sdl.obl
-obc -src 'C:\Users\Randy\Documents\Code\objeck-lang\src\compiler\test_src\sdl\sdl0.obs' -lib sdl.obl,collect.obl -dest ..\a.obe
+obc -src '..\..\..\compiler\lib_src\sdl.obs' -lib collect.obl -tar lib -dest sdl.obl
+obc -src '..\..\..\compiler\test_src\sdl\sdl0.obs' -lib sdl.obl,collect.obl -dest ..\sdl0.obe
 cd ..\..
 
 copy ..\lib\sdl\lib\x86\SDL2.dll deploy\bin
