f37619aac5810f7331404747be9ab7fb058e4bd9
Randy Hollines <objeck@objeck.org>
2015-10-25 15:43:44 -0700

    wip

--- src/objeck/sdl_code_gen.cmd
+++ src/objeck/sdl_code_gen.cmd
@@ -2,5 +2,5 @@
 
 cd deploy\bin
 obc -src ..\..\..\compiler\test_src\sdl\code_gen\sdl_scanner.obs,..\..\..\compiler\test_src\sdl\code_gen\sdl_parser.obs -opt s3 -lib collect.obl -dest ..\code_gen.obe
-obr ..\code_gen.obe D:\Temp\SDL2-2.0.3\include\SDL_surface.h
+obr ..\code_gen.obe D:\Temp\SDL2-2.0.3\include\SDL_video.h
 cd ..\..
