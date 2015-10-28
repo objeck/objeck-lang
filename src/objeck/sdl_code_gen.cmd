885d5c270b6cc71869b5625feeea586d44f15b40
Randy Hollines <objeck@objeck.org>
2015-10-28 14:35:51 -0700

    sdl code parser

--- src/objeck/sdl_code_gen.cmd
+++ src/objeck/sdl_code_gen.cmd
@@ -2,5 +2,5 @@
 
 cd deploy\bin
 obc -src ..\..\..\compiler\test_src\sdl\code_gen\sdl_scanner.obs,..\..\..\compiler\test_src\sdl\code_gen\sdl_parser.obs,..\..\..\compiler\test_src\sdl\code_gen\sdl_emitter.obs -opt s3 -lib collect.obl -dest ..\code_gen.obe
-obr ..\code_gen.obe SDL_Window D:\Temp\SDL2-2.0.3\include\SDL_video.h
+obr ..\code_gen.obe SDL_Window gen_tmpl.dat D:\Temp\SDL2-2.0.3\include\SDL_video.h
 cd ..\..
