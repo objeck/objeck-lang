5b0d193b33406223c53913b005fe037592b7e14b
Randy Hollines <objeck@objeck.org>
2015-11-02 20:39:35 -0800

    adding more code to the sdl code gen

--- src/objeck/sdl_code_gen.cmd
+++ src/objeck/sdl_code_gen.cmd
@@ -2,5 +2,5 @@
 
 cd deploy\bin
 obc -src ..\..\..\compiler\test_src\sdl\code_gen\sdl_scanner.obs,..\..\..\compiler\test_src\sdl\code_gen\sdl_parser.obs,..\..\..\compiler\test_src\sdl\code_gen\sdl_emitter.obs -opt s3 -lib collect.obl -dest ..\code_gen.obe
-obr ..\code_gen.obe SDL_Window gen_tmpl.dat ..\..\..\lib\sdl\include\SDL_video.h
+obr ..\code_gen.obe SDL_Window ..\..\..\lib\sdl\include\SDL_video.h
 cd ..\..
