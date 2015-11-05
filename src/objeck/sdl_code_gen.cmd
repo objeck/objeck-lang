e834ced700f2aa1a2f0501e76716e5a88294d5ac
Randy Hollines <objeck@objeck.org>
2015-11-05 09:14:47 -0800

    wip

--- src/objeck/sdl_code_gen.cmd
+++ src/objeck/sdl_code_gen.cmd
@@ -2,5 +2,5 @@
 
 cd deploy\bin
 obc -src ..\..\..\compiler\test_src\sdl\code_gen\sdl_scanner.obs,..\..\..\compiler\test_src\sdl\code_gen\sdl_parser.obs,..\..\..\compiler\test_src\sdl\code_gen\sdl_emitter.obs -opt s3 -lib collect.obl -dest ..\code_gen.obe
-REM obr ..\code_gen.obe Surface ..\..\..\lib\sdl\include\SDL_surface.h
+obr ..\code_gen.obe Surface ..\..\..\lib\sdl\include\SDL_surface.h
 cd ..\..
