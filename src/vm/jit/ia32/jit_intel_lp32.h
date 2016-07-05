839ff7af43c31822e0389c8d6bfff5775ee35d26
Randy Hollines <objeck@objeck.org>
2016-07-05 07:00:55 -0700

    code clean up

--- src/vm/jit/ia32/jit_intel_lp32.h
+++ src/vm/jit/ia32/jit_intel_lp32.h
@@ -1611,10 +1611,10 @@ namespace Runtime {
         unordered_map<int32_t, StackInstr*>::iterator iter;
         for(iter = jump_table.begin(); iter != jump_table.end(); ++iter) {
           StackInstr* instr = iter->second;
-          int32_t src_offset = iter->first;
-          int32_t dest_index = method->GetLabelIndex(instr->GetOperand()) + 1;
-          int32_t dest_offset = method->GetInstruction(dest_index)->GetOffset();
-          int32_t offset = dest_offset - src_offset - 4;
+          const int32_t src_offset = iter->first;
+		  const int32_t dest_index = method->GetLabelIndex(instr->GetOperand()) + 1;
+		  const int32_t dest_offset = method->GetInstruction(dest_index)->GetOffset();
+		  const int32_t offset = dest_offset - src_offset - 4;
           memcpy(&code[src_offset], &offset, 4); 
 #ifdef _DEBUG
           wcout << L"jump update: src=" << src_offset 
