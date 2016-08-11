08ee5b4094ffe4e4b1fa191941800b9221ddf499
Randy Hollines <objeck@objeck.org>
2016-08-11 07:53:52 -0700

    wip

--- src/shared/version.h
+++ src/shared/version.h
@@ -35,8 +35,8 @@
 #define MAGIC_NUM_WEB 0xdddf
 #define MAGIC_NUM_LIB 0xddde
 #define MAGIC_NUM_EXE 0xdddd
-#define VER_NUM 3314
+#define VER_NUM 340
 
-#define VERSION_STRING L"3.3.14"
+#define VERSION_STRING L"3.4.0"
 
 #endif
