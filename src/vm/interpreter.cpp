37c6ae4d615fee3655adf6e56e29061fa7d3ddc1
Randy Hollines <objeck@objeck.org>
2016-10-28 19:58:07 -0700

    v3.5.0

--- src/vm/interpreter.cpp
+++ src/vm/interpreter.cpp
@@ -1,7 +1,7 @@
 /***************************************************************************
  * VM stack machine.
  *
- * Copyright (c) 2008-2015, Randy Hollines
+ * Copyright (c) 2008-2016, Randy Hollines
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
