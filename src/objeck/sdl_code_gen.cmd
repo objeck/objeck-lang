67b8473c41ef10e2810fecea0b03f85246b4f447
Randy Hollines <objeck@objeck.org>
2016-12-24 16:26:33 -0700

    working on sdl

--- src/objeck/sdl_code_gen.cmd
+++ src/objeck/sdl_code_gen.cmd
@@ -3,5 +3,5 @@
 cd deploy\bin
 obc -src ..\..\..\compiler\lib_src\sdl.obs -lib collect.obl -tar lib -dest sdl.obl
 obc -src ..\..\..\compiler\test_src\sdl\code_gen\sdl_scanner.obs,..\..\..\compiler\test_src\sdl\code_gen\sdl_parser.obs,..\..\..\compiler\test_src\sdl\code_gen\sdl_emitter.obs -opt s2 -lib collect.obl -dest ..\code_gen.obe
-obr ..\code_gen.obe Surface ..\..\..\lib\sdl\include\SDL_surface.h
+obr ..\code_gen.obe Rect ..\..\..\lib\sdl\include\SDL_rect.h
 cd ..\..
