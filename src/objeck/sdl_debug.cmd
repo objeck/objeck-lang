a3a535cd31ff16d5d377320f407f8e2df469d91f
Randy Hollines <objeck@objeck.org>
2016-12-24 10:05:32 -0700

    working on sdl

--- src/objeck/sdl_debug.cmd
+++ src/objeck/sdl_debug.cmd
@@ -1,6 +1,6 @@
 cd deploy\bin
 obc -src '..\..\..\compiler\lib_src\sdl.obs' -lib collect.obl -tar lib -dest sdl.obl
 copy ..\..\..\lib\sdl\lib\x86\sdl2.dll .
-devenv /rebuild Debug ..\..\..\lib\sdl\sdl\sdl.sln
+REM devenv /rebuild Debug ..\..\..\lib\sdl\sdl\sdl.sln
 copy ..\..\..\lib\sdl\sdl\Debug\libobjk_sdl.dll ..\lib\objeck-lang
 cd ..\..
