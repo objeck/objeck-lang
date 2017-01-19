8d7d41a39369265337f50f322a0c52d7c8a31ab4
Randy Hollines <objeck@objeck.org>
2017-01-19 12:42:58 -0500

    tiny language task on rc

--- src/compiler/rc/tiny/build.cmd
+++ src/compiler/rc/tiny/build.cmd
@@ -1,3 +1,3 @@
 @echo off
 del /q *.obe
-obc -src scanner.obs,parser.obs,compiler.obs -lib collect.obl -dest compiler.obe
+obc -src scanner.obs,parser.obs,compiler.obs,emitter.obs -lib collect.obl -dest compiler.obe
