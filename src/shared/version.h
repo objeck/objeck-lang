e87ad647c3bea6ac4da51cc65deba0496b72680e
Randy Hollines <objeck@objeck.org>
2017-02-01 09:03:09 -0800

    v3.5.10

--- src/shared/version.h
+++ src/shared/version.h
@@ -35,8 +35,8 @@
 #define MAGIC_NUM_WEB 0xdddf
 #define MAGIC_NUM_LIB 0xddde
 #define MAGIC_NUM_EXE 0xdddd
-#define VER_NUM 359
+#define VER_NUM 3510
 
-#define VERSION_STRING L"3.5.9"
+#define VERSION_STRING L"3.5.10"
 
 #endif
