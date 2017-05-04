612f8704c4717a26d01bdb39a53f74b30363cd98
Randy Hollines <objeck@objeck.org>
2017-05-04 11:05:32 -0700

    mock 3.6.3

--- core/release/deploy.cmd
+++ core/release/deploy.cmd
@@ -1,8 +1,6 @@
 rmdir /s /q deploy
-rmdir /s /q deploy_fcgi
 
 mkdir deploy
-mkdir deploy_fcgi
 
 REM update version information
 powershell.exe -executionpolicy remotesigned -file  update_version.ps1
@@ -10,7 +8,6 @@ REM build binaries
 devenv /rebuild Release objeck.sln
 mkdir deploy\bin
 copy Release\*.exe deploy\bin
-del deploy\bin\obr_fcgi.exe
 mkdir deploy\lib
 copy ..\lib\*.obl deploy\lib
 del deploy\lib\gtk2.obl
@@ -20,11 +17,11 @@ del /q deploy\bin\a.*
 copy ..\vm\misc\*.pem deploy\lib
 REM openssl support
 mkdir deploy\lib\native
-cd ..\lib\openssl\openssl
+cd ..\lib\openssl
 devenv /rebuild Release openssl.sln
-copy Release\*.dll ..\..\..\release\deploy\lib\native
-copy ..\win32\bin\*.dll ..\..\..\release\deploy\bin
-cd ..\..\..\release
+copy Release\*.dll ..\..\release\deploy\lib\native
+copy ..\win32\bin\*.dll ..\..\release\deploy\bin
+cd ..\..\release
 REM odbc support
 cd ..\lib\odbc
 devenv /rebuild Release odbc.sln
@@ -46,22 +43,6 @@ mkdir deploy\doc\syntax
 copy ..\..\docs\syntax\* deploy\doc\syntax
 copy ..\..\docs\readme.htm deploy
 call code_doc.cmd
-REM create and build fcgi
-copy ..\lib\fcgi.obl deploy\lib
-xcopy /e deploy\* deploy_fcgi
-del deploy_fcgi\bin\obc.exe
-del deploy_fcgi\bin\obd.exe
-rmdir /s /q deploy_fcgi\doc
-rmdir /s /q deploy_fcgi\examples
-copy Release\obr_fcgi.exe deploy_fcgi\bin
-copy ..\lib\fcgi\windows\lib\*.dll deploy_fcgi\bin
-copy redistrib\*.dll deploy_fcgi\bin
-copy Release\libobjk_fcgi.dll deploy_fcgi\lib\native
-mkdir deploy_fcgi\examples
-copy ..\..\programs\web\* deploy_fcgi\examples
-copy /y ..\..\docs\fcgi_readme.htm deploy_fcgi\readme.htm
-mkdir deploy_fcgi\fcgi_readme_files
-copy ..\..\docs\fcgi_readme_files\* deploy_fcgi\fcgi_readme_files
 
 REM finished
 if [%1] NEQ [deploy] goto end
