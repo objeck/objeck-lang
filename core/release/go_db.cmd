75e424a37e7aafa7a215138d9dc1b6ef182f1f5d
Randy Hollines <objeck@gmail.com>
2017-12-17 18:44:38 -0800

    wip

--- core/release/go_db.cmd
+++ core/release/go_db.cmd
@@ -2,5 +2,7 @@
 cd deploy\bin
 obc -src ..\..\..\compiler\lib_src\db.obs -lib collect.obl -tar lib -dest ..\lib\db.obl
 obc -src ..\..\..\..\programs\db\0_test.obs -lib db.obl,collect.obl -dest ..\..\test_db.obe
+echo.
+echo.
 obr ..\..\test_db.obe
 cd ..\..
