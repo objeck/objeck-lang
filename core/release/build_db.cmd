58da68a875cff220ec370bf36cca291f5e39e3d8
Randy Hollines <objeck@gmail.com>
2017-12-18 06:24:16 -0800

    query updates

---
+++ core/release/build_db.cmd
@@ -0,0 +1,5 @@
+@echo off
+cd deploy\bin
+obc -src ..\..\..\compiler\lib_src\db.obs -lib collect.obl -tar lib -dest ..\lib\db.obl
+obc -src ..\..\..\..\programs\db\0_test.obs -lib db.obl,collect.obl -dest ..\..\test_db.obe
+cd ..\..
