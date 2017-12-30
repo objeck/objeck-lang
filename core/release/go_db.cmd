0b9936026daa1efbe9ad53c261d7f8c5bd93d9d8
Randy Hollines <objeck@gmail.com>
2017-12-30 09:35:09 -0700

    adding support for sublime highlighting

--- core/release/go_db.cmd
+++ core/release/go_db.cmd
@@ -1,7 +1,7 @@
 @echo off
 cd deploy\bin
-obc -src ..\..\..\compiler\lib_src\db.obs -lib collect.obl -tar lib -dest ..\lib\db.obl
-obc -src ..\..\..\..\programs\db\0_test.obs -lib db.obl,collect.obl -dest ..\..\test_db.obe
+obc -src ..\..\..\compiler\lib_src\db.obs -lib regex.obl,collect.obl -tar lib -dest ..\lib\db.obl
+obc -src ..\..\..\..\programs\db\0_test.obs -lib regex.obl,db.obl,collect.obl -dest ..\..\test_db.obe
 echo.
 echo.
 obr ..\..\test_db.obe
