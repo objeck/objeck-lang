1721c16a89e52ea2f7c6d003a94a8d861897b6a9
Randy Hollines <objeck@gmail.com>
2017-12-30 09:33:44 -0700

    adding support for sublime highlighting

--- core/release/build_db.cmd
+++ core/release/build_db.cmd
@@ -1,5 +1,5 @@
 @echo off
 cd deploy\bin
-obc -src ..\..\..\compiler\lib_src\db.obs -lib collect.obl -tar lib -dest ..\lib\db.obl
-obc -src ..\..\..\..\programs\db\0_test.obs -lib db.obl,collect.obl -dest ..\..\test_db.obe
+obc -src ..\..\..\compiler\lib_src\db.obs -lib regex.obl,collect.obl -tar lib -dest ..\lib\db.obl
+obc -src ..\..\..\..\programs\db\0_test.obs -lib regex.obl,db.obl,collect.obl -dest ..\..\test_db.obe
 cd ..\..
