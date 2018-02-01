647668f61e5171513df2f7974f2c35cc94229c5d
Randy Hollines <objeck@gmail.com>
2018-02-01 11:29:47 -0800

    inital jit refactor working on posix 64-bit

--- core/vm/arch/posix/memory.h
+++ core/vm/arch/posix/memory.h
@@ -1,7 +1,7 @@
 /***************************************************************************
  * VM memory manager. Implements a simple "mark and sweep" collection algorithm.
  *
- * Copyright (c) 2008-2015, Randy Hollines
+ * Copyright (c) 2008-2018, Randy Hollines
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
