1592201bf3b881d138cfd2eae74edbf4c127ff57
Randy Hollines <objeck@gmail.com>
2018-03-31 10:30:59 -0700

    sdl work

--- core/release/build_sdl.cmd
+++ core/release/build_sdl.cmd
@@ -1,11 +1,13 @@
 @echo off
 
 cd deploy\bin
-rem obc -src ..\..\..\..\programs\sdl\code_gen\sdl_emitter.obs,..\..\..\..\programs\sdl\code_gen\sdl_parser.obs,..\..\..\..\programs\sdl\code_gen\sdl_scanner.obs -lib collect.obl -dest ..\..\sdl_code_gen.obe
-rem obr ..\..\sdl_code_gen.obe %1 %2
 obc -src ..\..\..\compiler\lib_src\sdl.obs -lib collect.obl -tar lib -dest ..\lib\sdl.obl
 obc -src ..\..\..\..\programs\sdl\%1.obs -lib collect.obl,sdl.obl -dest ..\..\%1.obe
 copy /y "D:\Code\objeck-lang\core\lib\sdl\sdl\Debug\libobjk_sdl.dll" ..\lib\native
 copy /y ..\..\..\lib\sdl\lib\x86\*.dll .
-obr ..\..\%1.obe
+if %1.==. (
+	echo Compile only.
+) else (
+	obr ..\..\%1.obe
+)
 cd ..\..
