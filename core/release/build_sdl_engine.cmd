8afe56cd9c082bcf1ecbfca14ac625d809292dcc
Randy Hollines <objeck@gmail.com>
2018-04-29 11:29:52 -0700

    sdl support

--- core/release/build_sdl_engine.cmd
+++ core/release/build_sdl_engine.cmd
@@ -4,7 +4,7 @@ cd deploy\bin
 obc -src ..\..\..\compiler\lib_src\sdl.obs -lib collect.obl -tar lib -dest ..\lib\sdl.obl
 obc -src ..\..\..\..\programs\sdl\engine\sdl_engine.obs -lib collect.obl,sdl.obl -tar lib -dest ..\lib\sdl_engine.obl
 if NOT %1.==. (
-obc -src ..\..\..\..\programs\sdl\%1.obs -lib collect.obl,sdl.obl,sdl_engine -dest ..\..\%1.obe
+obc -src ..\..\..\..\programs\sdl\engine\tests\%1.obs -lib collect.obl,sdl.obl,sdl_engine.obl -dest ..\..\%1.obe
 REM devenv ..\..\..\lib\sdl\sdl\sdl.sln  /rebuild
 copy /y ..\..\..\lib\sdl\sdl\Debug\libobjk_sdl.dll ..\lib\native
 copy /y ..\..\..\lib\sdl\lib\x86\*.dll .
