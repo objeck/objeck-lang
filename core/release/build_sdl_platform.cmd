0e0573deab1c45eef1b53506ddcaf6c101fdf58c
Randy Hollines <objeck@gmail.com>
2018-06-30 12:04:01 -0700

    game demo 3

--- core/release/build_sdl_platform.cmd
+++ core/release/build_sdl_platform.cmd
@@ -3,7 +3,7 @@
 cd deploy\bin
 obc -src ..\..\..\compiler\lib_src\sdl.obs -lib collect.obl -tar lib -dest ..\lib\sdl.obl
 obc -src ..\..\..\..\programs\sdl\engine\sdl_engine.obs -lib collect.obl,sdl.obl -tar lib -dest ..\lib\sdl_engine.obl
-obc -src ..\..\..\..\programs\sdl\engine\tests\test3\platform.obs,..\..\..\..\programs\sdl\engine\tests\test3\player.obs,..\..\..\..\programs\sdl\engine\tests\test3\enemy.obs -lib collect.obl,sdl.obl,sdl_engine.obl -dest ..\..\sdl_platform.obe
+obc -src ..\..\..\..\programs\sdl\engine\tests\test3\platform.obs,..\..\..\..\programs\sdl\engine\tests\test3\player.obs,..\..\..\..\programs\sdl\engine\tests\test3\enemy.obs,..\..\..\..\programs\sdl\engine\tests\test3\bullet.obs -lib collect.obl,sdl.obl,sdl_engine.obl -dest ..\..\sdl_platform.obe
 
 if NOT %1.==. (
 	copy /y ..\..\..\lib\sdl\sdl\Debug\libobjk_sdl.dll ..\lib\native
