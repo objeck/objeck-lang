e595d65deda7f6529ef85c1e7b66f3deb83e7809
Randy Hollines <objeck@gmail.com>
2018-08-02 06:32:17 -0600

    platform demo

--- core/release/build_sdl_engine.cmd
+++ core/release/build_sdl_engine.cmd
@@ -3,15 +3,14 @@
 cd deploy\bin
 obc -src ..\..\..\compiler\lib_src\sdl.obs -lib collect.obl -tar lib -dest ..\lib\sdl.obl
 obc -src ..\..\..\..\programs\sdl\engine\sdl_engine.obs -lib collect.obl,sdl.obl -tar lib -dest ..\lib\sdl_engine.obl
-if NOT %1.==. (
 obc -src ..\..\..\..\programs\sdl\engine\tests\%1.obs -lib collect.obl,sdl.obl,sdl_engine.obl -dest ..\..\%1.obe
-REM devenv ..\..\..\lib\sdl\sdl\sdl.sln  /rebuild
-copy /y ..\..\..\lib\sdl\sdl\Debug\libobjk_sdl.dll ..\lib\native
-copy /y ..\..\..\lib\sdl\lib\x86\*.dll .
 
-rmdir /s /q images
-mkdir images
-xcopy /e ..\..\..\..\programs\sdl\engine\tests\media\images\*.png images\
-obr ..\..\%1.obe
+if NOT %2.==. (
+	copy /y ..\..\..\lib\sdl\sdl\Debug\libobjk_sdl.dll ..\lib\native
+	copy /y ..\..\..\lib\sdl\lib\x86\*.dll .
+	rmdir /s /q images
+	mkdir images
+	xcopy /e ..\..\..\..\programs\sdl\engine\tests\media\images\*.png images\
+	obr ..\..\%1.obe
 )
 cd ..\..
