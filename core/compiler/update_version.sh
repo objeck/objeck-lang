9f302302f97af1b97c1fdf94aac3fbd0b768ff34
Randy Hollines <objeck@gmail.com>
2018-08-16 18:43:52 -0700

    updating game framework libs

--- core/compiler/update_version.sh
+++ core/compiler/update_version.sh
@@ -13,3 +13,5 @@ make -f make/Makefile.32
 ./obc -src lib_src/fcgi.obs -lib collect.obl,json.obl -tar lib -opt s3 -dest ../lib/fcgi.obl
 ./obc -src lib_src/csv.obs -tar lib -lib collect.obl -opt s3 -dest ../lib/csv.obl
 ./obc -src lib_src/query.obs -tar lib -lib regex.obl,collect.obl -opt s3 -dest ../lib/query.obl
+./obc -src lib_src/sdl.obs -lib collect.obl -tar lib -dest ../lib/sdl.obl
+./obc -src lib_src/game.obs -lib collect.obl,sdl.obl -tar lib -dest ../lib/game.obl
