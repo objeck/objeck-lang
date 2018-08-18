074b8419725c30836ce604d78e8e00bfb9b331af
Randy Hollines <objeck@gmail.com>
2018-08-17 20:42:38 -0700

    wip

--- core/lib/sdl/build_linux.sh
+++ core/lib/sdl/build_linux.sh
@@ -2,4 +2,4 @@
 rm -rf *.o
 rm -rf *.so
 g++ -O3 -Wno-unused-function -Wall -fPIC -c *$1.cpp `pkg-config --cflags --libs sdl2`
-g++ -O3 -shared -Wl,-soname,$1.so.1 -o $1.so *.o -lSDL2 -lSDL2main
+g++ -O3 -shared -Wl,-soname,$1.so.1 -o $1.so *.o -lSDL2_ttf -lSDL2_mixer -lSDL2_image -lSDL2 -lSDL2main
