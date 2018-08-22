8b1fb3def6a7abcfb0b36ec871e0ca61b12fc776
Randy Hollines <objeck@gmail.com>
2018-08-22 09:06:59 -0700

    wip

--- core/release/build_sdl_game.cmd
+++ core/release/build_sdl_game.cmd
@@ -1,19 +1,12 @@
 @echo off
 
 cd deploy\bin
-set path=%cd%\..\bin;%cd%\..\lib\sdl;%cd%\..\lib\sdl;;%path%
-set OBJECK_LIB_PATH=%cd%\..\lib
 
 obc -src ..\..\..\compiler\lib_src\sdl2.obs -lib collect.obl -tar lib -dest ..\lib\sdl2.obl
 obc -src ..\..\..\compiler\lib_src\sdl_game.obs -lib collect.obl,sdl2.obl -tar lib -dest ..\lib\sdl_game.obl
 
-set prev_dir=%cd%
-cd ..\..\..\..\programs\sdl\engine\tests\test6
-obc -src game.obs,blocks.obs,enemies.obs,player.obs,level.obs -lib csv.obl,collect.obl,sdl2.obl,sdl_game.obl -dest %prev_dir%\..\..\..\game.obe
-if not errorlevel 0 goto on_fail
-
-:on_fail
-cd %prev_dir%
+set prgm_path=..\..\..\..\programs\sdl\engine\tests\test6
+obc -src %prgm_path%\game.obs,%prgm_path%\blocks.obs,%prgm_path%\enemies.obs,%prgm_path%\player.obs,%prgm_path%\level.obs -lib csv.obl,collect.obl,sdl2.obl,sdl_game.obl -dest ..\..\..\game.obe
 
 if [%1] EQU [brun] (
 	rmdir /s /q images
