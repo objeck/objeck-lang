7b616549924fd15c30c11786ad4a18a1844810f5
Randy Hollines <objeck@gmail.com>
2018-08-21 19:50:25 -0700

    2d platformer

--- core/release/build_sdl_game.cmd
+++ core/release/build_sdl_game.cmd
@@ -1,10 +1,19 @@
 @echo off
 
 cd deploy\bin
-set path=..\lib\sdl;%path%
+set path=%cd%\..\bin;%cd%\..\lib\sdl;%cd%\..\lib\sdl;;%path%
+set OBJECK_LIB_PATH=%cd%\..\lib
+
 obc -src ..\..\..\compiler\lib_src\sdl2.obs -lib collect.obl -tar lib -dest ..\lib\sdl2.obl
 obc -src ..\..\..\compiler\lib_src\sdl_game.obs -lib collect.obl,sdl2.obl -tar lib -dest ..\lib\sdl_game.obl
-obc -src ..\..\..\..\programs\sdl\engine\tests\test6\game.obs,..\..\..\..\programs\sdl\engine\tests\test6\level.obs,..\..\..\..\programs\sdl\engine\tests\test6\player.obs,..\..\..\..\programs\sdl\engine\tests\test6\platform.obs -lib csv.obl,collect.obl,sdl2.obl,sdl_game.obl -dest ..\..\..\game.obe
+
+set prev_dir=%cd%
+cd ..\..\..\..\programs\sdl\engine\tests\test6
+obc -src game.obs,blocks.obs,enemies.obs,player.obs,level.obs -lib csv.obl,collect.obl,sdl2.obl,sdl_game.obl -dest %prev_dir%\..\..\..\game.obe
+if not errorlevel 0 goto on_fail
+
+:on_fail
+cd %prev_dir%
 
 if [%1] EQU [brun] (
 	rmdir /s /q images
