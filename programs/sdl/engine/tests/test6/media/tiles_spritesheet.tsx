2bce9fc136176bb0dcf5952fbcd5705f27971ebe
Randy Hollines <objeck@gmail.com>
2018-08-25 22:53:42 -0700

    2d game demo

---
+++ programs/sdl/engine/tests/test6/media/tiles_spritesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles_spritesheet" tilewidth="72" tileheight="72" tilecount="156" columns="12">
+ <image source="tiles_spritesheet.png" width="914" height="936"/>
+</tileset>
