198561e05df318080709a8d89d8f9aea82ac060e
Randy Hollines <objeck@gmail.com>
2018-08-30 09:41:42 -0700

    2d game

--- programs/sdl/engine/tests/test6/media/tiles_spritesheet.tsx
+++ programs/sdl/engine/tests/test6/media/tiles_spritesheet.tsx
@@ -7,6 +7,11 @@
    <object id="7" x="25.6364" y="94.3636"/>
   </objectgroup>
  </tile>
+ <tile id="123">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="72" height="36"/>
+  </objectgroup>
+ </tile>
  <tile id="124">
   <objectgroup draworder="index">
    <object id="1" x="0" y="44" width="72" height="26"/>
