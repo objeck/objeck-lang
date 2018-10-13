d426c24b28f8446ae7283dc697bb6402180a0389
Randy Hollines <objeck@gmail.com>
2018-10-13 10:03:49 -0700

    werking on more 2d demos

---
+++ programs/sdl/proj/build.cmd
@@ -0,0 +1,19 @@
+@echo off
+setlocal
+
+if "%~1" == "" goto ERROR 
+	del /q %1.obe
+
+	set PATH=%PATH%;..\..\..\core\release\deploy\bin
+	set OBJECK_LIB_PATH=..\..\..\core\release\deploy\lib
+
+	obc -src %1\component.obs,%1\actor.obs,%1\game.obs -lib collect.obl,sdl2.obl -dest %1.obe
+
+	if [%2] == [deploy] goto END
+		obr %1.obe
+		goto END
+
+:ERROR
+	echo No code directory provided!
+
+:END
