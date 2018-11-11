a6095095bfa705c09f242e999136d6c9d4693657
Randy Hollines <objeck@gmail.com>
2018-11-11 05:51:27 -0800

    working on sdl texture lock/unlock

--- core/release/build_sdl_engine.cmd
+++ core/release/build_sdl_engine.cmd
@@ -1,4 +1,5 @@
-SETLOCAL
+@echo off
+setlocal
 
 cd deploy\bin
 set path=..\lib\sdl;%path%
@@ -6,4 +7,3 @@ obc -src ..\..\..\compiler\lib_src\sdl2.obs -lib collect.obl -tar lib -dest ..\l
 obc -src ..\..\..\compiler\lib_src\sdl_game.obs -lib collect.obl,sdl2.obl -tar lib -dest ..\lib\sdl_game.obl
 
 copy ..\..\..\lib\sdl\sdl\Debug\Win32\libobjk_sdl.dll ..\..\deploy\lib\native
-copy ..\lib\sdl2.obl ..\..\deploy\lib
