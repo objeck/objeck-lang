8961f0890b91785fcf145464c6012cfa548bab3d
Randy Hollines <objeck@gmail.com>
2018-11-22 08:21:15 -0800

    repo clean up

--- core/release/build_db.sh
+++
@@ -1,4 +0,0 @@
-cd deploy/bin
-./obc -src ../../../compiler/lib_src/db.obs -lib regex.obl,collect.obl -tar lib -dest ../lib/db.obl
-./obc -src ../../../../programs/db/0_test.obs -lib regex.obl,db.obl,collect.obl -dest ../../test_db.obe
-cd ../..
--- core/release/go_db.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-cd deploy\bin
-obc -src ..\..\..\compiler\lib_src\query.obs -lib regex.obl,collect.obl -tar lib -dest ..\lib\query.obl
-obc -src ..\..\..\..\programs\db\0_test.obs -lib regex.obl,query.obl,collect.obl -dest ..\..\test_query.obe
-echo.
-echo.
-obr ..\..\test_query.obe
-cd ..\..
