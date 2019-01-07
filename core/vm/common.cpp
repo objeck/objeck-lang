29d156adc992a20563d30c39982db989051c7076
Randy Hollines <objeck@gmail.com>
2019-01-06 20:16:22 -0800

    adding zlib compression support

--- core/vm/common.cpp
+++ core/vm/common.cpp
@@ -2858,6 +2858,45 @@ bool TrapProcessor::DeserlFloatAry(StackProgram* program, size_t* inst, size_t* 
 
 bool TrapProcessor::CompressBytes(StackProgram* program, size_t* inst, size_t* &op_stack, long* &stack_pos, StackFrame* frame)
 {
+  // original string len = 36
+  char a[50] = "Hello Hello Hello Hello Hello Hello!";
+
+  // placeholder for the compressed (deflated) version of "a" 
+  char b[50];
+
+  // placeholder for the UNcompressed (inflated) version of "b"
+  char c[50];
+
+
+  printf("Uncompressed size is: %lu\n", strlen(a));
+  printf("Uncompressed string is: %s\n", a);
+
+
+  printf("\n----------\n\n");
+
+  // STEP 1.
+  // deflate a into b. (that is, compress a into b)
+
+  // zlib struct
+  z_stream defstream;
+  defstream.zalloc = Z_NULL;
+  defstream.zfree = Z_NULL;
+  defstream.opaque = Z_NULL;
+  // setup "a" as the input and "b" as the compressed output
+  defstream.avail_in = (uInt)strlen(a) + 1; // size of input, string + terminator
+  defstream.next_in = (Bytef *)a; // input char array
+  defstream.avail_out = (uInt)sizeof(b); // size of output
+  defstream.next_out = (Bytef *)b; // output char array
+
+  // the actual compression work.
+  deflateInit(&defstream, Z_BEST_COMPRESSION);
+  deflate(&defstream, Z_FINISH);
+  deflateEnd(&defstream);
+
+  // This is one way of getting the size of the output
+  printf("Compressed size is: %lu\n", strlen(b));
+  printf("Compressed string is: %s\n", b);
+
   return false;
 }
 
