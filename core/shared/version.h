d3afcfeafc47f78470044144a7726199a011ff78
Randy Hollines <objeck@gmail.com>
2019-01-12 08:43:43 -0800

    v4.1.6

--- core/shared/version.h
+++ core/shared/version.h
@@ -35,8 +35,8 @@
 #define MAGIC_NUM_WEB 0xdddf
 #define MAGIC_NUM_LIB 0xddde
 #define MAGIC_NUM_EXE 0xdddd
-#define VER_NUM 415
+#define VER_NUM 416
 
-#define VERSION_STRING L"4.1.5"
+#define VERSION_STRING L"4.1.6"
 
 #endif
