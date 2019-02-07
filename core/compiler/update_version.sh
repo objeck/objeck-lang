f38b9bbb63b05f81d146e659690e8eef2aebaebc
Randy Hollines <objeck@gmail.com>
2019-02-07 08:08:59 -0800

    updated libs

--- core/compiler/update_version.sh
+++ core/compiler/update_version.sh
@@ -10,7 +10,7 @@ make -f make/Makefile.32
 ./obc -src lib_src/encrypt.obs -lib collect.obl -tar lib -opt s3 -dest ../lib/encrypt.obl
 ./obc -src lib_src/odbc.obs -lib collect.obl -tar lib -opt s3 -dest ../lib/odbc.obl
 ./obc -src lib_src/regex.obs -lib collect.obl -tar lib -opt s3 -dest ../lib/regex.obl
-./obc -src lib_src/finder.obs -lib collect.obl -tar lib -opt s3 -dest ../lib/finder.obl
+./obc -src lib_src/finder.obs -lib collect.obl,regex.obl -tar lib -opt s3 -dest ../lib/finder.obl
 ./obc -src lib_src/fcgi.obs -lib collect.obl,json.obl -tar lib -opt s3 -dest ../lib/fcgi.obl
 ./obc -src lib_src/csv.obs -tar lib -lib collect.obl -opt s3 -dest ../lib/csv.obl
 ./obc -src lib_src/query.obs -tar lib -lib regex.obl,collect.obl -opt s3 -dest ../lib/query.obl
