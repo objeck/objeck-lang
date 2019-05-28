ef0080bb2c158cf3cc81dec8656c66d47bb6f995
Randy Hollines <objeck@gmail.com>
2019-05-27 22:03:24 -0700

    updating libs

--- core/compiler/update_version.sh
+++ core/compiler/update_version.sh
@@ -13,7 +13,7 @@ make -f make/Makefile.32
 ./obc -src lib_src/odbc.obs -lib collect.obl -tar lib -opt s3 -dest ../lib/odbc.obl
 ./obc -src lib_src/regex.obs -lib gen_collect.obl -tar lib -opt s3 -dest ../lib/regex.obl
 ./obc -src lib_src/fcgi.obs -lib collect.obl,json.obl -tar lib -opt s3 -dest ../lib/fcgi.obl
-./obc -src lib_src/csv.obs -tar -lib gen_collect.obl -opt s3 -dest ../lib/csv.obl
-./obc -src lib_src/query.obs -tar lib -lib xml.obl,regex.obl,gen_collect.obl -opt s3 -dest ../lib/query.obl
+./obc -src lib_src/csv.obs -tar lib :-lib gen_collect.obl -opt s3 -dest ../lib/csv.obl
+./obc -src lib_src/query.obs -tar lib -lib xml.obl,regex.obl,net_misc.obl,gen_collect.obl -opt s3 -dest ../lib/query.obl
 ./obc -src lib_src/sdl2.obs -lib collect.obl -tar lib -dest ../lib/sdl2.obl
 ./obc -src lib_src/sdl_game.obs -lib collect.obl,sdl2.obl -tar lib -dest ../lib/sdl_game.obl
