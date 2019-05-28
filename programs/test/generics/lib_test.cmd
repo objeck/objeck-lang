4c53e279c3cbefbeb295b8e518b061dc112cf43c
Randy Hollines <objeck@gmail.com>
2019-05-27 22:25:54 -0700

    porting and debugging

--- programs/test/generics/lib_test.cmd
+++ programs/test/generics/lib_test.cmd
@@ -4,8 +4,8 @@ if "%~1"=="" goto blank
 for %%i in (%1) do (
 	set fn=%%~ni
 	obc -src ..\..\..\core\compiler\lib_src\gen_collect.obs -tar lib -dest ..\..\..\core\release\deploy64\lib\gen_collect.obl
-REM	obc -src ..\..\..\core\compiler\lib_src\%fn%.obs -lib xml.obl,regex.obl,gen_collect.obl -tar lib -dest ..\..\..\core\release\deploy64\lib\%fn%.obl
-	obc -src ..\..\..\core\compiler\lib_src\%fn%.obs -lib gen_collect.obl -tar lib -dest ..\..\..\core\release\deploy64\lib\%fn%.obl
+	obc -src ..\..\..\core\compiler\lib_src\%fn%.obs -lib xml.obl,regex.obl,gen_collect.obl,net_misc.obl -tar lib -dest ..\..\..\core\release\deploy64\lib\%fn%.obl
+REM	obc -src ..\..\..\core\compiler\lib_src\%fn%.obs -lib gen_collect.obl -tar lib -dest ..\..\..\core\release\deploy64\lib\%fn%.obl
 	goto end
 )
 
