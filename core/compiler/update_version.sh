0f23c580ddfa842440c0992ed773dfb64205e1ce
Randy Hollines <objeck@gmail.com>
2019-05-27 17:37:29 -0700

    porting and debugging

--- core/compiler/update_version.sh
+++ core/compiler/update_version.sh
@@ -6,9 +6,9 @@ make -f make/Makefile.32 clean
 make -f make/Makefile.32
 ./obc -src lib_src/collect.obs -tar lib -opt s3 -dest ../lib/collect.obl
 ./obc -src lib_src/gen_collect.obs -tar lib -opt s3 -dest ../lib/gen_collect.obl
-./obc -src lib_src/xml.obs -lib collect.obl -tar lib -opt s3 -dest ../lib/xml.obl
-./obc -src lib_src/json.obs -lib collect.obl -tar lib -opt s3 -dest ../lib/json.obl
-./obc -src lib_src/encrypt.obs -lib collect.obl -tar lib -opt s3 -dest ../lib/encrypt.obl
+./obc -src lib_src/xml.obs -lib gen_collect.obl -tar lib -opt s3 -dest ../lib/xml.obl
+./obc -src lib_src/json.obs -lib gen_collect.obl -tar lib -opt s3 -dest ../lib/json.obl
+./obc -src lib_src/encrypt.obs -tar lib -opt s3 -dest ../lib/encrypt.obl
 ./obc -src lib_src/odbc.obs -lib collect.obl -tar lib -opt s3 -dest ../lib/odbc.obl
 ./obc -src lib_src/regex.obs -lib collect.obl -tar lib -opt s3 -dest ../lib/regex.obl
 ./obc -src lib_src/fcgi.obs -lib collect.obl,json.obl -tar lib -opt s3 -dest ../lib/fcgi.obl
