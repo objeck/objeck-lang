592ddd03d6a310964373fcad26f7dfbec1fd344a
Randy Hollines <objeck@gmail.com>
2019-06-12 08:26:27 -0700

    working on lambda support

--- core/compiler/scanner.cpp
+++ core/compiler/scanner.cpp
@@ -118,7 +118,7 @@ void Scanner::LoadKeywords()
   ident_map[L"select"] = TOKEN_SELECT_ID;
   ident_map[L"other"] = TOKEN_OTHER_ID;
   ident_map[L"enum"] = TOKEN_ENUM_ID;
-  ident_map[L"templates"] = TOKEN_TEMPLATES_ID;
+  ident_map[L"template"] = TOKEN_TEMPLATE_ID;
   ident_map[L"consts"] = TOKEN_CONSTS_ID;
   ident_map[L"for"] = TOKEN_FOR_ID;
   ident_map[L"each"] = TOKEN_EACH_ID;
@@ -323,7 +323,7 @@ void Scanner::CheckIdentifier(int index)
     case TOKEN_FROM_ID:
     case TOKEN_OTHER_ID:
     case TOKEN_ENUM_ID:
-    case TOKEN_TEMPLATES_ID:
+    case TOKEN_TEMPLATE_ID:
     case TOKEN_CONSTS_ID:
     case TOKEN_FOR_ID:
     case TOKEN_EACH_ID:
