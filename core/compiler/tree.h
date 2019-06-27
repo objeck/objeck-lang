14a3180417538702387efcf8182ae1ee1f132e46
Randy Hollines <objeck@gmail.com>
2019-06-26 19:36:36 -0700

    refactor from old code

--- core/compiler/tree.h
+++ core/compiler/tree.h
@@ -2641,6 +2641,7 @@ namespace frontend {
     Method* method;
     ExpressionList* parameters;
     MethodCall* method_call;
+    vector<pair<SymbolEntry*, SymbolEntry*> > copies;
     
   public:
     Lambda(const wstring& file_name, const int line_num, Type* t, const wstring &n, Method* m, ExpressionList* p) : Expression(file_name, line_num) {
@@ -2680,6 +2681,12 @@ namespace frontend {
 
     void SetMethodCall(MethodCall* c) {
       method_call = c;
+    }
+    void AddCopy(pair<SymbolEntry*, SymbolEntry*> &copy) {
+      copies.push_back(copy);
+    }
+    vector<pair<SymbolEntry*, SymbolEntry*> > GetCopies() {
+      return copies;
     }
   };
 
