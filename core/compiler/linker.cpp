b14f5bec44b359b568170993f8d241c03d53a057
Randy Hollines <objeck@gmail.com>
2019-07-01 13:40:08 -0700

    code refactoring

--- core/compiler/linker.cpp
+++ core/compiler/linker.cpp
@@ -179,12 +179,21 @@ void Linker::ResolveExternalMethodCalls()
             if(lib_klass) {
               LibraryMethod* lib_method = lib_klass->GetMethod(instr->GetOperand6());
               if(lib_method) {
-                // set method
+
+                const int16_t lib_method_id = lib_method->GetId();
+                const int16_t lib_cls_id = lib_method->GetLibraryClass()->GetId();
+                const int lib_method_cls_id = (lib_cls_id << 16) | lib_method_id;
+
                 instr->SetType(instructions::LOAD_INT_LIT);
-                instr->SetOperand(lib_method->GetId());
+                instr->SetOperand(lib_method_cls_id);
+
+                /*
+                // set method
+                
                 // set class
                 instrs[j + 1]->SetType(instructions::LOAD_INT_LIT);
                 instrs[j + 1]->SetOperand(lib_klass->GetId());
+                */
               }
               else {
                 wcerr << L"Error: Unable to resolve external library method: '" << instr->GetOperand6() << L"'; check library path" << endl;
@@ -1073,7 +1082,6 @@ void Library::LoadStatements(LibraryMethod* method, bool is_debug)
       Linker::Debug(msg, 0, 3);
 #endif
       instrs.push_back(new LibraryInstr(line_num, LIB_FUNC_DEF, -1, cls_name, mthd_name));
-      instrs.push_back(new LibraryInstr(line_num, LOAD_INT_LIT, -1));
     }
       break;
 
