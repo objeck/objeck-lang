2f114da3704d572bbf0a218e839392562e229e5d
Randy Hollines <objeck@gmail.com>
2019-07-12 23:09:24 -0700

    adding memory command to the debugger

--- core/debugger/scanner.cpp
+++ core/debugger/scanner.cpp
@@ -89,6 +89,8 @@ void Scanner::LoadKeywords()
   ident_map[L"?stack"] = TOKEN_STACK_ID;
   ident_map[L"?print"] = TOKEN_PRINT_ID;
   ident_map[L"?p"] = TOKEN_PRINT_ID;
+  ident_map[L"?memory"] = TOKEN_MEMORY_ID;
+  ident_map[L"?m"] = TOKEN_MEMORY_ID;
   ident_map[L"?info"] = TOKEN_INFO_ID;
   ident_map[L"?i"] = TOKEN_INFO_ID;
   ident_map[L"?frame"] = TOKEN_FRAME_ID;
