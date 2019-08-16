4aadea80d84963b25beb65cc64a05dbd179331f0
Randy Hollines <objeck@gmail.com>
2019-08-16 11:39:47 -0500

    code clean up

--- core/compiler/context.h
+++ core/compiler/context.h
@@ -373,13 +373,16 @@ class ContextAnalyzer {
     if(!method_call->GetConcreteTypes().empty()) {
       return method_call->GetConcreteTypes();
     }
-    else if(method_call->GetEntry()) {
-      return method_call->GetEntry()->GetType()->GetGenerics();
+    else if(method_call->GetEvalType() && method_call->GetEntry()) {
+      return method_call->GetEvalType()->GetGenerics();
     }
     else if(method_call->GetEvalType()) {
       return method_call->GetEvalType()->GetGenerics();
     }
-    
+    else if(method_call->GetEntry()) {
+      return method_call->GetEntry()->GetType()->GetGenerics();
+    }
+
     return vector<Type*>();
   }
   
