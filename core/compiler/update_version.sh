6e8659fad6a1c88dc28ed6be027b34a3a8528701
Randy Hollines <objeck@gmail.com>
2019-12-27 14:16:50 -0800

    updating libs

--- core/compiler/update_version.sh
+++ core/compiler/update_version.sh
@@ -6,7 +6,7 @@ make -f make/Makefile.32 clean
 make -f make/Makefile.32
 ./obc -src lib_src/collect.obs -tar lib -opt s3 -dest ../lib/collect.obl
 ./obc -src lib_src/gen_collect.obs -tar lib -opt s3 -dest ../lib/gen_collect.obl
-./obc -src lib_src/misc.obs -tar lib -opt s3 -dest ../lib/misc.obl
+./obc -src lib_src/misc.obs -lib gen_collect.obl -tar lib -opt s3 -dest ../lib/misc.obl
 ./obc -src lib_src/xml.obs -lib gen_collect.obl -tar lib -opt s3 -dest ../lib/xml.obl
 ./obc -src lib_src/net.obs -tar lib -lib xml.obl,gen_collect.obl -opt s3 -dest ../lib/net.obl
 ./obc -src lib_src/rss.obs -tar lib -lib xml.obl,gen_collect.obl,net.obl -opt s3 -dest ../lib/rss.obl
@@ -16,6 +16,6 @@ make -f make/Makefile.32
 ./obc -src lib_src/regex.obs -lib gen_collect.obl -tar lib -opt s3 -dest ../lib/regex.obl
 ./obc -src lib_src/fcgi.obs -lib net.obl,collect.obl,json.obl -tar lib -opt s3 -dest ../lib/fcgi.obl
 ./obc -src lib_src/csv.obs -tar lib -lib gen_collect.obl -opt s3 -dest ../lib/csv.obl
-./obc -src lib_src/query.obs -tar lib -lib xml.obl,rss.obl,regex.obl,gen_collect.obl -opt s3 -dest ../lib/query.obl
+./obc -src lib_src/query.obs -tar lib -lib xml.obl,misc.obl,regex.obl,gen_collect.obl -opt s3 -dest ../lib/query.obl
 ./obc -src lib_src/sdl2.obs -tar lib -dest ../lib/sdl2.obl
 ./obc -src lib_src/sdl_game.obs -lib gen_collect.obl,sdl2.obl -tar lib -dest ../lib/sdl_game.obl
