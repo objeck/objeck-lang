5c5a93371c6402eb5c2cf955f9d41c1d712f6af3
Randy Hollines <objeck@gmail.com>
2020-04-07 20:18:01 -0700

    bumped version

--- core/release/update_version.ps1
+++ core/release/update_version.ps1
@@ -1,5 +1,5 @@
 # version string
-$version = "5.3.4"
+$version = "5.3.5"
 
 # alternative strings
 $version_number = $version.Replace(".", "")
--- core/shared/version.h
+++ core/shared/version.h
@@ -35,8 +35,8 @@
 #define MAGIC_NUM_WEB 0xdddf
 #define MAGIC_NUM_LIB 0xddde
 #define MAGIC_NUM_EXE 0xdddd
-#define VER_NUM 534
+#define VER_NUM 535
 
-#define VERSION_STRING L"5.3.4"
+#define VERSION_STRING L"5.3.5"
 
 #endif
