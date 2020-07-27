12ee547acc27a7485e498123c46a94292e03c78b
Randy Hollines <objeck@gmail.com>
2020-07-26 19:53:09 -0700

    fixing bug for generic to concrete type check

--- core/compiler/context.cpp
+++ core/compiler/context.cpp
@@ -6985,6 +6985,17 @@ Type* ContextAnalyzer::ResolveGenericType(Type* candidate_type, MethodCall* meth
               if(klass && method_call->GetEvalType()) {
                 const vector<Type*> map_types = method_call->GetEvalType()->GetGenerics();
                 if(i < map_types.size()) {
+                  Type* map_type = map_types[i];
+                  const int map_type_index = klass->GenericIndex(map_type->GetName());
+                  if(map_type_index > -1 && map_type_index < real_types.size()) {
+                    Type* concrete_type = real_types[map_type_index];
+                    if(rtrn_candidate_type->GetName() != concrete_type->GetName()) {
+                      ProcessError(static_cast<Expression*>(method_call), L"Invalid concrete type '" + concrete_type->GetName() + L"'");
+                    }
+                  }
+                  else {
+                    ProcessError(static_cast<Expression*>(method_call), L"Invalid generic type '" + map_type->GetName() + L"'");
+                  }
                   ResolveClassEnumType(map_types[i]);
                 }
                 else {
@@ -6994,7 +7005,18 @@ Type* ContextAnalyzer::ResolveGenericType(Type* candidate_type, MethodCall* meth
               else if(lib_klass && method_call->GetEvalType()) {
                 const vector<Type*> map_types = method_call->GetEvalType()->GetGenerics();
                 if(i < map_types.size()) {
-                  ResolveClassEnumType(map_types[i]);
+                  Type* map_type = map_types[i];
+                  const int map_type_index = lib_klass->GenericIndex(map_type->GetName());
+                  if(map_type_index > -1 && map_type_index < real_types.size()) {
+                    Type* concrete_type = real_types[map_type_index];
+                    if(rtrn_candidate_type->GetName() != concrete_type->GetName()) {
+                      ProcessError(static_cast<Expression*>(method_call), L"Invalid concrete type '" + concrete_type->GetName() + L"'");
+                    }
+                  }
+                  else {
+                    ProcessError(static_cast<Expression*>(method_call), L"Invalid generic type '" + map_type->GetName() + L"'");
+                  }
+                  ResolveClassEnumType(map_type);
                 }
                 else {
                   ProcessError(static_cast<Expression*>(method_call), L"Concrete to generic size mismatch");
