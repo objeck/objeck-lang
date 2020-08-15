86e59e9fdf18d082c038dd027af245b46afd1b5f
Randy Hollines <objeck@gmail.com>
2020-08-15 14:50:18 -0700

    updating fcgi support

--- programs/web/build.cmd
+++ programs/web/build.cmd
@@ -1,3 +1,8 @@
-obc -src ..\..\core\compiler\lib_src\fcgi_web.obs -lib fcgi,gen_collect,net,misc -tar lib -dest ..\..\core\lib\fcgi_web.obl
+obc -src ..\..\core\compiler\lib_src\fcgi.obs -lib gen_collect,net,misc -tar lib -opt s3 -dest ..\..\core\lib\fcgi.obl
+obc -src ..\..\core\compiler\lib_src\fcgi_web.obs -lib fcgi,gen_collect,net,misc -tar lib -opt s3 -dest ..\..\core\lib\fcgi_web.obl
+copy /y ..\..\core\lib\fcgi.obl ..\..\core\release\deploy64\lib\
 copy /y ..\..\core\lib\fcgi_web.obl ..\..\core\release\deploy64\lib\
 obc -src %1.obs -lib fcgi,gen_collect,net -tar web -dest %1.obw
+"D:\Program Files (x86)\WinSCP\WinSCP.exe" /client objec:%2@IIS-VM /command "put ..\..\core\lib\fcgi.obl /inetpub/wwwroot/objeck_fcgi/lib/fcgi.obl"
+"D:\Program Files (x86)\WinSCP\WinSCP.exe" /client objec:%2@IIS-VM /command "put ..\..\core\lib\fcgi_web.obl /inetpub/wwwroot/objeck_fcgi/lib/fcgi_web.obl"
+"D:\Program Files (x86)\WinSCP\WinSCP.exe" /client objec:%2@IIS-VM /command "put %1.obw /inetpub/wwwroot/objeck_fcgi/apps/%1.obw"
