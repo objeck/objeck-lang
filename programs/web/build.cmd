2eaa297b8bfb70784451d1fddf4c40bfdd0d2156
Randy Hollines <objeck@gmail.com>
2020-08-15 08:00:10 -0700

    updating fcgi support

--- programs/web/build.cmd
+++ programs/web/build.cmd
@@ -1,1 +1,3 @@
+obc -src ..\..\core\compiler\lib_src\fcgi_web.obs -lib fcgi,gen_collect,net,misc -tar lib -dest ..\..\core\lib\fcgi_web.obl
+copy /y ..\..\core\lib\fcgi_web.obl ..\..\core\release\deploy64\lib\
 obc -src %1.obs -lib fcgi,gen_collect,net -tar web -dest %1.obw
