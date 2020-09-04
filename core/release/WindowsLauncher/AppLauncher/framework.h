4869ef48caa3a31e3ca077188f7937dfb547d355
Randy Hollines <objeck@gmail.com>
2020-09-04 13:34:31 -0700

    windows app launcher

---
+++ core/release/WindowsLauncher/AppLauncher/framework.h
@@ -0,0 +1,23 @@
+// header.h : include file for standard system include files,
+// or project specific include files
+//
+
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#pragma once
+
+#include "targetver.h"
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+
+// Windows Header Files
+#include <windows.h>
+#include <commctrl.h>
+#include <shellapi.h>
+
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
