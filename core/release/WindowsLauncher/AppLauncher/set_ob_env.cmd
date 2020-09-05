2125eb3ae0d4f76369ae62914107da827fa68db1
Randy Hollines <objeck@gmail.com>
2020-09-04 19:32:24 -0700

    windows launcher

--- core/release/WindowsLauncher/AppLauncher/set_ob_env.cmd
+++ core/release/WindowsLauncher/AppLauncher/set_ob_env.cmd
@@ -3,7 +3,7 @@
 @echo Objeck Command Prompt (obc, obr, obd)
 @echo Copyright(c) 2008-2020, Randy Hollines
 @echo =========================================
-set PATH=%PATH%;D:\Code\objeck-lang\core\release\deploy64\bin;;D:\Code\objeck-lang\core\release\deploy64\lib\sdl
+set PATH=%PATH%;..\bin;..\lib\sdl
 set OBJECK_LIB_PATH=D:\Code\objeck-lang\core\release\deploy64\lib
 if [%1]==[] goto end
 cd %1
