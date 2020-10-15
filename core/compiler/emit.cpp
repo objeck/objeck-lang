1ae3a7638cad0718d8322e19d18f49b8248a0259
Randy Hollines <objeck@gmail.com>
2020-10-15 11:17:58 -0700

    improving instruction loading

--- core/compiler/emit.cpp
+++ core/compiler/emit.cpp
@@ -350,6 +350,10 @@ void IntermediateInstruction::Write(bool is_debug, OutputStream& out_stream) {
     else {
       WriteByte(LOAD_CLS_INST_INT_VAR, out_stream);
     }
+
+    if(is_debug) {
+      WriteInt(line_num, out_stream);
+    }
     WriteInt(operand, out_stream);
     break;
 
@@ -359,6 +363,10 @@ void IntermediateInstruction::Write(bool is_debug, OutputStream& out_stream) {
     }
     else {
       WriteByte(STOR_CLS_INST_INT_VAR, out_stream);
+    }
+
+    if(is_debug) {
+      WriteInt(line_num, out_stream);
     }
     WriteInt(operand, out_stream);
     break;
@@ -370,12 +378,19 @@ void IntermediateInstruction::Write(bool is_debug, OutputStream& out_stream) {
     else {
       WriteByte(COPY_CLS_INST_INT_VAR, out_stream);
     }
+
+    if(is_debug) {
+      WriteInt(line_num, out_stream);
+    }
     WriteInt(operand, out_stream);
     break;
 
   default:
     WriteByte(type, out_stream);
-    
+    if(is_debug) {
+      WriteInt(line_num, out_stream);
+    }
+
     switch(type) {
     case LOAD_INT_LIT:
     case NEW_FLOAT_ARY:
@@ -451,10 +466,6 @@ void IntermediateInstruction::Write(bool is_debug, OutputStream& out_stream) {
       break;
     }
     break;
-  }
-
-  if(is_debug) {
-    WriteInt(line_num, out_stream);
   }
 }
 
@@ -538,10 +549,10 @@ void IntermediateInstruction::Debug() {
 
   case LOAD_INT_VAR:
     if(operand2 == LOCL) {
-      GetLogger() << L"LOAD_LOCL_INT_VAR: id=" << operand2 << endl;
+      GetLogger() << L"LOAD_LOCL_INT_VAR: id=" << operand << endl;
     }
     else {
-      GetLogger() << L"LOAD_CLS_INST_INT_VAR: id=" << operand2 << endl;
+      GetLogger() << L"LOAD_CLS_INST_INT_VAR: id=" << operand << endl;
     }
     break;
 
