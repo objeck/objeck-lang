3347977a0e83f21c5888655b686ea98d65d15c91
Randy Hollines <objeck@gmail.com>
2020-10-15 20:06:16 -0700

    Revert "improving instruction loading"
    This reverts commit 427899c22f81e905550cbc4186aa42cc711f3e3b.

--- core/vm/loader.cpp
+++ core/vm/loader.cpp
@@ -512,15 +512,12 @@ void Loader::LoadStatements(StackMethod* method, bool is_debug)
     }
       break;
 
-    case STOR_LOCL_INT_VAR: {
+    case STOR_INT_VAR: {
       const long id = ReadInt();
-      mthd_instrs[i] = new StackInstr(line_num, STOR_LOCL_INT_VAR, id);
-    }
-      break;
-
-    case STOR_CLS_INST_INT_VAR: {
-      const long id = ReadInt();
-      mthd_instrs[i] = new StackInstr(line_num, STOR_CLS_INST_INT_VAR, id);
+      MemoryContext mem_context = (MemoryContext)ReadInt();
+      mthd_instrs[i] = new StackInstr(line_num, 
+              mem_context == LOCL ? STOR_LOCL_INT_VAR : STOR_CLS_INST_INT_VAR, 
+              id, mem_context);
     }
       break;
 
@@ -538,16 +535,12 @@ void Loader::LoadStatements(StackMethod* method, bool is_debug)
     }
       break;
 
-    case COPY_LOCL_INT_VAR: {
+    case COPY_INT_VAR: {
       const long id = ReadInt();
-      mthd_instrs[i] = new StackInstr(line_num, COPY_LOCL_INT_VAR, id);
-    }
-      break;
-
-
-    case COPY_CLS_INST_INT_VAR: {
-      const long id = ReadInt();
-      mthd_instrs[i] = new StackInstr(line_num, COPY_CLS_INST_INT_VAR, id);
+      MemoryContext mem_context = (MemoryContext)ReadInt();
+      mthd_instrs[i] = new StackInstr(line_num, 
+              mem_context == LOCL ? COPY_LOCL_INT_VAR : COPY_CLS_INST_INT_VAR, 
+              id, mem_context);
     }
       break;
 
