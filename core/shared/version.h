0978ca88c27149ad412067b35baa54fea4c9ec18
Randy Hollines <objeck@gmail.com>
2020-10-17 09:55:30 -0700

    v5.4.3

--- core/shared/version.h
+++ core/shared/version.h
@@ -35,8 +35,8 @@
 #define MAGIC_NUM_WEB 0xdddf
 #define MAGIC_NUM_LIB 0xddde
 #define MAGIC_NUM_EXE 0xdddd
-#define VER_NUM 546
+#define VER_NUM 550
 
-#define VERSION_STRING L"5.4.6"
+#define VERSION_STRING L"5.5.0"
 
 #endif
