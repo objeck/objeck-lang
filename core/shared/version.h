74c5cadaec71febdc3c569fc0735016d109ce24b
Randy Hollines <objeck@gmail.com>
2020-12-17 08:12:42 -0800

    back to m1 port

--- core/shared/version.h
+++ core/shared/version.h
@@ -35,8 +35,8 @@
 #define MAGIC_NUM_WEB 0xdddf
 #define MAGIC_NUM_LIB 0xddde
 #define MAGIC_NUM_EXE 0xdddd
-#define VER_NUM 555
+#define VER_NUM 560
 
-#define VERSION_STRING L"5.5.5"
+#define VERSION_STRING L"5.6.0"
 
 #endif
