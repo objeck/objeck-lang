bb63f599760469eabf9f6c464ee31efbe5f1ddd7
Randy Hollines <objeck@gmail.com>
2020-12-26 15:15:33 -0800

    working on build

---
+++ core/release/deploy_macos_arm64.sh
@@ -0,0 +1,94 @@
+#!/bin/sh
+
+# setup directories
+rm -rf deploy
+mkdir deploy
+mkdir deploy/bin
+mkdir deploy/lib
+mkdir deploy/lib/sdl
+mkdir deploy/lib/sdl/fonts
+mkdir deploy/lib/native
+mkdir deploy/doc
+
+# build compiler
+cd ../compiler
+xcodebuild -project xcode/Compiler.xcodeproj build
+# make clean; make -j3 OBJECK_LIB_PATH=\\\".\\\"
+cp xcode/build/Release/obc ../release/deploy/bin
+cp ../lib/*.obl ../release/deploy/lib
+cp ../vm/misc/*.pem ../release/deploy/lib
+rm ../release/deploy/lib/gtk2.obl
+
+# build VM
+cd ../vm
+xcodebuild -project xcode/VM.xcodeproj build
+cp xcode/build/Release/obr ../release/deploy/bin
+
+# build debugger
+cd ../debugger
+: '
+if [ ! -z "$1" ] && [ "$1" = "32" ]; then
+	cp make/Makefile.32 Makefile
+elif [ ! -z "$1" ] && [ "$1" = "osx" ]; then
+	cp make/Makefile.OSX.64 Makefile
+else
+	cp make/Makefile.64 Makefile
+fi
+make clean; make -j3
+cp obd ../release/deploy/bin
+
+# build libraries
+
+cd ../lib/odbc
+if [ ! -z "$1" ] && [ "$1" = "osx" ]; then
+	./build_osx_x64.sh odbc
+	cp odbc.dylib ../../release/deploy/lib/native/libobjk_odbc.dylib
+else
+	./build_linux.sh odbc
+	cp odbc.so ../../release/deploy/lib/native/libobjk_odbc.so
+fi
+
+cd ../sdl
+if [ ! -z "$1" ] && [ "$1" = "osx" ]; then
+	./build_osx_x64.sh sdl
+	cp sdl.dylib ../../release/deploy/lib/native/libobjk_sdl.dylib
+else
+	./build_linux.sh sdl
+	cp sdl.so ../../release/deploy/lib/native/libobjk_sdl.so
+fi
+cp lib/fonts/*.ttf ../../release/deploy/lib/sdl/fonts
+'
+
+cd ../lib/openssl
+xcodebuild -project macos/xcode/objk_openssl.xcodeproj build
+cp macos/xcode/build/Release/libobjk_openssl.dylib ../../release/deploy/lib/native/libobjk_openssl.dylib
+
+# copy docs
+cd ../../..
+cp -R docs/syntax core/release/deploy/doc/syntax
+cp docs/readme.html core/release/deploy
+cp LICENSE core/release/deploy
+unzip docs/api.zip -d core/release/deploy/doc
+
+# copy examples
+mkdir core/release/deploy/examples
+mkdir core/release/deploy/examples/media
+cp programs/deploy/*.obs core/release/deploy/examples
+cp programs/deploy/media/*.png core/release/deploy/examples/media
+cp programs/deploy/media/*.wav core/release/deploy/examples/media
+cp -aR programs/doc core/release/deploy/examples
+cp -aR programs/tiny core/release/deploy/examples
+
+cd core/release
+
+# deploy
+if [ ! -z "$1" ] && [ "$1" = "deploy" ]; then
+	rm -rf ~/Desktop/objeck-lang
+	cp -rf ../release/deploy ~/Desktop/objeck-lang
+	cd ~/Desktop
+	
+	rm -f objeck.tar objeck.tgz
+	tar cf objeck.tar objeck-lang
+	gzip objeck.tar
+	mv objeck.tar.gz objeck.tgz	
+fi;
