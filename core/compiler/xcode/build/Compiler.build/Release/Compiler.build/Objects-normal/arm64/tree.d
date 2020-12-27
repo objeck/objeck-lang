5c58eb8a44d9d82dfb1205bd30f7bf10805111c1
Randy Hollines <objeck@gmail.com>
2020-12-27 10:20:24 -0800

    working on build for debugger

--- core/compiler/xcode/build/Compiler.build/Release/Compiler.build/Objects-normal/arm64/tree.d
+++
@@ -1,9 +0,0 @@
-dependencies: \
-  /Users/randyhollines/Documents/Code/objeck-lang/core/compiler/tree.cpp \
-  /Users/randyhollines/Documents/Code/objeck-lang/core/compiler/tree.h \
-  /Users/randyhollines/Documents/Code/objeck-lang/core/compiler/linker.h \
-  /Users/randyhollines/Documents/Code/objeck-lang/core/compiler/types.h \
-  /Users/randyhollines/Documents/Code/objeck-lang/core/compiler/../shared/instrs.h \
-  /Users/randyhollines/Documents/Code/objeck-lang/core/compiler/../shared/sys.h \
-  /Users/randyhollines/Documents/Code/objeck-lang/core/compiler/../shared/logger.h \
-  /Users/randyhollines/Documents/Code/objeck-lang/core/compiler/../shared/traps.h
