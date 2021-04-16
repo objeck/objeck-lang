806e44a76be12ee433b9db8e2b670d0d7d51d043
Randy Hollines <objeck@gmail.com>
2021-04-16 09:50:08 -0700

    v5.7.4

--- core/shared/version.h
+++ core/shared/version.h
@@ -35,8 +35,8 @@
 #define MAGIC_NUM_WEB 0xdddf
 #define MAGIC_NUM_LIB 0xddde
 #define MAGIC_NUM_EXE 0xdddd
-#define VER_NUM 573
+#define VER_NUM 574
 
-#define VERSION_STRING L"5.7.3"
+#define VERSION_STRING L"5.7.4"
 
 #endif
