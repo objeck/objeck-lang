e363bd60397a686db50bc5ebc4c9ebbc4491f9c6
Randy Hollines <objeck@gmail.com>
2021-04-25 14:26:32 -0700

    updating version

--- core/compiler/parser.cpp
+++ core/compiler/parser.cpp
@@ -3952,7 +3952,6 @@ void Parser::ParseAnonymousClass(MethodCall* method_call, int depth)
   NextToken();
 
   Class* klass = TreeFactory::Instance()->MakeClass(file_name, line_num, cls_name, method_call->GetVariableName(), interface_names);
-  klass->SetPublic(true);
 
   Class* prev_class = current_class;
   prev_method = current_method;
