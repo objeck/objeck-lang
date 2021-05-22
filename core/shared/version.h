e062d7d1e8c98115b9363d655f47c4a4a0254b39
Randy Hollines <objeck@gmail.com>
2021-05-22 14:10:37 -0700

    bumped version

--- core/shared/version.h
+++ core/shared/version.h
@@ -35,8 +35,8 @@
 #define MAGIC_NUM_WEB 0xdddf
 #define MAGIC_NUM_LIB 0xddde
 #define MAGIC_NUM_EXE 0xdddd
-#define VER_NUM 583
+#define VER_NUM 584
 
-#define VERSION_STRING L"5.8.3"
+#define VERSION_STRING L"5.8.4"
 
 #endif
