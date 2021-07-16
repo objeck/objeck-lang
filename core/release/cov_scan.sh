6ffaf62161d740ed346657d9ad2fecb45d69bdd9
Randy Hollines <objeck@gmail.com>
2021-07-15 21:25:21 -0700

    coverity scan

--- core/release/cov_scan.sh
+++ core/release/cov_scan.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 rm -rf /tmp/cov-int
 rm objeck-int.tgz
-/home/randy/Documents/Code/coverity/bin/cov-build --dir /tmp/cov-int ./deploy_posix.sh 64
+/home/objeck/Documents/Code/coverity/bin/cov-build --dir /tmp/cov-int ./deploy_posix.sh 64
 tar -czf objeck-int.tgz -C /tmp/ cov-int/
 curl --form token=ZmtAZyNCX5XLFzQLR9QMXg \
   --form email=objeck@gmail.com \
   --form file=@objeck-int.tgz \
-  --form version="v5.8.5" \
+  --form version="v5.9.7" \
   --form description="Objeck" \
   https://scan.coverity.com/builds?project=Objeck
