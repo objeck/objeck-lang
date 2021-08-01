e0469a7416d92c4166ede9eb849a15432785a742
Randy Hollines <objeck@gmail.com>
2021-08-01 09:53:37 -0700

    diag support

--- core/lib/diags/debug_deploy64.cmd
+++ core/lib/diags/debug_deploy64.cmd
@@ -2,5 +2,5 @@
 
 set PATH=%PATH%;..\..\release\deploy64\bin
 set OBJECK_LIB_PATH=..\..\release\deploy64\lib
-obc -src ..\..\compiler\lib_src\diag.obs -tar lib -dest %OBJECK_LIB_PATH%\diag.obl
+obc -src ..\..\compiler\lib_src\diags.obs -tar lib -dest %OBJECK_LIB_PATH%\diags.obl
 copy /y Debug\win64\libobjk_diags.dll %OBJECK_LIB_PATH%\native
