a4e4edff4606fc8da18a23b28cd339d8656a869b
Randy Hollines <objeck@gmail.com>
2021-08-08 15:02:14 -0700

    adding diagnostics library to build process

--- core/lib/diags/debug_deploy64.cmd
+++ core/lib/diags/debug_deploy64.cmd
@@ -4,6 +4,8 @@ set PATH=%PATH%;..\..\release\deploy64\bin
 set OBJECK_LIB_PATH=..\..\release\deploy64\lib
 obc -src ..\..\compiler\lib_src\diags.obs -tar lib -dest %OBJECK_LIB_PATH%\diags.obl
 copy /y %OBJECK_LIB_PATH%\diags.obl ..
-copy /y Debug\win64\libobjk_diags.dll %OBJECK_LIB_PATH%\native
+copy /y vs\Debug\x64\libobjk_diags.dll %OBJECK_LIB_PATH%\native
 
-REM obc -src ..\..\..\..\objeck-lang-server\server\src\parser.obs -lib gen_collect,json,diags -dest ..\..\..\..\objeck-lang-server\server\src\parser.obe
+pushd ..\..\..\..\objeck-lang-server\server\src
+call build_server.cmd
+popd
