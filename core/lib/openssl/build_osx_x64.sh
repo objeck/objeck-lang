43fe74a45e94cea970b3f16687aab6230fd20d1e
Randy Hollines <objeck@gmail.com>
2021-08-08 17:34:32 -0700

    diagnostics build support for intel mac

--- core/lib/openssl/build_osx_x64.sh
+++ core/lib/openssl/build_osx_x64.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 rm -rf *.o *.dylib
 # g++ -D_X64 -shared -fPIC -c -g -D_DEBUG -Wall $1.cpp
-g++ -D_X64 -D_OSX -shared -Wno-unused-function -Wno-deprecated-declarations -fPIC -c -O3 -Wall -Imacos/include $1.cpp
+g++ -D_X64 -D_OSX -fPIC -c -O3  -shared -Wno-unused-function -Wno-deprecated-declarations -Wall -Imacos/include $1.cpp
 g++ -D_X64 -D_OSX -dynamiclib -L/usr/local/lib -Wl,-headerpad_max_install_names,-undefined,dynamic_lookup,-compatibility_version,1.0,-current_version,1.0 -o $1.dylib $1.o
