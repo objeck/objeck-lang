cd5f4d4f8e520478cf60088866ee959e42b4c3e6
Randy Hollines <objeck@gmail.com>
2021-08-12 02:27:11 -0700

    code clean up

--- core/lib/diags/build_linux.sh
+++ core/lib/diags/build_linux.sh
@@ -2,7 +2,7 @@
 rm -rf *.o
 rm -rf *.so
 
-g++ -O3 -D_DIAG_LIB -std=c++11 -Wall -Wno-unused-function -fPIC -c *$1.cpp ../../compiler/scanner.cpp ../../compiler/parser.cpp ../../compiler/context.cpp ../../compiler/tree.cpp ../../compiler/types.cpp
+g++ -O3 -D_DIAG_LIB -std=c++11 -Wall -Wno-unused-function -fPIC -c *$1.cpp ../../compiler/scanner.cpp ../../compiler/parser.cpp ../../compiler/context.cpp ../../compiler/tree.cpp ../../compiler/types.cpp ../../compiler/linker.cpp
 g++ -O3 -shared -Wl,-soname,$1.so.1 -o $1.so *.o -lssl -lcrypto -ldl -lz -pthread
 
 # g++ -f -D_DIAG_LIB -std=c++11 -Wall -Wno-unused-function -fPIC -c *$1.cpp ../../compiler/scanner.cpp ../../compiler/parser.cpp ../../compiler/context.cpp ../../compiler/tree.cpp ../../compiler/types.cpp
