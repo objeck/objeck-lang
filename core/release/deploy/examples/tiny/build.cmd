63c06b12567e8cd44b2813f0ec9ff5a84ccae5ef
Randy Hollines <objeck@gmail.com>
2021-08-21 17:38:55 -0700

    Delete core/release/deploy directory

--- core/release/deploy/examples/tiny/build.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-del /q *.obe
-obc -src compiler\scanner.obs,compiler\parser.obs,compiler\compiler.obs,compiler\emitter.obs,common\instructions.obs -lib collect.obl -dest compiler.obe
-obc -src vm\interpreter.obs,common\instructions.obs -lib collect.obl -dest vm.obe
