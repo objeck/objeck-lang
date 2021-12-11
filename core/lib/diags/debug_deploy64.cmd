a45aef811bad9e087fa85d34f94aa8105aad4cab
Randy Hollines <objeck@gmail.com>
2021-12-11 00:23:57 -0800

    code clean up

--- core/lib/diags/debug_deploy64.cmd
+++ core/lib/diags/debug_deploy64.cmd
@@ -6,6 +6,6 @@ obc -src ..\..\compiler\lib_src\diags.obs -tar lib -dest %OBJECK_LIB_PATH%\diags
 copy /y %OBJECK_LIB_PATH%\diags.obl ..
 copy /y vs\Debug\x64\libobjk_diags.dll %OBJECK_LIB_PATH%\native
 
-pushd ..\..\..\..\objeck-lang-server\server
+pushd ..\..\..\..\objeck-lsp\server
 call build_server.cmd
 popd
