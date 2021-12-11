c7df19159c9147904ee283f82f11b46f995e4cd5
Randy Hollines <objeck@gmail.com>
2021-12-10 20:17:02 -0800

    moving web server code into it's own project

--- programs/server/keygen.sh
+++
@@ -1,9 +0,0 @@
-rm -f local.* *.pem *.crt
-openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes \
-  -keyout local.key -out local.crt -subj "/CN=localhost" \
-  -addext "subjectAltName=DNS:localhost,DNS:www.local.net,IP:127.0.0.1"
-openssl x509 -in local.crt -out cert.pem
-openssl rsa -aes256 -in local.key -out local.encrypted.key
-mv local.encrypted.key cert.key
-rm local.crt local.key
-chmod 600 cert.key
