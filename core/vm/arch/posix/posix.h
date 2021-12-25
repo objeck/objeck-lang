d0cb0d9b04a561055b136a19f8ac9834e0c0ef84
Randy Hollines <objeck@gmail.com>
2021-12-25 14:51:35 -0800

    fixing sa findings

--- core/vm/arch/posix/posix.h
+++ core/vm/arch/posix/posix.h
@@ -231,6 +231,7 @@ class IPSocket {
   static SOCKET Open(const char* address, int port) {
 		addrinfo* addr;
 		if(getaddrinfo(address, nullptr, nullptr, &addr)) {
+			close(sock);
 			return -1;
 		}
 
