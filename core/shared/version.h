1223ee754af597eff94bb5f4f16db2259f01f31d
Randy Hollines <objeck@gmail.com>
2022-03-14 16:34:21 -0700

    updating version

--- core/shared/version.h
+++ core/shared/version.h
@@ -35,8 +35,8 @@
 #define MAGIC_NUM_WEB 0xdddf
 #define MAGIC_NUM_LIB 0xddde
 #define MAGIC_NUM_EXE 0xdddd
-#define VER_NUM 631
+#define VER_NUM 632
 
-#define VERSION_STRING L"6.3.1"
+#define VERSION_STRING L"6.3.2"
 
 #endif
