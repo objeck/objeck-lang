d21ab5826bcf8f04eff1f12d27043069a7973e38
Randy Hollines <objeck@gmail.com>
2022-04-01 18:30:46 -0700

    v6.3.3

--- core/release/deploy_all.cmd
+++ core/release/deploy_all.cmd
@@ -1,3 +1,16 @@
- call deploy_ia32.cmd %1 %2
- call deploy_amd64.cmd %1 %2
- call deploy_arm32.cmd %1 %2
+REM build targets
+call deploy_ia32.cmd %1 %2
+call deploy_amd64.cmd %1 %2
+call deploy_arm32.cmd %1 %2
+
+REM build LSP
+setlocal
+rmdir /q /s "%USERPROFILE%\Desktop\objeck-lsp"
+mkdir "%USERPROFILE%\Desktop\objeck-lsp"
+set CUR_DIR=%CD%
+set PATH=%CUR_DIR%\deploy64\bin;%PATH%
+set OBJECK_LIB_PATH=%CUR_DIR%\deploy64\lib
+pushd ..\..\..\objeck-lsp
+call deploy_lsp.cmd deploy
+xcopy objeck-lsp-*.zip "%USERPROFILE%\Desktop\objeck-lsp"
+popd
