da2ba5857ad63f4611177796b211931d4d040333
Randy Hollines <objeck@gmail.com>
2022-04-15 20:52:07 -0700

    bug fix for unboxing IntHolder as array index in nested method call

--- programs/tiny/build.cmd
+++ programs/tiny/build.cmd
@@ -1,4 +1,4 @@
 @echo off
 del /q *.obe
-obc -src compiler\scanner.obs,compiler\parser.obs,compiler\compiler.obs,compiler\emitter.obs,common\instructions.obs -lib collect.obl -dest compiler.obe
-obc -src vm\interpreter.obs,common\instructions.obs -lib collect.obl -dest vm.obe
+obc -src compiler\scanner.obs,compiler\parser.obs,compiler\compiler.obs,compiler\emitter.obs,common\instructions.obs -dest compiler.obe
+obc -src vm\interpreter.obs,common\instructions.obs -dest vm.obe
