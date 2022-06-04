a09c4244010979e96a39ba840eb3daed5a10266d
Randy Hollines <objeck@gmail.com>
2022-06-04 08:35:05 -0700

    adding json 'IsNull()' check

--- core/shared/version.h
+++ core/shared/version.h
@@ -35,8 +35,8 @@
 #define MAGIC_NUM_WEB 0xdddf
 #define MAGIC_NUM_LIB 0xddde
 #define MAGIC_NUM_EXE 0xdddd
-#define VER_NUM 643
+#define VER_NUM 644
 
-#define VERSION_STRING L"6.4.3"
+#define VERSION_STRING L"6.4.4"
 
 #endif
