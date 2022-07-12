ccd73f8e1519d45cef75b96f9f32fc105facd222
Randy Hollines <objeck@gmail.com>
2022-07-12 08:53:23 -0700

    added SetLocale

--- core/release/update_version.ps1
+++ core/release/update_version.ps1
@@ -1,5 +1,5 @@
 # version string
-$version = "6.5.2"
+$version = "6.5.3"
 
 # alternative strings
 $version_number = $version.Replace(".", "")
--- core/shared/version.h
+++ core/shared/version.h
@@ -35,8 +35,8 @@
 #define MAGIC_NUM_WEB 0xdddf
 #define MAGIC_NUM_LIB 0xddde
 #define MAGIC_NUM_EXE 0xdddd
-#define VER_NUM 652
+#define VER_NUM 653
 
-#define VERSION_STRING L"6.5.2"
+#define VERSION_STRING L"6.5.3"
 
 #endif
