84e4ae524fd8d04d4032b67d985b6dc733938828
Randy Hollines <objeck@gmail.com>
2022-09-03 03:22:48 -0700

    code clean up

--- core/compiler/arm_compiler/arm_compiler/toolchain.cmake
+++
@@ -1,4 +0,0 @@
-SET(CMAKE_SYSTEM_NAME Linux)
-SET(CMAKE_C_COMPILER "${TOOLCHAIN_ROOT}/bin/arm-linux-gnueabihf-gcc.exe")
-SET(CMAKE_CXX_COMPILER "${TOOLCHAIN_ROOT}/bin/arm-linux-gnueabihf-g++.exe")
-SET(CMAKE_SYSROOT "$ENV{TOOLCHAIN_ROOT}/arm-linux-gnueabihf/sysroot")
