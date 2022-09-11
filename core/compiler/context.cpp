fb240d2c72de84c02854dc81ec00d95f081688e1
Randy Hollines <objeck@gmail.com>
2022-09-11 16:53:34 -0700

    bumped version, fixing issue #66

--- core/compiler/context.cpp
+++ core/compiler/context.cpp
@@ -4587,8 +4587,8 @@ void ContextAnalyzer::AnalyzeCalculationCast(CalculatedExpression* expression, c
   }
 
   if(!IsScalar(left_expr) || !IsScalar(right_expr)) {
-    if(right->GetType() != NIL_TYPE && expression->GetExpressionType() != EQL_EXPR && expression->GetExpressionType() != NEQL_EXPR) {
-      ProcessError(left_expr, L"Invalid array calculation");
+    if(!(right->GetType() == NIL_TYPE && (expression->GetExpressionType() == EQL_EXPR || expression->GetExpressionType() == NEQL_EXPR))) {
+      ProcessError(left_expr, L"Invalid array based calculation");
     }
   }
   else {
--- core/release/update_version.ps1
+++ core/release/update_version.ps1
@@ -1,5 +1,5 @@
 # version string
-$version = "6.6.5"
+$version = "6.6.6"
 
 # alternative strings
 $version_number = $version.Replace(".", "")
