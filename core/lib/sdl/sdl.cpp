e51fa23bf249cabea9fdffbf32b175ac8d2229de
Randy Hollines <objeck@gmail.com>
2022-09-18 12:09:01 -0700

    updating sdl2 bindings

--- core/lib/sdl/sdl.cpp
+++ core/lib/sdl/sdl.cpp
@@ -3205,12 +3205,19 @@ extern "C" {
 #ifdef _WIN32
   __declspec(dllexport)
 #endif
-    void sdl_gamecontroller_get_button(VMContext& context) {
+  void sdl_gamecontroller_get_button(VMContext& context) {
     SDL_GameController* gamecontroller = (SDL_GameController*)APITools_GetIntValue(context, 1);
     const SDL_GameControllerButton button = (SDL_GameControllerButton)APITools_GetIntValue(context, 2);
     APITools_SetIntValue(context, 0, SDL_GameControllerGetButton(gamecontroller, button));
   }
 
+#ifdef _WIN32
+  __declspec(dllexport)
+#endif
+  void sdl_gamecontroller_get_num_touchpads(VMContext& context) {
+    SDL_GameController* gamecontroller = (SDL_GameController*)APITools_GetIntValue(context, 1);
+    APITools_SetIntValue(context, 0, SDL_GameControllerGetNumTouchpads(gamecontroller));
+  }
 
 
 
