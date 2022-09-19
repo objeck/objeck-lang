5761fdfedadf69176296707edfbbda9b5c8c5568
Randy Hollines <objeck@gmail.com>
2022-09-19 09:00:57 -0700

    bumped version

--- programs/sdl/code_gen/brun.cmd
+++ programs/sdl/code_gen/brun.cmd
@@ -1,3 +1,5 @@
 del code_gen.obe
 obc -src sdl_emitter.obs,sdl_parser.obs,sdl_scanner.obs -lib collect.obl -dest code_gen.obe
-obr code_gen.obe Renderer D:\Temp\SDL2_gfx-1.0.4\SDL2_gfxPrimitives.h
+IF NOT "%~1"=="" IF NOT "%~2"==""  (
+	obr code_gen.obe %1 %2
+)
