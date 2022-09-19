84e0b420eee175b9efcf8b120f24e61e252b48f7
Randy Hollines <objeck@gmail.com>
2022-09-19 10:08:05 -0700

    v6.7.1

--- programs/sdl/code_gen/brun.cmd
+++ programs/sdl/code_gen/brun.cmd
@@ -1,5 +1,8 @@
+@echo off
+
 del code_gen.obe
-obc -src sdl_emitter.obs,sdl_parser.obs,sdl_scanner.obs -lib collect.obl -dest code_gen.obe
-IF NOT "%~1"=="" IF NOT "%~2"==""  (
+
+obc -src sdl_emitter.obs,sdl_parser.obs,sdl_scanner.obs -dest code_gen.obe
+IF NOT "%~1"=="" IF NOT "%~2"=="" (
 	obr code_gen.obe %1 %2
 )
