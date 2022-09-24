ccaa782ca8ccad63a79308aec9e91d746a918ffc
Randy Hollines <objeck@gmail.com>
2022-09-24 14:43:12 -0700

    added native app launcher 'process'

--- core/native_launcher/native_launcher.cpp
+++ core/native_launcher/native_launcher.cpp
@@ -1,7 +1,60 @@
+/***************************************************************************
+ * Native executable launcher for 
+ *
+ * Copyright (c) 2022, Randy Hollines
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in
+ * the documentation and/or other materials provided with the distribution.
+ * - Neither the name of the Objeck team nor the names of its
+ * contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ***************************************************************************/
+
 #include "native_launcher.h"
 
 int main(int argc, char* argv[])
 {
-  const char* path = ".\\runtime\\bin\\obr.exe";
-  const char* args[] = { path, ".\\app\\app.obe" , nullptr };
+  const char* spawn_path = ".\\runtime\\bin\\obr.exe";
+  const char* spawn_args[] = { spawn_path, ".\\app\\app.obe" , nullptr };
+
+  const string working_dir = GetWorkingDirectory();
+  if(working_dir.empty()) {
+    cerr << ">>> Unable to find the working directory <<<" << endl;
+    return 1;
+  }
+
+  const string path_env = GetEnviromentPath(working_dir);
+  const string lib_env = GetLibraryPath(working_dir);
+  if(path_env.empty() || lib_env.empty()) {
+    cout << ">>> Unable to determine the current working directory <<<" << endl;
+    return 1;
+  }
+
+  const char* spawn_env[] = { path_env.c_str(), lib_env.c_str(), nullptr };
+
+  // _spawnve(P_WAIT, spawn_path, spawn_args, spawn_env);
+
+
+ // cout << "env_path=|" << env_path << "|, lib_path=|" << lib_path << '|' << endl;
+
+  return 0;
 }
--- core/native_launcher/native_launcher.h
+++ core/native_launcher/native_launcher.h
@@ -1,3 +1,34 @@
+/***************************************************************************
+ * Native executable launcher for
+ *
+ * Copyright (c) 2022, Randy Hollines
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in
+ * the documentation and/or other materials provided with the distribution.
+ * - Neither the name of the Objeck team nor the names of its
+ * contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ***************************************************************************/
+
 #ifndef __NATIVE_LAUNCHER__
 #define __NATIVE_LAUNCHER__
 
@@ -5,6 +36,7 @@
 #include <iostream>
 
 #ifdef _WIN32
+#include "process.h"
 #include "windows.h"
 #endif
 
@@ -94,6 +126,33 @@ static const string GetWorkingDirectory() {
   }
 
   return "";
+}
+
+static const string GetEnviromentPath(const string& working_dir) {
+  char* cur_env = nullptr;
+
+#ifdef _WIN32
+  size_t cur_env_len;
+  _dupenv_s(&cur_env, &cur_env_len, "PATH");
+  if(cur_env) {
+    return "PATH=" + string(cur_env) + ';' + working_dir + "\\runtime\\bin" + ';' + working_dir + "\\runtime\\lib\\native";
+  }
+#else
+  cur_env = getenv("PATH");
+  if(cur_env) {
+    return "PATH=" + string(cur_env) + ';' + working_dir + "/runtime/bin" + ';' + working_dir + "/runtime/lib/native";
+  }
+#endif
+
+  return "";
+}
+
+static const string GetLibraryPath(const string& working_dir) {
+#ifdef _WIN32
+  return "OBJECK_LIB_PATH=" + working_dir + "\\runtime\\lib";
+#else
+  return "OBJECK_LIB_PATH=" + working_dir + "/runtime/lib";
+#endif
 }
 
 #endif
