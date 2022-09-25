fac57b999dbca732eb92c2371a4d0de2e2fea27a
Randy Hollines <objeck@gmail.com>
2022-09-24 20:13:22 -0700

    native lanucher, linux testing

--- core/native_launcher/native_launcher.cpp
+++ core/native_launcher/native_launcher.cpp
@@ -64,7 +64,7 @@ int main(int argc, char* argv[])
 #endif
   char* spawn_env[] = { path_env_ptr, lib_env_ptr, nullptr };
 
-  // cout << "spawn_path=|" << spawn_path << "|\nenv_path=|" << path_env << "|\nlib_env=|" << lib_env << '|' << endl;
+cout << "spawn_path=|" << spawn_path << "|\nenv_path=|" << path_env << "|\nlib_env=|" << lib_env << '|' << endl;
   const int status = Spawn(spawn_path.c_str(), spawn_args, spawn_env);
 
   free(path_env_ptr);
