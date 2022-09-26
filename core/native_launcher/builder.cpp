bcc1e9567a7cbecd3da84ccc6074dc0a1eb20343
Randy Hollines <objeck@gmail.com>
2022-09-26 14:03:47 -0700

    adding obn and obb tools

--- core/native_launcher/builder.cpp
+++ core/native_launcher/builder.cpp
@@ -34,67 +34,143 @@
 
 int main(int argc, char* argv[])
 {
-  if(argc == 3) {
-    const wstring from_base_dir = BytesToUnicode(argv[1]);
-    const wstring to_base_dir = BytesToUnicode(argv[2]) + fs::path::preferred_separator + L"app";
+  map<const wstring, wstring> cmd_params = ParseCommnadLine(argc, argv);
+
+  list<wstring> argument_options;
+  for(map<const wstring, wstring>::iterator intr = cmd_params.begin(); intr != cmd_params.end(); ++intr) {
+    argument_options.push_back(intr->first);
+  }
+
+  // check program target
+  wstring runtime_base_dir;
+  map<const wstring, wstring>::iterator result = cmd_params.find(L"install");
+  if(result != cmd_params.end()) {
+    runtime_base_dir = result->second;
+    argument_options.remove(L"install");
+  }
+
+  wstring to_base_dir;
+  result = cmd_params.find(L"to_dir");
+  if(result != cmd_params.end()) {
+    to_base_dir = result->second;
+    argument_options.remove(L"to_dir");
+  }
+
+  wstring to_name;
+  result = cmd_params.find(L"to_name");
+  if(result != cmd_params.end()) {
+    to_name = result->second;
+    argument_options.remove(L"to_name");
+  }
+
+  wstring src_obe_file;
+  result = cmd_params.find(L"src");
+  if(result != cmd_params.end()) {
+    src_obe_file = result->second;
+    if(!EndsWith(src_obe_file, L".obe")) {
+      wcout << GetUsage() << endl;
+      exit(1);
+    }
+    argument_options.remove(L"src");
+  }
+
+  if(argument_options.empty() && cmd_params.size() == 4) {
+    to_base_dir += fs::path::preferred_separator;
+    to_base_dir += to_name;
 
     try {
-      if(!fs::exists(to_base_dir)) {
-        fs::create_directory(to_base_dir);
+      bool is_ok = true;
 
-        fs::create_directory(to_base_dir + fs::path::preferred_separator + L"app");
-        fs::create_directory(to_base_dir + fs::path::preferred_separator + L"runtime");
-
-        // copy 'bin'
-        fs::path from_bin_path(from_base_dir + fs::path::preferred_separator + L"bin");
-
-        const wstring to_bin_str = to_base_dir + fs::path::preferred_separator + L"runtime" + fs::path::preferred_separator + L"bin";
-        fs::path to_bin_path(to_bin_str);
-
-        fs::copy(from_bin_path, to_bin_path, fs::copy_options::overwrite_existing | fs::copy_options::recursive);
-
-#ifdef _WIN32
-        fs::remove(to_bin_str + fs::path::preferred_separator + L"obc.exe");
-        fs::remove(to_bin_str + fs::path::preferred_separator + L"obd.exe");
-        fs::remove(to_bin_str + fs::path::preferred_separator + L"obb.exe");
-#else
-        fs::path renove(to_bin_str + fs::path::preferred_separator + L"obc");
-        fs::path renove(to_bin_str + fs::path::preferred_separator + L"obd");
-        fs::path renove(to_bin_str + fs::path::preferred_separator + L"obb");
-#endif
-
-        // copy 'lib'
-        fs::path from_lib_path(from_base_dir + fs::path::preferred_separator + L"lib");
-        fs::path to_lib_path(to_base_dir + fs::path::preferred_separator + L"runtime" + fs::path::preferred_separator + L"lib");
-        fs::copy(from_lib_path, to_lib_path, fs::copy_options::overwrite_existing | fs::copy_options::recursive);
-        remove_all_file_types(to_lib_path, L".obl");
-
-        // copy executable and configuration
-        fs::path from_lib_misc_path_obn(from_base_dir + fs::path::preferred_separator + L"lib" + 
-                                        fs::path::preferred_separator + L"native" + 
-                                        fs::path::preferred_separator + L"misc" +
-#ifdef _WIN32
-                                        fs::path::preferred_separator + L"obn.exe");
-#else
-                                        fs::path::preferred_separator + L"obn");
-#endif
-        fs::copy(from_lib_misc_path_obn, to_base_dir);
-
-        fs::path from_lib_misc_path_prop(from_base_dir + fs::path::preferred_separator + L"lib" +
-                                        fs::path::preferred_separator + L"native" +
-                                        fs::path::preferred_separator + L"misc" +
-                                        fs::path::preferred_separator + L"config.prop");
-        fs::copy(from_lib_misc_path_prop, to_base_dir);
+      if(!fs::exists(src_obe_file)) {
+        is_ok = false;
+        cerr << ">>> Unable to find soruce *.obe file <<<" << endl;
       }
-      else {
-        cerr << ">>> File or directory already exists <<<" << endl;
+
+      if(fs::exists(to_base_dir)) {
+        cerr << ">>> Target directory already exists <<<" << endl;
+        is_ok = false;
+      }
+
+      if(!is_ok) {
         exit(1);
       }
+
+      fs::create_directory(to_base_dir);
+
+      fs::create_directory(to_base_dir + fs::path::preferred_separator);
+      fs::create_directory(to_base_dir + fs::path::preferred_separator + L"runtime");
+      
+      fs::path to_app_path(to_base_dir + fs::path::preferred_separator + L"app");
+      fs::create_directory(to_app_path);
+
+      // copy 'bin'
+      fs::path from_bin_path(runtime_base_dir + fs::path::preferred_separator + L"bin");
+
+      const wstring to_bin_str = to_base_dir + fs::path::preferred_separator + L"runtime" + fs::path::preferred_separator + L"bin";
+      fs::path to_bin_path(to_bin_str);
+
+      fs::copy(from_bin_path, to_bin_path, fs::copy_options::overwrite_existing | fs::copy_options::recursive);
+
+#ifdef _WIN32
+      fs::remove(to_bin_str + fs::path::preferred_separator + L"obc.exe");
+      fs::remove(to_bin_str + fs::path::preferred_separator + L"obd.exe");
+      fs::remove(to_bin_str + fs::path::preferred_separator + L"obb.exe");
+#else
+      fs::path renove(to_bin_str + fs::path::preferred_separator + L"obc");
+      fs::path renove(to_bin_str + fs::path::preferred_separator + L"obd");
+      fs::path renove(to_bin_str + fs::path::preferred_separator + L"obb");
+#endif
+
+      // copy 'lib'
+      fs::path from_lib_path(runtime_base_dir + fs::path::preferred_separator + L"lib");
+      fs::path to_lib_path(to_base_dir + fs::path::preferred_separator + L"runtime" + fs::path::preferred_separator + L"lib");
+      fs::copy(from_lib_path, to_lib_path, fs::copy_options::overwrite_existing | fs::copy_options::recursive);
+      remove_all_file_types(to_lib_path, L".obl");
+
+      // copy executable and configuration
+      fs::path from_lib_misc_path_obn(runtime_base_dir + fs::path::preferred_separator + L"lib" +
+                                      fs::path::preferred_separator + L"native" +
+                                      fs::path::preferred_separator + L"misc" +
+#ifdef _WIN32
+                                      fs::path::preferred_separator + L"obn.exe");
+#else
+        fs::path::preferred_separator + L"obn");
+#endif
+      fs::copy(from_lib_misc_path_obn, to_base_dir);
+
+      fs::path from_lib_misc_path_prop(runtime_base_dir + fs::path::preferred_separator + L"lib" +
+                                       fs::path::preferred_separator + L"native" +
+                                       fs::path::preferred_separator + L"misc" +
+                                       fs::path::preferred_separator + L"config.prop");
+      fs::copy(from_lib_misc_path_prop, to_base_dir);
+
+      // copy app
+      fs::path to_obe_file(to_app_path);
+      to_obe_file += fs::path::preferred_separator;
+      to_obe_file += L"app.obe";
+      fs::copy(src_obe_file, to_obe_file);
+
+      // rename binary
+      fs::path from_exe_file(to_base_dir);
+      from_exe_file += fs::path::preferred_separator;
+      from_exe_file += L"obn.exe";
+
+      fs::path to_exe_file(to_base_dir);
+      to_exe_file += fs::path::preferred_separator;
+      to_exe_file += to_name + L".exe";
+
+      fs::rename(from_exe_file, to_exe_file);
+
+      wcout << L"Successfully created native runtime for: '" + src_obe_file + L"' in '" + to_base_dir + L"'\n---" << endl;
     }
     catch(std::exception& e) {
       cerr << e.what() << endl;
     }
   }
-
+  else {
+    wcout << GetUsage() << endl;
+    exit(1);
+  }
+  
   return 0;
 }
