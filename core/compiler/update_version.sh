7ec94140f920879f5ab16e25f2828e65e75e7c0c
Randy Hollines <objeck@gmail.com>
2022-10-09 22:55:08 -0700

    updating build for linux amd64

--- core/compiler/update_version.sh
+++ core/compiler/update_version.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-make -f make/Makefile.SYS.64 clean
-make -f make/Makefile.SYS.64
+make -f make/Makefile.sys.amd64 clean
+make -f make/Makefile.sys.amd64
 ./obc -src lib_src/lang.obs -tar lib -opt s2 -dest ../lib/lang.obl -strict
-make -f make/Makefile.64 clean
-make -f make/Makefile.64
+make -f make/Makefile.amd64 clean
+make -f make/Makefile.amd64
 ./obc -src lib_src/gen_collect.obs -lib ../lib/lang -tar lib -opt s3 -dest ../lib/gen_collect.obl -strict
 ./obc -src lib_src/diags.obs -lib gen_collect -tar lib -opt s3 -dest ../lib/diags.obl
 ./obc -src lib_src/misc.obs -lib gen_collect -tar lib -opt s3 -dest ../lib/misc.obl
