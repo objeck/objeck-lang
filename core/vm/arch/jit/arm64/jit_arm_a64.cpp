fc4881d2ad20447aae6c61fc9d6f08dcc47a2edc
Randy Hollines <objeck@gmail.com>
2022-10-10 20:04:31 -0700

    minor code clean up

--- core/vm/arch/jit/arm64/jit_arm_a64.cpp
+++ core/vm/arch/jit/arm64/jit_arm_a64.cpp
@@ -3903,12 +3903,10 @@ RegisterHolder* JitArm64::ArrayIndex(StackInstr* instr, MemoryType type)
   const long dim = instr->GetOperand();
   for(int i = 1; i < dim; ++i) {
     // index *= array[i];
-    mul_mem_reg((i + 2) * sizeof(size_t), array_holder->GetRegister(), index_holder->GetRegister());
-    if(holder) {
-      delete holder;
-      holder = nullptr;
-    }
-
+    mul_mem_reg((i + 2) * sizeof(size_t), array_holder->GetRegister(), index_holder->GetRegister());    
+    delete holder;
+    holder = nullptr;
+    
     holder = working_stack.front();
     working_stack.pop_front();
     switch(holder->GetType()) {
