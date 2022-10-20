27be90ca930ceea8fc34c89dd97d61fa7ecc342a
Randy Hollines <objeck@gmail.com>
2022-10-20 11:58:07 -0400

    fixing lsp renamming bug for class and instance variables, need to clean up code

--- core/lib/diags/diags.h
+++ core/lib/diags/diags.h
@@ -75,6 +75,6 @@ enum ResultPosition {
 size_t* FormatErrors(VMContext& context, const vector<wstring>& error_strings, const vector<wstring>& warning_strings = vector<wstring>());
 void GetTypeName(frontend::Type* type, wstring &output);
 size_t* GetExpressionsCalls(VMContext& context, frontend::ParsedProgram* program, const wstring uri, const int line_num, const int line_pos, const wstring lib_path);
-vector<frontend::Expression*> FindAllExpressions(const int line_num, const int line_pos, frontend::Class* klass, class ContextAnalyzer& analyzer, bool is_mthd);
+vector<frontend::Expression*> FindAllExpressions(const int line_num, const int line_pos, frontend::Class* klass, class ContextAnalyzer& analyzer);
 
 #endif
