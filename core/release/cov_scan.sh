80986aa13cd1c4035d5ea76ee5a1e3ae98884e29
Randy Hollines <objeck@gmail.com>
2022-11-05 10:31:01 -0700

    scan update

--- core/release/cov_scan.sh
+++ core/release/cov_scan.sh
@@ -6,6 +6,6 @@ tar -czf objeck-int.tgz -C /tmp/ cov-int/
 curl --form token=ZmtAZyNCX5XLFzQLR9QMXg \
   --form email=objeck@gmail.com \
   --form file=@objeck-int.tgz \
-  --form version="v6.8.5" \
+  --form version="v6.8.6" \
   --form description="Objeck" \
   https://scan.coverity.com/builds?project=Objeck
