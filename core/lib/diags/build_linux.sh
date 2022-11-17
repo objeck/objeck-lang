37d83d5492965168e2d5a8552f4c2739aa13cd08
Randy Hollines <objeck@gmail.com>
2022-11-17 06:34:48 -0800

    updated msys2 makefiles

--- core/lib/diags/build_linux.sh
+++ core/lib/diags/build_linux.sh
@@ -2,8 +2,5 @@
 rm -rf *.o
 rm -rf *.so
 
-g++ -O3 -D_DIAG_LIB -std=c++11 -Wall -Wno-unused-function -fPIC -c *$1.cpp ../../compiler/scanner.cpp ../../compiler/parser.cpp ../../compiler/context.cpp ../../compiler/tree.cpp ../../compiler/types.cpp ../../compiler/linker.cpp
+g++ -O3 -D_DIAG_LIB -std=c++17 -Wall -Wno-unused-function -fPIC -c *$1.cpp ../../compiler/scanner.cpp ../../compiler/parser.cpp ../../compiler/context.cpp ../../compiler/tree.cpp ../../compiler/types.cpp ../../compiler/linker.cpp
 g++ -O3 -shared -Wl,-soname,$1.so.1 -o $1.so *.o -lssl -lcrypto -ldl -lz -pthread
-
-# g++ -f -D_DIAG_LIB -std=c++11 -Wall -Wno-unused-function -fPIC -c *$1.cpp ../../compiler/scanner.cpp ../../compiler/parser.cpp ../../compiler/context.cpp ../../compiler/tree.cpp ../../compiler/types.cpp
-# g++ -g -shared -Wl,-soname,$1.so.1 -o $1.so *.o -lssl -lcrypto -ldl -lz -pthread
