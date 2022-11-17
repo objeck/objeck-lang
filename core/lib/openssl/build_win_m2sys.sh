5bc0ab324bfdad951ebbaf02a2625475bc880232
Randy Hollines <objeck@gmail.com>
2022-11-17 05:36:12 -0800

    updated msys2 makefiles

--- core/lib/openssl/build_win_m2sys.sh
+++ core/lib/openssl/build_win_m2sys.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 rm -rf *.o
-rm -rf *.so
+rm -rf *.dll
 
 g++ -O3 -Wno-unused-function -std=c++11 -Wall -fPIC -c *$1.cpp
-g++ -O3 -shared -Wl,-soname,$1.so.1 -o $1.so *.o -lssl -lcrypto
+g++ -O3 -shared -Wl,-soname,$1.so.1 -o $1.dll *.o -lssl -lcrypto
