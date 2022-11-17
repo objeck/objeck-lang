50bd80e0e418f7fe5711ba7d3c75d0fefa1a0d6f
Randy Hollines <objeck@gmail.com>
2022-11-17 05:57:55 -0800

    updated msys2 makefiles

--- core/lib/odbc/build_win_msys2.sh
+++ core/lib/odbc/build_win_msys2.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 rm -rf *.o
-rm -rf *.so
+rm -rf *.dll
 
 g++ -O3 -Wno-int-to-pointer-cast -Wno-unused-function -Wall -std=c++17 -fPIC -c *$1.cpp
-g++ -O3 -shared -Wl,-soname,$1.so.1 -o $1.so *.o -lodbc
+g++ -O3 -shared -Wl,-soname,$1.dll.1 -o $1.dll *.o -lodbc
