f76b240247a2fbd83f31e3da2e4129fec5df3c34
Randy Hollines <objeck@gmail.com>
2022-11-23 01:23:22 -0800

    updating msys2 build files

--- core/release/deploy_msys2-ucrt.sh
+++ core/release/deploy_msys2-ucrt.sh
@@ -1,33 +1,33 @@
 #!/bin/sh
 
 # setup directories
-rm -rf deploy
-mkdir deploy
-mkdir deploy/bin
-mkdir deploy/lib
-mkdir deploy/lib/sdl
-mkdir deploy/lib/sdl/fonts
-mkdir deploy/lib/native
-mkdir deploy/lib/msys2-ucrt64
-mkdir deploy/lib/native/misc
-mkdir deploy/doc
+rm -rf deploy-msys2-ucrt
+mkdir deploy-msys2-ucrt
+mkdir deploy-msys2-ucrt/bin
+mkdir deploy-msys2-ucrt/lib
+mkdir deploy-msys2-ucrt/lib/sdl
+mkdir deploy-msys2-ucrt/lib/sdl/fonts
+mkdir deploy-msys2-ucrt/lib/native
+mkdir deploy-msys2-ucrt/lib/msys2-ucrt64
+mkdir deploy-msys2-ucrt/lib/native/misc
+mkdir deploy-msys2-ucrt/doc
 
 # build compiler
 cd ../compiler
 cp make/Makefile.msys2-ucrt.amd64 Makefile
 
 make clean; make -j3 OBJECK_LIB_PATH=///".///"
-cp obc ../release/deploy/bin
-cp ../lib/*.obl ../release/deploy/lib
-cp ../vm/misc/*.pem ../release/deploy/lib
-rm ../release/deploy/lib/gtk2.obl
+cp obc ../release/deploy-msys2-ucrt/bin
+cp ../lib/*.obl ../release/deploy-msys2-ucrt/lib
+cp ../vm/misc/*.pem ../release/deploy-msys2-ucrt/lib
+rm ../release/deploy-msys2-ucrt/lib/gtk2.obl
 
 # build VM
 cd ../vm
 cp make/Makefile.msys2-ucrt.amd64 Makefile
 
 make clean; make -j3
-cp obr ../release/deploy/bin
+cp obr ../release/deploy-msys2-ucrt/bin
 
 make clean; make -j3
 
@@ -36,55 +36,55 @@ cd ../debugger
 cp make/Makefile.msys2-ucrt.amd64 Makefile
 
 make clean; make -j3
-cp obd ../release/deploy/bin
+cp obd ../release/deploy-msys2-ucrt/bin
 
 # copy msys2 libs
-unzip ../lib/msys2-ucrt64/msys2-ucrt64.zip -d ../release/deploy/lib/msys2-ucrt64
+unzip ../lib/msys2-ucrt64/msys2-ucrt64.zip -d ../release/deploy-msys2-ucrt/lib/msys2-ucrt64
 
 # build libraries
 
 cd ../lib/odbc
 ./build_win_msys2.sh odbc
-cp odbc.dll ../../release/deploy/lib/native/libobjk_odbc.dll
+cp odbc.dll ../../release/deploy-msys2-ucrt/lib/native/libobjk_odbc.dll
 
 cd ../openssl
 ./build_win_msys2.sh openssl
-cp openssl.dll ../../release/deploy/lib/native/libobjk_openssl.dll
+cp openssl.dll ../../release/deploy-msys2-ucrt/lib/native/libobjk_openssl.dll
 
 cd ../sdl
 ./build_win_msys2.sh sdl
-cp sdl.dll ../../release/deploy/lib/native/libobjk_sdl.dll
-cp lib/fonts/*.ttf ../../release/deploy/lib/sdl/fonts
+cp sdl.dll ../../release/deploy-msys2-ucrt/lib/native/libobjk_sdl.dll
+cp lib/fonts/*.ttf ../../release/deploy-msys2-ucrt/lib/sdl/fonts
 
 cd ../diags
 ./build_win_msys2.sh diags
-cp diags.dll ../../release/deploy/lib/native/libobjk_diags.dll
+cp diags.dll ../../release/deploy-msys2-ucrt/lib/native/libobjk_diags.dll
 
 cd ../../native_launcher
 make -f make/Makefile.obb.msys2-ucrt.amd64 clean; make -f make/Makefile.obb.msys2-ucrt.amd64
-cp obb.exe ../release/deploy/bin
+cp obb.exe ../release/deploy-msys2-ucrt/bin
 
 make -f make/Makefile.obn.msys2-ucrt.amd64 clean; make -f make/Makefile.obn.msys2-ucrt.amd64
-cp obn.exe ../release/deploy/lib/native/misc
+cp obn.exe ../release/deploy-msys2-ucrt/lib/native/misc
 
-cp ../vm/misc/config.prop ../release/deploy/lib/native/misc
+cp ../vm/misc/config.prop ../release/deploy-msys2-ucrt/lib/native/misc
 cd ../release
 
 # copy docs
 cd ../..
-cp -R docs/syntax core/release/deploy/doc/syntax
+cp -R docs/syntax core/release/deploy-msys2-ucrt/doc/syntax
 cp docs/readme.html core/release/deploy
 cp LICENSE core/release/deploy
-unzip docs/api.zip -d core/release/deploy/doc
+unzip docs/api.zip -d core/release/deploy-msys2-ucrt/doc
 
 # copy examples
-mkdir core/release/deploy/examples
-mkdir core/release/deploy/examples/media
-cp programs/deploy/*.obs core/release/deploy/examples
-cp programs/deploy/media/*.png core/release/deploy/examples/media
-cp programs/deploy/media/*.wav core/release/deploy/examples/media
-cp -aR programs/doc core/release/deploy/examples
-cp -aR programs/tiny core/release/deploy/examples
+mkdir core/release/deploy-msys2-ucrt/examples
+mkdir core/release/deploy-msys2-ucrt/examples/media
+cp programs/deploy/*.obs core/release/deploy-msys2-ucrt/examples
+cp programs/deploy/media/*.png core/release/deploy-msys2-ucrt/examples/media
+cp programs/deploy/media/*.wav core/release/deploy-msys2-ucrt/examples/media
+cp -aR programs/doc core/release/deploy-msys2-ucrt/examples
+cp -aR programs/tiny core/release/deploy-msys2-ucrt/examples
 
 cd core/release
 
