5f72c9d4624d541c1b5797dd4eebe6a1b73dced0
Randy Hollines <objeck@gmail.com>
2022-11-24 12:41:18 -0800

    clean up build scripts

--- core/lib/openssl/build_msys2-ucrt.sh
+++ core/lib/openssl/build_msys2-ucrt.sh
@@ -2,5 +2,5 @@
 rm -rf *.o
 rm -rf *.dll
 
-g++ -O3 -Wno-unused-function -std=c++11 -Wall -fPIC -c *$1.cpp
+g++ -O3 -std=c++11 -Wall -fPIC -c *$1.cpp -Wno-unused-function
 g++ -O3 -shared -Wl,-soname,$1.so.1 -o $1.dll *.o -lssl -lcrypto
--- core/lib/sdl/build_msys2-ucrt.sh
+++ core/lib/sdl/build_msys2-ucrt.sh
@@ -2,5 +2,5 @@
 rm -rf *.o
 rm -rf *.dll
 
-g++ -O3 -Wno-unused-function -Wall -std=c++11 -fPIC -c *$1.cpp SDL2_framerate.c SDL2_gfxPrimitives.c SDL2_imageFilter.c SDL2_rotozoom.c -I/ucrt64/include/sdl2 -Wno-maybe-uninitialized -Wno-unused-but-set-variable
+g++ -O3 -Wall -std=c++11 -fPIC -c *$1.cpp SDL2_framerate.c SDL2_gfxPrimitives.c SDL2_imageFilter.c SDL2_rotozoom.c -I/ucrt64/include/sdl2 -Wno-maybe-uninitialized -Wno-unused-but-set-variable
 g++ -O3 -shared -Wl,-soname,$1.dll.1 -o $1.dll *.o -lSDL2_ttf -lSDL2_mixer -lSDL2_image -lSDL2 -lSDL2main
