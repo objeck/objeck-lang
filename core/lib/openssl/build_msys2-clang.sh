67f1812aececd0328f5c3f05308d580031ffe8ed
Randy Hollines <objeck@gmail.com>
2022-11-24 14:10:12 -0800

    adding msys2 clang build support

--- core/lib/openssl/build_msys2-clang.sh
+++ core/lib/openssl/build_msys2-clang.sh
@@ -2,5 +2,5 @@
 rm -rf *.o
 rm -rf *.dll
 
-clang++ -O3 -std=c++11 -Wall -fPIC -c *$1.cpp -Wno-unused-function 
+clang++ -O3 -std=c++11 -Wall -fPIC -c *$1.cpp -Wno-unused-function -D_MSYS2_CLANG
 clang++ -O3 -undefined dynamic_lookup -o $1.dll *.o -lssl -lcrypto 
