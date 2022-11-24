6eb4602f5fa3a3b23d30a8f416c315b5df45e3f4
Randy Hollines <objeck@gmail.com>
2022-11-24 12:41:18 -0800

    clean up build scripts

--- core/lib/diags/build_msys2-ucrt.sh
+++ core/lib/diags/build_msys2-ucrt.sh
@@ -2,5 +2,5 @@
 rm -rf *.o
 rm -rf *.dll
 
-g++ -O3 -D_DIAG_LIB -D_X64 -D_WIN32 -D_DIAG_LIB -D_MSYS2 -std=c++17 -Wall -Wno-unused-function -Wno-unused-variable -fPIC -c *$1.cpp ../../compiler/scanner.cpp ../../compiler/parser.cpp ../../compiler/context.cpp ../../compiler/tree.cpp ../../compiler/types.cpp ../../compiler/linker.cpp
+g++ -O3 -D_DIAG_LIB -D_X64 -D_WIN32 -D_DIAG_LIB -D_MSYS2 -std=c++17 -Wall-c *$1.cpp ../../compiler/scanner.cpp ../../compiler/parser.cpp ../../compiler/context.cpp ../../compiler/tree.cpp ../../compiler/types.cpp ../../compiler/linker.cpp -fPIC -Wno-unused-function -Wno-unused-variable 
 g++ -O3 -shared -Wl,-soname,$1.dll.1 -o $1.dll *.o -lssl -lcrypto -lz -pthread
--- core/lib/odbc/build_msys2-ucrt.sh
+++ core/lib/odbc/build_msys2-ucrt.sh
@@ -2,5 +2,5 @@
 rm -rf *.o
 rm -rf *.dll
 
-g++ -O3 -Wno-int-to-pointer-cast -Wno-unused-function -Wall -std=c++17 -fPIC -c *$1.cpp
+g++ -O3 -Wno-int-to-pointer-cast -Wall -std=c++17 -fPIC -c *$1.cpp -Wno-unused-function
 g++ -O3 -shared -Wl,-soname,$1.dll.1 -o $1.dll *.o -lodbc
