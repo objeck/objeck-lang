8480e24851b393e853ef6f8170f5bca9a1646a85
Randy Hollines <objeck@gmail.com>
2022-11-24 17:00:38 -0800

    updated makefiles for linux amd64

--- core/lib/diags/build_linux.sh
+++ core/lib/diags/build_linux.sh
@@ -3,5 +3,5 @@
 rm -rf *.o
 rm -rf *.so
 
-g++ -O3 -D_DIAG_LIB -std=c++17 -Wall *$1.cpp ../../compiler/scanner.cpp ../../compiler/parser.cpp ../../compiler/context.cpp ../../compiler/tree.cpp ../../compiler/types.cpp ../../compiler/linker.cpp -fPIC -Wno-unused-function 
+g++ -O3 -fPIC -D_DIAG_LIB -std=c++17 -Wall -c *$1.cpp ../../compiler/scanner.cpp ../../compiler/parser.cpp ../../compiler/context.cpp ../../compiler/tree.cpp ../../compiler/types.cpp ../../compiler/linker.cpp -Wno-unused-function -Wno-unused-variable
 g++ -O3 -shared -Wl,-soname,$1.so.1 -o $1.so *.o -lssl -lcrypto -ldl -lz -pthread
