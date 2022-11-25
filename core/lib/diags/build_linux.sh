2defe3bf79a50ed573333a3b51de2e0d6227afef
Randy Hollines <objeck@gmail.com>
2022-11-24 16:13:34 -0800

    adding msys2 clang build support

--- core/lib/diags/build_linux.sh
+++ core/lib/diags/build_linux.sh
@@ -1,6 +1,7 @@
 #/bin/sh
+
 rm -rf *.o
 rm -rf *.so
 
-g++ -O3 -D_DIAG_LIB -std=c++17 -Wall -Wno-unused-function -fPIC -c *$1.cpp ../../compiler/scanner.cpp ../../compiler/parser.cpp ../../compiler/context.cpp ../../compiler/tree.cpp ../../compiler/types.cpp ../../compiler/linker.cpp
+g++ -O3 -D_DIAG_LIB -std=c++17 -Wall-c *$1.cpp ../../compiler/scanner.cpp ../../compiler/parser.cpp ../../compiler/context.cpp ../../compiler/tree.cpp ../../compiler/types.cpp ../../compiler/linker.cpp -fPIC -Wno-unused-function 
 g++ -O3 -shared -Wl,-soname,$1.so.1 -o $1.so *.o -lssl -lcrypto -ldl -lz -pthread
