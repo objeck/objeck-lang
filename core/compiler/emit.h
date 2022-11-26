725e565173c9b311baf2a182e43b4b5a44471223
Randy Hollines <objeck@gmail.com>
2022-11-25 17:30:24 -0800

    updating windows build

--- core/compiler/emit.h
+++ core/compiler/emit.h
@@ -1,7 +1,7 @@
 /***************************************************************************
 * Defines how the intermediate code is written to output files
 *
-* Copyright (c) 2008-2023, Randy Hollines
+* Copyright (c) 2023, Randy Hollines
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
