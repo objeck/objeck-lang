23c3140f2a9c4241ec7e36ddc63370bdd8ccfec5
Randy Hollines <objeck@gmail.com>
2022-12-17 00:53:12 -0800

    gtk testing

--- programs/gtk/test/test.cpp
+++ programs/gtk/test/test.cpp
@@ -1,27 +1,18 @@
 #include <gtk/gtk.h>
 
-static void
-activate (GtkApplication* app,
-          gpointer        user_data)
+static void activate(GtkApplication* app, gpointer user_data)
 {
-  GtkWidget *window;
-
-  window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Window");
-  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
-  gtk_widget_show_all (window);
+  GtkWidget *window = gtk_application_window_new(app);
+  gtk_window_set_title(GTK_WINDOW (window), "GTK Test");
+  gtk_window_set_default_size (GTK_WINDOW (window), 400, 300);
+  gtk_widget_show_all(window);
 }
 
-int
-main (int    argc,
-      char **argv)
+int main(int argc, char **argv)
 {
-  GtkApplication *app;
-  int status;
-
-  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+  GtkApplication *app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
   g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-  status = g_application_run (G_APPLICATION (app), argc, argv);
+  const int status = g_application_run (G_APPLICATION (app), argc, argv);
   g_object_unref (app);
 
   return status;
