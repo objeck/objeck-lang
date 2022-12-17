26022f6d2c0ce35f9308678055276158b6c62c4d
Randy Hollines <objeck@gmail.com>
2022-12-17 00:53:50 -0800

    gtk testing

--- programs/gtk/test/test.cpp
+++ programs/gtk/test/test.cpp
@@ -11,7 +11,7 @@ static void activate(GtkApplication* app, gpointer user_data)
 int main(int argc, char **argv)
 {
   GtkApplication* app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
-  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  g_signal_connect (app, "activate", G_CALLBACK (activate), nullptr);
   const int status = g_application_run (G_APPLICATION (app), argc, argv);
   g_object_unref (app);
 
