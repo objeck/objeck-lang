4af4915af61053055e1f40a443bda30df810e6ec
Randy Hollines <objeck@gmail.com>
2022-12-18 13:46:34 -0700

    gtk code gens

--- programs/gtk/code_gen/brun.cmd
+++ programs/gtk/code_gen/brun.cmd
@@ -3,6 +3,6 @@
 del code_gen.obe
 
 obc -src sdl_emitter.obs,sdl_parser.obs,sdl_scanner.obs -dest code_gen.obe
-IF NOT "%~1"=="" IF NOT "%~2"=="" (
-	obr code_gen.obe %1 %2
+IF NOT "%~1"=="" IF NOT "%~2"=="" IF NOT "%~3"=="" (
+	obr code_gen.obe %1 %2 %3
 )
