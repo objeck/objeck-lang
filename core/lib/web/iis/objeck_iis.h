eaf9abfd1a092241507f140d910f0285f0c76dab
Randy Hollines <objeck@gmail.com>
2022-12-25 17:26:26 -0800

    setting up webi dev env

---
+++ core/lib/web/iis/objeck_iis.h
@@ -0,0 +1,72 @@
+/***************************************************************************
+ * Copyright (c) 2023, Randy Hollines
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in
+ * the documentation and/or other materials provided with the distribution.
+ * - Neither the name of the StackVM Team nor the names of its
+ * contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ***************************************************************************/
+
+#ifndef __OBJECK_IIS_H__
+#define __OBJECK_IIS_H__
+
+#include <windows.h>
+#include <sal.h>
+#include <httpserv.h>
+
+#include "memory.h"
+#include "loader.h"
+#include "interpreter.h"
+#include "sys.h"
+
+// Web action class
+class ObjeckIIS : public CHttpModule {
+  bool is_ok;
+  std::string html_out;
+
+  Runtime::StackInterpreter* intpr;
+  size_t* op_stack;
+  long* stack_pos;
+
+  void DebugEnvironment(const std::string& progam_path, const std::string& install_path);
+  std::map<std::string, std::string> LoadConfiguration();
+  void LoadProgam(const std::wstring& name);
+
+public:
+  ObjeckIIS();
+  ~ObjeckIIS();
+
+  REQUEST_NOTIFICATION_STATUS OnBeginRequest(IN IHttpContext* pHttpContext, IN IHttpEventProvider* pProvider);
+};
+
+// Module factory
+class ObjeckIISFactory : public IHttpModuleFactory {
+
+public:
+  HRESULT GetHttpModule(OUT CHttpModule** ppModule, IN IModuleAllocator* pAllocator);
+  void Terminate();
+};
+
+HRESULT __stdcall RegisterModule(DWORD dwServerVersion, IHttpModuleRegistrationInfo* pModuleInfo, IHttpServer* pGlobalInfo);
+
+#endif
