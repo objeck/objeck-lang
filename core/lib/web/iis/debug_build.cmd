047b96cb06533ccf65c5ee7ce2e53257e266e583
Randy Hollines <objeck@gmail.com>
2022-12-31 15:30:25 -0800

    testing and working on iis support

--- core/lib/web/iis/debug_build.cmd
+++ core/lib/web/iis/debug_build.cmd
@@ -1,4 +1,4 @@
 @echo off
 
 obc -src ..\..\..\compiler\lib_src\web_server.obs -tar lib -dest ..\..\..\release\deploy64\lib\web_server.obl
-obc -src ..\..\..\..\programs\web\hello.obs -lib web_server -tar web -dest hello.obw
+obc -src ..\..\..\..\programs\frameworks\web\get_post.obs -lib web_server -tar web -dest get_post.obw
