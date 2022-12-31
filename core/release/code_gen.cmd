b29a0b480bfd6cca29845472eedd406ffe8badc1
Randy Hollines <objeck@gmail.com>
2022-12-31 05:58:56 -0800

    repository clean up

--- core/release/code_gen.cmd
+++
@@ -1,4 +0,0 @@
-cd deploy\bin
-obc -src ..\..\..\..\programs\sdl\code_gen\sdl_emitter.obs,..\..\..\..\programs\sdl\code_gen\sdl_parser.obs,..\..\..\..\programs\sdl\code_gen\sdl_scanner.obs -lib collect.obl -dest ..\..\code_gen.obe
-obr ..\..\code_gen.obe %1 %2
-cd ..\..
