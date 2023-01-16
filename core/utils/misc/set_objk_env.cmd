cc3f02315b2c41251645ff5910e08ed7e66a85bb
Randy Hollines <objeck@gmail.com>
2023-01-16 13:06:28 -0800

    bumping version

---
+++ core/utils/misc/set_objk_env.cmd
@@ -0,0 +1,3 @@
+@echo off
+set OBJECK_LIB_PATH=D:\Code\objeck-lang\src\objeck\deploy\lib
+set path=%path%;D:\Code\objeck-lang\src\objeck\deploy\bin
---
+++ core/utils/misc/set_objk_env.sh
@@ -0,0 +1,4 @@
+PATH=$PATH:~/Documents/Code/objeck-lang/src/objeck/deploy/bin/
+export PATH
+OBJECK_LIB_PATH=~/Documents/Code/objeck-lang/src/objeck/deploy/lib/
+export OBJECK_LIB_PATH
