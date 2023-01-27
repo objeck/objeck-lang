d5d27e4f9f2e3cfc0fefade490b91649131d538f
Randy Hollines <objeck@gmail.com>
2023-01-27 05:28:39 -0800

    code clean up

--- core/compiler/scanner.h
+++ core/compiler/scanner.h
@@ -482,7 +482,7 @@ class Scanner {
   // start marker position
   size_t start_pos;
   // end marker position
-  size_t foo_pos;
+  size_t end_pos;
   // input characters
   wchar_t cur_char, nxt_char, nxt_nxt_char;
   // map of reserved identifiers
