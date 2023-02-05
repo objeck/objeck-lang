9b0654d077162aa31dc3795a22a4c9d8d6aa9b0c
Randy Hollines <objeck@gmail.com>
2023-02-04 18:09:47 -0800

    working on mlpack support

--- core/lib/experimental/mlpack/examples/movies/movies.cpp
+++ core/lib/experimental/mlpack/examples/movies/movies.cpp
@@ -1,3 +1,5 @@
+#include <mlpack/mlpack.hpp>
+
 using namespace mlpack;
 
 int main()
@@ -9,13 +11,13 @@ int main()
   // Now, load the names of the movies as a single-feature categorical dataset.
   // We can use `moviesInfo.UnmapString(i, 0)` to get the i'th string.
   data::DatasetInfo moviesInfo;
-  mat movies; // This will be unneeded.
+  arma::mat movies; // This will be unneeded.
   if(!data::Load("movies.csv", movies, moviesInfo))
     throw std::runtime_error("Could not load movies.csv!");
 
   // Split the ratings into a training set and a test set, using 10% of the
   // dataset for the test set.
-  mat trainRatings, testRatings;
+  arma::mat trainRatings, testRatings;
   data::Split(ratings, trainRatings, testRatings, 0.1);
 
   // Train the CF model using RegularizedSVD as the decomposition algorithm.
@@ -28,28 +30,27 @@ int main()
 
   // Now compute the RMSE for the test set user and item combinations.  To do
   // this we must assemble the list of users and items.
-  Mat<size_t> combinations(2, testRatings.n_cols);
+  arma::Mat<size_t> combinations(2, testRatings.n_cols);
   for(size_t i = 0; i < testRatings.n_cols; ++i)
   {
     combinations(0, i) = size_t(testRatings(0, i)); // (user)
     combinations(1, i) = size_t(testRatings(1, i)); // (item)
   }
-  vec predictions;
+  arma::vec predictions;
   cf.Predict(combinations, predictions);
   const double rmse = norm(predictions - testRatings.row(2).t(), 2) /
     sqrt((double)testRatings.n_cols);
-  std::cout << "RMSE of trained model is " << rmse << "." << endl;
+  std::cout << "RMSE of trained model is " << rmse << "." << std::endl;
 
   // Compute the top 10 movies for user 1.
-  Col<size_t> users = { 1 };
-  Mat<size_t> recommendations;
+  arma::Col<size_t> users = { 1 };
+  arma::Mat<size_t> recommendations;
   cf.GetRecommendations(10, recommendations, users);
 
   // Now print each movie.
-  cout << "Recommendations for user 1:" << endl;
+  std::cout << "Recommendations for user 1:" << std::endl;
   for(size_t i = 0; i < recommendations.n_elem; ++i)
   {
-    cout << "  " << (i + 1) << ". "
-      << moviesInfo.UnmapString(recommendations[i], 2) << "." << endl;
+    std::cout << "  " << (i + 1) << ". " << moviesInfo.UnmapString(recommendations[i], 2) << "." << std::endl;
   }
 }
