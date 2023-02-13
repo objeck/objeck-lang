8c254c30da36205d5253b7c7c910afaf2a6fd271
Randy Hollines <objeck@gmail.com>
2023-02-13 10:22:03 -0800

    removing cygwin integration

--- core/debugger/debugger.cpp
+++ core/debugger/debugger.cpp
@@ -51,8 +51,6 @@ int main(int argc, char* argv[])
   usage += L" (x86-64 Windows)";
 #elif _WIN32
   usage += L" (x86 Windows)";
-#elif __CYGWIN__
-  usage += L" (Cygwin x86_64)";
 #elif _OSX
 #ifdef _ARM64
   usage += L" (macOS ARM64)";
