3a23ef7e3178e9f6e7be5bae32e0efc384bbcddb
Randy Hollines <objeck@gmail.com>
2023-02-24 09:07:13 -0800

    gtk poc work under windows and linux

---
+++ core/lib/experimental/gtk/poc/gtk3_test.h
@@ -0,0 +1,85 @@
+#ifndef __GTK3_TEST_H__
+#define __GTK3_TEST_H__
+
+#include <gtk/gtk.h>
+#include "../../../../vm/lib_api.h"
+#include "../../../../vm/interpreter.h"
+#include "../../../../shared/sys.h"
+
+#define EXEC_STACK_MAX 32
+
+class MemoryAllocator {
+  APITools_AllocateObject_Ptr allocate_object;
+  APITools_MethodCallById_Ptr method_call_by_id;
+  std::stack<std::pair<size_t*, long*>> exec_stack_mem;
+  std::stack<void*> raw_allocations;
+
+public:
+  MemoryAllocator(APITools_AllocateObject_Ptr ao, APITools_MethodCallById_Ptr mc) {
+    allocate_object = ao;
+    method_call_by_id = mc;
+
+    for(size_t i = 0; i < EXEC_STACK_MAX; ++i) {
+      size_t* op_stack = new size_t[OP_STACK_SIZE];
+      long* stack_pos = new long;
+
+      exec_stack_mem.push(std::pair<size_t*, long*>(op_stack, stack_pos));
+    }
+  }
+
+  ~MemoryAllocator() {
+    allocate_object = nullptr;
+    method_call_by_id = nullptr;
+
+    while(!exec_stack_mem.empty()) {
+      std::pair<size_t*, long*> value = exec_stack_mem.top();
+      exec_stack_mem.pop();
+
+      size_t* op_stack = value.first;
+      delete[] op_stack;
+      op_stack = nullptr;
+
+      long* stack_pos = value.second;
+      delete stack_pos;
+      stack_pos = nullptr;
+    }
+
+    while(!raw_allocations.empty()) {
+      void* mem = raw_allocations.top();
+      raw_allocations.pop();
+      // delete
+      delete mem;
+      mem = nullptr;
+    }
+  }
+
+  APITools_AllocateObject_Ptr GetAllocateObject() {
+    return allocate_object;
+  }
+
+  APITools_MethodCallById_Ptr GetMethodCallById() {
+    return method_call_by_id;
+  }
+
+  std::pair<size_t*, long*> FetchExecStackMemory() {
+    if(!exec_stack_mem.empty()) {
+      std::pair<size_t*, long*> mem_pair = exec_stack_mem.top();
+      exec_stack_mem.pop();
+
+      return mem_pair;
+    }
+
+    return std::pair<size_t*, long*>();
+  }
+
+  void ReleaseExecStackMemory(std::pair<size_t*, long*> mem_pair) {
+    exec_stack_mem.push(mem_pair);
+  }
+
+  void AddAllocation(void* mem) {
+    raw_allocations.push(mem);
+  }
+};
+
+#endif
+
