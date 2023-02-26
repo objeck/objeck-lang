e2deb45ea943414fdb76c3c90730deb1fa50fc17
Randy Hollines <objeck@gmail.com>
2023-02-26 02:01:35 -0800

    working on gtk support

--- core/lib/experimental/gtk/code_gen/build.cmd
+++ core/lib/experimental/gtk/code_gen/build.cmd
@@ -1,9 +1,8 @@
 @echo off
 
-REM obc -src gtk_parser_test1.obs -lib xml
-obc -src gtk_parser_test3.obs -lib xml
+obc -src gtk_parser.obs -lib xml
 
 if [%1] NEQ [] (
 REM	obr gtk_parser_test1 %1
-	obr gtk_parser_test3 %1
+	obr gtk_parser %1
 )
