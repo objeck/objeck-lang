05a5e9479ffb6aae0ea754bca70c98b48f0dcb66
Randy Hollines <objeck@gmail.com>
2023-02-28 07:24:53 -0800

    working on gtk support, c++ side

--- core/lib/experimental/gtk/code_gen/build_win.cmd
+++ core/lib/experimental/gtk/code_gen/build_win.cmd
@@ -7,7 +7,7 @@ obc -src gtk3_binder.obs -lib xml
 if [%1] NEQ [] (
 	obr gtk3_binder %1
 	copy /y gens\objk_code.txt gens\gtk3_binder.obs
-	obc -src gens\objk_code.obs -tar lib
+	obc -src gens\gtk3_binder.obs -tar lib
 
 	copy /y gens\cxx_code.txt gens\gtk3_binder.cpp
 	REM compiler
