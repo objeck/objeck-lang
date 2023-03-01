50d3eb9c304c13ccd658e58403d3faf05cea39c7
Randy Hollines <objeck@gmail.com>
2023-03-01 07:45:42 -0800

    working on gtk support

--- core/lib/experimental/gtk/code_gen/build_win.cmd
+++ core/lib/experimental/gtk/code_gen/build_win.cmd
@@ -5,7 +5,7 @@ del gens\*.obl gens\*.obs gens\*.cpp gens\*.txt
 obc -src gtk3_binder.obs -lib xml
 
 if [%1] NEQ [] (
-	obr gtk3_binder %1
+	obr gtk3_binder %1 %2
 	copy /y gens\objk_code.txt gens\gtk3_binder.obs
 	obc -src gens\gtk3_binder.obs -tar lib
 
