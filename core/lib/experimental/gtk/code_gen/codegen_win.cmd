bc2627925c52010e2b39ddb729356299ecc364a5
Randy Hollines <objeck@gmail.com>
2023-03-05 06:21:52 -0800

    working on gtk3 support

--- core/lib/experimental/gtk/code_gen/codegen_win.cmd
+++ core/lib/experimental/gtk/code_gen/codegen_win.cmd
@@ -1,14 +1,16 @@
 @echo off
 
-del *.obe gens\*.obl gens\*.txt
+del *.obe
+rmdir /s /q out
+mkdir out
 
 obc -src gtk3_codegen.obs -lib xml
 
 if [%1] NEQ [] (
 	obr gtk3_codegen %1 %2
-	copy /y gens\objk_code.txt gens\gtk3_codegen.obs
-	obc -src gens\gtk3_codegen.obs -tar lib
+	copy /y out\objk_code.txt out\gtk3_codegen.obs
+	obc -src out\gtk3_codegen.obs -tar lib
 	
-	copy /y gens\cxx_code.txt gens\gtk3_codegen.cpp
+	copy /y out\cxx_code.txt out\gtk3_codegen.cpp
 	REM compiler
 )
