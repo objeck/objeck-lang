e764babbc7d64a8e8d9ade5781449fa07be349fa
Randy Hollines <objeck@gmail.com>
2023-03-18 13:36:29 -0700

    add named pipe support

--- core/compiler/scanner.cpp
+++ core/compiler/scanner.cpp
@@ -246,7 +246,9 @@ void Scanner::LoadKeywords()
   ident_map[L"FILE_DELETE"] = FILE_DELETE;
   ident_map[L"FILE_RENAME"] = FILE_RENAME;
   ident_map[L"FILE_COPY"] = FILE_COPY;
-  ident_map[L"PIPE_OPEN_READ_WRITE"] = PIPE_OPEN_READ_WRITE;
+  ident_map[L"PIPE_CREATE"] = PIPE_CREATE;
+  ident_map[L"PIPE_CONNECT"] = PIPE_CONNECT;
+  ident_map[L"PIPE_TO_WAIT"] = PIPE_TO_WAIT;
   ident_map[L"PIPE_IN_BYTE"] = PIPE_IN_BYTE;
   ident_map[L"PIPE_OUT_BYTE"] = PIPE_OUT_BYTE;
   ident_map[L"PIPE_IN_BYTE_ARY"] = PIPE_IN_BYTE_ARY;
@@ -507,7 +509,9 @@ void Scanner::CheckIdentifier(int index)
     case FILE_DELETE:
     case FILE_RENAME:
     case FILE_COPY:
-    case PIPE_OPEN_READ_WRITE:
+    case PIPE_CREATE:
+    case PIPE_CONNECT:
+    case PIPE_TO_WAIT:
     case PIPE_IN_BYTE:
     case PIPE_OUT_BYTE:
     case PIPE_IN_BYTE_ARY:
