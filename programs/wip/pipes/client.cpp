852adb967b3fa9418f1571ffe1072b823024b764
Randy Hollines <objeck@gmail.com>
2023-03-19 12:45:28 -0700

    working on named socket support

--- programs/wip/pipes/client.cpp
+++ programs/wip/pipes/client.cpp
@@ -14,14 +14,12 @@ int _tmain(int argc, TCHAR *argv[])
    DWORD  cbRead, cbToWrite, cbWritten, dwMode; 
    LPTSTR lpszPipename = TEXT("\\\\.\\pipe\\objk"); 
 
-   if( argc > 1 )
+   if(argc > 1)
       lpvMessage = argv[1];
  
-// Try to open a named pipe; wait for it, if necessary. 
- 
-   while (1) 
-   { 
-      hPipe = CreateFile( 
+   // Try to open a named pipe; wait for it, if necessary. 
+   while(1) { 
+      hPipe = CreateFile(
          lpszPipename,   // pipe name 
          GENERIC_READ |  // read and write access 
          GENERIC_WRITE, 
@@ -31,89 +29,63 @@ int _tmain(int argc, TCHAR *argv[])
          0,              // default attributes 
          NULL);          // no template file 
  
-   // Break if the pipe handle is valid. 
- 
-      if (hPipe != INVALID_HANDLE_VALUE) 
+      // Break if the pipe handle is valid. 
+      if(hPipe != INVALID_HANDLE_VALUE) 
          break; 
  
       // Exit if an error other than ERROR_PIPE_BUSY occurs. 
  
-      if (GetLastError() != ERROR_PIPE_BUSY) 
-      {
-         _tprintf( TEXT("Could not open pipe. GLE=%d\n"), GetLastError() ); 
+      if(GetLastError() != ERROR_PIPE_BUSY) {
+         _tprintf(TEXT("Could not open pipe. GLE=%d\n"), GetLastError()); 
          return -1;
       }
  
       // All pipe instances are busy, so wait for 20 seconds. 
- 
-      if ( ! WaitNamedPipe(lpszPipename, 20000)) 
-      { 
+      if(!WaitNamedPipe(lpszPipename, 20000)) { 
          printf("Could not open pipe: 20 second wait timed out."); 
          return -1;
       } 
    } 
- 
- /*
-// The pipe connected; change to message-read mode. 
- 
-   dwMode = PIPE_READMODE_MESSAGE; 
-   fSuccess = SetNamedPipeHandleState( 
-      hPipe,    // pipe handle 
-      &dwMode,  // new pipe mode 
-      NULL,     // don't set maximum bytes 
-      NULL);    // don't set maximum time 
-   if ( ! fSuccess) 
-   {
-      _tprintf( TEXT("SetNamedPipeHandleState failed. GLE=%d\n"), GetLastError() ); 
-      return -1;
-   }
- */
-// Send a message to the pipe server. 
- 
-   cbToWrite = (lstrlen(lpvMessage)+1)*sizeof(TCHAR);
-   _tprintf( TEXT("Sending %d byte message: \"%s\"\n"), cbToWrite, lpvMessage); 
 
-   fSuccess = WriteFile( 
+   // Send a message to the pipe server. 
+   cbToWrite = (lstrlen(lpvMessage)+1)*sizeof(TCHAR);
+   _tprintf(TEXT("Sending %d byte message: \"%s\"\n"), cbToWrite, lpvMessage); 
+
+   fSuccess = WriteFile(
       hPipe,                  // pipe handle 
       lpvMessage,             // message 
       cbToWrite,              // message length 
       &cbWritten,             // bytes written 
       NULL);                  // not overlapped 
 
-   if ( ! fSuccess) 
-   {
-      _tprintf( TEXT("WriteFile to pipe failed. GLE=%d\n"), GetLastError() ); 
+   if(!fSuccess) {
+      _tprintf(TEXT("WriteFile to pipe failed. GLE=%d\n"), GetLastError()); 
       return -1;
    }
 
    printf("\nMessage sent to server, receiving reply as follows:\n");
  
-   do 
-   { 
-   // Read from the pipe. 
- 
-      fSuccess = ReadFile( 
+   do { 
+      // Read from the pipe.
+      fSuccess = ReadFile(
          hPipe,    // pipe handle 
          chBuf,    // buffer to receive reply 
          BUFSIZE*sizeof(TCHAR),  // size of buffer 
          &cbRead,  // number of bytes read 
          NULL);    // not overlapped 
  
-      if ( ! fSuccess && GetLastError() != ERROR_MORE_DATA )
+      if(!fSuccess && GetLastError() != ERROR_MORE_DATA)
          break; 
  
-      _tprintf( TEXT("\"%s\"\n"), chBuf ); 
-   } while ( ! fSuccess);  // repeat loop if ERROR_MORE_DATA 
+      _tprintf(TEXT("\"%s\"\n"), chBuf); 
+   } 
+   while(!fSuccess);  // repeat loop if ERROR_MORE_DATA 
 
-   if ( ! fSuccess)
-   {
-      _tprintf( TEXT("ReadFile from pipe failed. GLE=%d\n"), GetLastError() );
+   if(!fSuccess) {
+      _tprintf(TEXT("ReadFile from pipe failed. GLE=%d\n"), GetLastError());
       return -1;
    }
 
-   printf("\n<End of message, press ENTER to terminate connection and exit>");
-   _getch();
- 
    CloseHandle(hPipe); 
  
    return 0; 
