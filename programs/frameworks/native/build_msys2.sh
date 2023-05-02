865707596ee2939116e4a7eb908917c2b704418a
Randy Hollines <objeck@gmail.com>
2023-05-01 18:11:11 -0700

    native code example

--- programs/frameworks/native/build_msys2.sh
+++ programs/frameworks/native/build_msys2.sh
@@ -1,9 +1,17 @@
 #/bin/sh
 
 CC=g++
+LIB=lib_foo
+
+export PATH=$PATH:../../../core/release/deploy/bin
+export OBJECK_LIB_PATH=../../../core/release/deploy/lib
 
 rm -rf *.o
 rm -rf *.dll
+rm ../../../core/release/deploy/lib/native/$LIB.so
 
-$CC -O3 -Wall -D_DIAG_LIB -D_X64 -D_WIN32 -D_DIAG_LIB -D_MSYS2 -std=c++17  -fPIC -Wno-unused-function -Wno-unused-variable -c *lib_foo.cpp
-$CC -O3 -shared -Wl,-soname,$1.dll.1 -o lib_foo.dll *.o -lssl -lcrypto -lz -pthread
+obc foo.so
+
+$CC -O3 -Wall -D_DIAG_LIB -D_X64 -D_WIN32 -D_DIAG_LIB -D_MSYS2 -std=c++17 -fPIC -Wno-unused-function -Wno-unused-variable -c *lib_foo.cpp
+$CC -O3 -shared -Wl,-soname,$LIB.so.1 -o $LIB.so *.o -lssl -lcrypto -ldl -lz -pthread
+cp $LIB.so ../../../core/release/deploy/lib/native/
