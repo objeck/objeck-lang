92a289e9e5a3cd9c7a0c8192775f5479e4ac2ee5
Randy Hollines <objeck@gmail.com>
2023-05-01 17:54:48 -0700

    native code example

---
+++ programs/frameworks/native/lib_foo.cpp
@@ -0,0 +1,34 @@
+extern "C" {
+  //
+  // initialize diagnostics environment
+  //
+#ifdef _WIN32
+  __declspec(dllexport)
+#endif
+  void load_lib(VMContext& context)
+  {
+  }
+
+  //
+  // release diagnostics resources
+  //
+#ifdef _WIN32
+  __declspec(dllexport)
+#endif
+  void unload_lib()
+  {
+  }
+
+  //
+  // Add two numbers
+  //
+#ifdef _WIN32
+  __declspec(dllexport)
+#endif
+  void add(VMContext& context)
+  {
+    const long left = APITools_GetIntValue(context, 1);
+    const long right = APITools_GetIntValue(context, 1);
+    APITools_SetIntValue(context, 0, left + right);
+  }
+}
