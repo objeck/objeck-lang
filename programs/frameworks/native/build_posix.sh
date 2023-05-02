66f8975194bc20b1a236aae66a728315f69fcde1
Randy Hollines <objeck@gmail.com>
2023-05-01 18:59:52 -0700

    native code example

--- programs/frameworks/native/build_posix.sh
+++ programs/frameworks/native/build_posix.sh
@@ -10,7 +10,7 @@ rm -rf *.o *.so *.obe
 rm ../../../core/release/deploy/lib/native/$LIB.so
 
 $CC -O3 -Wall -D_DIAG_LIB -D_X64 -D_DIAG_LIB -D_MSYS2 -std=c++17 -fPIC -Wno-unused-function -Wno-unused-variable -c *$LIB.cpp
-$CC -O3 -shared -Wl,-soname,$1.so.1 -o $LIB.so *.o -lssl -lcrypto -ldl -lz -pthread
+$CC -O3 -shared -Wl,-soname,$LIB.so.1 -o $LIB.so *.o -lssl -lcrypto -ldl -lz -pthread
 cp $LIB.so ../../../core/release/deploy/lib/native/
 
 obc foo; obr foo
