1a2a136d845b486cd2218195d7f2cab7e9ee5589
Randy Hollines <objeck@gmail.com>
2023-05-01 18:46:13 -0700

    native build example

--- programs/frameworks/native/build_msys2.sh
+++ programs/frameworks/native/build_msys2.sh
@@ -2,16 +2,13 @@
 
 CC=g++
 LIB=lib_foo
+DEPLOY_DIR=deploy-msys2-ucrt
 
-export PATH=$PATH:../../../core/release/deploy/bin
-export OBJECK_LIB_PATH=../../../core/release/deploy/lib
-
-rm -rf *.o
-rm -rf *.dll
-rm ../../../core/release/deploy/lib/native/$LIB.so
-
-obc foo.so
+rm -rf *.o *.dll *.obe
+rm ../../../core/release/$DEPLOY_DIR/lib/native/$LIB.dll
 
 $CC -O3 -Wall -D_DIAG_LIB -D_X64 -D_WIN32 -D_DIAG_LIB -D_MSYS2 -std=c++17 -fPIC -Wno-unused-function -Wno-unused-variable -c *lib_foo.cpp
-$CC -O3 -shared -Wl,-soname,$LIB.so.1 -o $LIB.so *.o -lssl -lcrypto -ldl -lz -pthread
-cp $LIB.so ../../../core/release/deploy/lib/native/
+$CC -O3 -shared -Wl,-soname,$LIB.dll.1 -o $LIB.dll *.o -lssl -lcrypto -lz -pthread
+cp $LIB.dll ../../../core/release/$DEPLOY_DIR/lib/native/
+
+obc foo && obr foo
