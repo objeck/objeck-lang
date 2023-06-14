20f80f7bd8c466a6a0deb1976e716952de248928
Randy Hollines <objeck@gmail.com>
2023-06-14 06:40:55 -0700

    updated to symlink support

--- core/compiler/intermediate.cpp
+++ core/compiler/intermediate.cpp
@@ -2816,9 +2816,8 @@ void IntermediateEmitter::EmitSystemDirective(SystemStatement* statement)
   case SYM_LINK_CREATE:
     imm_block->AddInstruction(IntermediateFactory::Instance()->MakeInstruction(statement, cur_line_num, LOAD_INT_VAR, 0, LOCL));
     imm_block->AddInstruction(IntermediateFactory::Instance()->MakeInstruction(statement, cur_line_num, LOAD_INT_VAR, 1, LOCL));
-    imm_block->AddInstruction(IntermediateFactory::Instance()->MakeInstruction(statement, cur_line_num, LOAD_INT_VAR, 2, LOCL));
     imm_block->AddInstruction(IntermediateFactory::Instance()->MakeIntLitInstruction(statement, cur_line_num, instructions::SYM_LINK_CREATE));
-    imm_block->AddInstruction(IntermediateFactory::Instance()->MakeInstruction(statement, cur_line_num, TRAP, 4L));
+    imm_block->AddInstruction(IntermediateFactory::Instance()->MakeInstruction(statement, cur_line_num, TRAP, 3L));
     break;
 
   case SYM_LINK_COPY:
