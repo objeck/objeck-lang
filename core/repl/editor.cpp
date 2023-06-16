69cee9d7b45061ceab6694740943f761edae86cb
Randy Hollines <objeck@gmail.com>
2023-06-16 08:44:05 -0700

    refactoring repl code

--- core/repl/editor.cpp
+++ core/repl/editor.cpp
@@ -43,7 +43,7 @@ size_t Document::Reset()
 {
   lines.clear();
 
-  lines.push_back(Line(L"class Shell {", Line::Type::RO_CLS_START_LINE));
+  lines.push_back(Line(L"class Repl {", Line::Type::RO_CLS_START_LINE));
   lines.push_back(Line(L"  function : Main(args : String[]) ~ Nil {", Line::Type::RO_FUNC_START_LINE));
   lines.push_back(Line(L"  }", Line::Type::RO_FUNC_END_LINE));
   lines.push_back(Line(L"}", Line::Type::RO_CLS_END_LINE));
