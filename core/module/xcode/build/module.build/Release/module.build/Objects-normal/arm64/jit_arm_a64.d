b99a0aa9e98c7c3cdec48e2ab331f5734b07aaff
Randy Hollines <objeck@gmail.com>
2023-07-14 10:32:17 -0700

    upgraded openssl for macos arm64 to 1.1n

--- core/module/xcode/build/module.build/Release/module.build/Objects-normal/arm64/jit_arm_a64.d
+++ core/module/xcode/build/module.build/Release/module.build/Objects-normal/arm64/jit_arm_a64.d
@@ -22,25 +22,33 @@ dependencies: \
   ../../lib/openssl/macos/include/openssl/ssl.h \
   ../../lib/openssl/macos/include/openssl/comp.h \
   ../../lib/openssl/macos/include/openssl/comperr.h \
-  ../../lib/openssl/macos/include/openssl/lhash.h \
-  ../../lib/openssl/macos/include/openssl/pem.h \
+  ../../lib/openssl/macos/include/openssl/x509.h \
+  ../../lib/openssl/macos/include/openssl/buffer.h \
+  ../../lib/openssl/macos/include/openssl/buffererr.h \
   ../../lib/openssl/macos/include/openssl/evp.h \
   ../../lib/openssl/macos/include/openssl/evperr.h \
   ../../lib/openssl/macos/include/openssl/objects.h \
   ../../lib/openssl/macos/include/openssl/obj_mac.h \
   ../../lib/openssl/macos/include/openssl/asn1.h \
   ../../lib/openssl/macos/include/openssl/asn1err.h \
+  ../../lib/openssl/macos/include/openssl/bn.h \
+  ../../lib/openssl/macos/include/openssl/bnerr.h \
   ../../lib/openssl/macos/include/openssl/objectserr.h \
-  ../../lib/openssl/macos/include/openssl/x509.h \
-  ../../lib/openssl/macos/include/openssl/buffer.h \
-  ../../lib/openssl/macos/include/openssl/buffererr.h \
   ../../lib/openssl/macos/include/openssl/ec.h \
   ../../lib/openssl/macos/include/openssl/ecerr.h \
+  ../../lib/openssl/macos/include/openssl/rsa.h \
+  ../../lib/openssl/macos/include/openssl/rsaerr.h \
+  ../../lib/openssl/macos/include/openssl/dsa.h \
+  ../../lib/openssl/macos/include/openssl/dh.h \
+  ../../lib/openssl/macos/include/openssl/dherr.h \
+  ../../lib/openssl/macos/include/openssl/dsaerr.h \
   ../../lib/openssl/macos/include/openssl/sha.h \
   ../../lib/openssl/macos/include/openssl/x509err.h \
   ../../lib/openssl/macos/include/openssl/x509_vfy.h \
+  ../../lib/openssl/macos/include/openssl/lhash.h \
   ../../lib/openssl/macos/include/openssl/pkcs7.h \
   ../../lib/openssl/macos/include/openssl/pkcs7err.h \
+  ../../lib/openssl/macos/include/openssl/pem.h \
   ../../lib/openssl/macos/include/openssl/pemerr.h \
   ../../lib/openssl/macos/include/openssl/hmac.h \
   ../../lib/openssl/macos/include/openssl/async.h \
