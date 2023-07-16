11c2ecf07a26b22a051b85cf6ffb0229a5907354
Randy Hollines <objeck@gmail.com>
2023-07-16 09:48:13 -0700

    api doc examples for collections

---
+++ core/release/test_doc64.cmd
@@ -0,0 +1,20 @@
+@echo off
+
+set ZIP_BIN="\Program Files\7-Zip"
+
+cd deploy64\bin
+rmdir /s /q ..\html 
+mkdir ..\html
+obc -src ..\..\..\lib\code_doc\doc_html.obs,..\..\..\lib\code_doc\doc_parser.obs -lib xml -dest ..\..\code_doc.obe
+obr ..\..\code_doc.obe ..\..\..\lib\code_doc\templates 2023.7.3 ..\..\..\compiler\lib_src\gen_collect.obs
+rmdir /s /q ..\doc\api
+mkdir ..\doc\api
+copy ..\..\..\lib\code_doc\templates\index.html ..\doc\api
+xcopy /e ..\html\* ..\doc\api
+mkdir ..\doc\api\resources
+copy ..\..\..\lib\code_doc\templates\resources\* ..\doc\api\resources
+rmdir /s /q ..\html
+cd ..\doc
+%ZIP_BIN%\7z.exe a -r -tzip api.zip api\*
+move api.zip ..\..\..\..\docs
+cd ..\..
