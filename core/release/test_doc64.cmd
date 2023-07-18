64c60b809d0cc48a8b0ad06199476e855d4e6bc5
Randy Hollines <objeck@gmail.com>
2023-07-18 13:21:23 -0700

    added api doc example for regex

--- core/release/test_doc64.cmd
+++ core/release/test_doc64.cmd
@@ -6,7 +6,7 @@ cd deploy64\bin
 rmdir /s /q ..\html 
 mkdir ..\html
 obc -src ..\..\..\lib\code_doc\doc_html.obs,..\..\..\lib\code_doc\doc_parser.obs -lib xml -dest ..\..\code_doc.obe
-obr ..\..\code_doc.obe ..\..\..\lib\code_doc\templates 2023.7.3 ..\..\..\compiler\lib_src\json.obs ..\..\..\compiler\lib_src\xml.obs
+obr ..\..\code_doc.obe ..\..\..\lib\code_doc\templates 2023.7.3 ..\..\..\compiler\lib_src\json.obs ..\..\..\compiler\lib_src\xml.obs ..\..\..\compiler\lib_src\regex.obs
 rmdir /s /q ..\doc\api
 mkdir ..\doc\api
 copy ..\..\..\lib\code_doc\templates\index.html ..\doc\api
