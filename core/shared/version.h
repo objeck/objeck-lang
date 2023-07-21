6499f5264774e241ab12de588f1a11ec5e5b41c0
Randy Hollines <objeck@gmail.com>
2023-07-21 03:43:16 -0700

    bumping version, fixing 'leak' in the compiler

--- core/shared/version.h
+++ core/shared/version.h
@@ -35,8 +35,8 @@
 #define MAGIC_NUM_WEB 0xdddf
 #define MAGIC_NUM_LIB 0xddde
 #define MAGIC_NUM_EXE 0xdddd
-#define VER_NUM 202373
+#define VER_NUM 202374
 
-#define VERSION_STRING L"2023.7.3"
+#define VERSION_STRING L"2023.7.4"
 
 #endif
