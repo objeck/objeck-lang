14d79f2713708d6cdf3cf4ddc3490de215d1afb6
Randy Hollines <objeck@gmail.com>
2023-08-20 00:27:36 -0700

    example of embedding objeck in c++

---
+++ core/module/example/example.cpp
@@ -0,0 +1,54 @@
+#include "../lang.h"
+
+// read source from file, see below
+std::wstring ReadFile(const std::wstring filename);
+
+int main(int argc, char* argv[]) {
+#ifdef _DEBUG
+  OpenLogger("debug.log");
+#endif
+
+  if(argc == 2) {
+    // setup file name/source pair
+    std::vector<std::pair<std::wstring, std::wstring>> file_source;
+    const std::wstring filename = BytesToUnicode(argv[1]);
+    const std::wstring source = ReadFile(filename);
+    file_source.push_back(std::make_pair(filename, source));
+
+    // compile code
+    ObjeckLang lang(L"lang.obl");
+    if(lang.Compile(file_source, L"s2")) {
+      // execute
+      lang.Execute(L"");
+    }
+    // show errors
+    else {
+      auto errors = lang.GetErrors();
+      for(auto& error : errors) {
+        std::wcout << error << std::endl;
+      }
+    }
+  }
+
+#ifdef _DEBUG
+  CloseLogger();
+#endif
+
+  return 0;
+}
+
+std::wstring ReadFile(const std::wstring filename)
+{
+  std::wstring buffer;
+
+  std::wifstream file(filename);
+  if(file.good()) {
+    std::wstring line;
+    while(getline(file, line)) {
+      buffer += line;
+    }
+    file.close();
+  }
+
+  return buffer;
+}
