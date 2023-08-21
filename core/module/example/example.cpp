3bc514b5d48ab05c5c9a1d5e9246e259babe5fce
Randy Hollines <objeck@gmail.com>
2023-08-21 02:05:21 -0700

    updated embedded example to compile on msys2

--- core/module/example/example.cpp
+++ core/module/example/example.cpp
@@ -41,7 +41,7 @@ std::wstring ReadFile(const std::wstring filename)
 {
   std::wstring buffer;
 
-  std::wifstream file(filename);
+  std::wifstream file(filename.c_str());
   if(file.good()) {
     std::wstring line;
     while(getline(file, line)) {
