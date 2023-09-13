502ba40bd52a2528984c558876b9f6466bacda54
Randy Hollines <objeck@gmail.com>
2023-09-13 01:27:57 -0700

    macos build updates

--- core/module/xcode/build/module.build/Release/module.build/Objects-normal/arm64/linker.d
+++
@@ -1,9 +0,0 @@
-dependencies: \
-  /Users/randyhollines/Documents/Code/objeck-lang/core/compiler/linker.cpp \
-  /Users/randyhollines/Documents/Code/objeck-lang/core/compiler/linker.h \
-  /Users/randyhollines/Documents/Code/objeck-lang/core/compiler/types.h \
-  /Users/randyhollines/Documents/Code/objeck-lang/core/compiler/../shared/instrs.h \
-  /Users/randyhollines/Documents/Code/objeck-lang/core/compiler/../shared/sys.h \
-  /Users/randyhollines/Documents/Code/objeck-lang/core/compiler/../shared/logger.h \
-  /Users/randyhollines/Documents/Code/objeck-lang/core/compiler/../shared/traps.h \
-  /Users/randyhollines/Documents/Code/objeck-lang/core/compiler/../shared/version.h
