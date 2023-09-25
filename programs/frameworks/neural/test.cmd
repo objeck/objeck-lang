226ff42a9c31798ccc72ee38d3d538ad6ca9d66a
Randy Hollines <objeck@gmail.com>
2023-09-25 05:02:48 -0700

    clean up

--- programs/frameworks/neural/test.cmd
+++ programs/frameworks/neural/test.cmd
@@ -1,4 +1,10 @@
+
+if [%1] NEQ [data] goto end
+obc data\gen
+obr data\gen > data\test.csv
+:end
+
 obc -src ..\..\..\core\compiler\lib_src\ml.obs -tar lib -lib csv -dest ..\..\..\core\lib\ml.obl
 copy /y ..\..\..\core\lib\ml.obl ..\..\..\core\release\deploy64\lib\ml.obl
 obc -src nn6 -lib csv,ml
-obr nn6
+obr nn6 data\test.csv
