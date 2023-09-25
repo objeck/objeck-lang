1f8f6c320f9a303a39788e3c3c018d66a59d8e65
Randy Hollines <objeck@gmail.com>
2023-09-25 16:31:10 -0700

    nn lib

---
+++ programs/frameworks/neural/build.cmd
@@ -0,0 +1,18 @@
+@echo off
+
+REM if [%2] NEQ [data] goto data
+REM 	obc data\gen
+REM 	obr data\gen > data\test.csv
+REM :data
+
+obc -src ..\..\..\core\compiler\lib_src\ml.obs -tar lib -lib csv -dest ..\..\..\core\lib\ml.obl
+copy /y ..\..\..\core\lib\ml.obl ..\..\..\core\release\deploy64\lib\ml.obl
+
+obc -src nn6 -lib csv,ml
+
+if [%1] NEQ [test] goto test
+	echo [Training]
+	obr nn6 data\test.csv test
+	echo [Testing]
+	obr nn6 data\test.csv
+:test
