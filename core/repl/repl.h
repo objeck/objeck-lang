6a2539db347eb48e5a95afadf5fe5949499a383f
Randy Hollines <objeck@gmail.com>
2023-10-27 08:09:08 -0700

    v2023.10.0

--- core/repl/repl.h
+++ core/repl/repl.h
@@ -35,29 +35,29 @@
 #include "editor.h"
 #include "../module/lang.h"
 
-#define SYNTAX_ERROR "Huh, cannot do that. Try help '/h'?"
+constexpr wchar_t SYNTAX_ERROR[] = L"Huh, cannot do that. Try help '/h'?";
 
-#define HELP_PARAM L"help"
-#define HELP_ALT_PARAM L"h"
+constexpr wchar_t HELP_PARAM[] = L"help";
+constexpr wchar_t HELP_ALT_PARAM[] = L"h";
 
-#define FILE_PARAM L"file"
-#define FILE_ALT_PARAM L"f"
+constexpr wchar_t FILE_PARAM[] = L"file";
+constexpr wchar_t FILE_ALT_PARAM[] = L"f";
 
-#define INLINE_PARAM L"inline"
-#define INLINE_ALT_PARAM L"i"
+constexpr wchar_t INLINE_PARAM[] = L"inline";
+constexpr wchar_t INLINE_ALT_PARAM[] = L"i";
 
-#define LIBS_PARAM L"lib"
-#define LIBS_ALT_PARAM L"l"
+constexpr wchar_t LIBS_PARAM[] = L"lib";
+constexpr wchar_t LIBS_ALT_PARAM[] = L"l";
 
-#define OPT_PARAM L"opt"
-#define OPT_ALT_PARAM L"o"
+constexpr wchar_t OPT_PARAM[] = L"opt";
+constexpr wchar_t OPT_ALT_PARAM[] = L"o";
 
-#define EXIT_PARAM L"quit"
-#define EXIT_ALT_PARAM L"q"
+constexpr wchar_t EXIT_PARAM[] = L"quit";
+constexpr wchar_t EXIT_ALT_PARAM[] = L"q";
 
-static std::wstring GetArg(std::map<const std::wstring, std::wstring> arguments, const std::wstring values[]);
-static void RemoveArg(std::map<const std::wstring, std::wstring>& arguments, const std::wstring values[]);
-static bool HasArg(std::map<const std::wstring, std::wstring> arguments, const std::wstring values[]);
+static std::wstring GetArgument(std::map<const std::wstring, std::wstring> arguments, const std::wstring values[]);
+static void RemoveArgument(std::map<const std::wstring, std::wstring>& arguments, const std::wstring values[]);
+static bool HasArgument(std::map<const std::wstring, std::wstring> arguments, const std::wstring values[]);
 
 static void Usage();
 static void SetEnv();
