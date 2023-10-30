5c096483c97f2dc994ad0eeac6c4102419f3a138
Randy Hollines <objeck@gmail.com>
2023-10-30 09:20:51 -0700

    ml updates

--- programs/frameworks/neural/build.cmd
+++ programs/frameworks/neural/build.cmd
@@ -8,14 +8,8 @@ REM :data
 obc -src ..\..\..\core\compiler\lib_src\ml.obs -tar lib -lib csv -dest ..\..\..\core\lib\ml.obl
 copy /y ..\..\..\core\lib\ml.obl ..\..\..\core\release\deploy64\lib\ml.obl
 
-obc -src nn6 -lib csv,ml
+obc -src dt3 -lib csv,ml
 
 if [%1] NEQ [test] goto test
-	echo:
-	echo [Training]
-	obr nn6
-	echo:
-	echo [Testing]
-	obr nn6 test
-	echo:
+	obr dt3
 :test
