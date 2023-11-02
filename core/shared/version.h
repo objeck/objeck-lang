509d818f186d020a5dd3bd8f6b7a65b2ef9d99ee
Randy Hollines <objeck@gmail.com>
2023-11-02 01:22:43 -0700

    updated version

--- core/shared/version.h
+++ core/shared/version.h
@@ -38,7 +38,7 @@
 #define MAGIC_NUM_EXE 0xffbe // bitmask 'e'
 #define MAGIC_NUM_LIB 0xffb6 // bitmask 'k'
 
-#define VER_NUM 2023101
-#define VERSION_STRING L"2023.10.1"
+#define VER_NUM 2023110
+#define VERSION_STRING L"2023.11.0"
 
 #endif
