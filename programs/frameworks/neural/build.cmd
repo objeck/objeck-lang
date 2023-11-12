9bd25400b38ad303448b57e7975fc96822483a23
Randy Hollines <objeck@gmail.com>
2023-11-12 14:14:08 -0800

    add 'KNearestNeighbors' class to ML library

--- programs/frameworks/neural/build.cmd
+++ programs/frameworks/neural/build.cmd
@@ -5,8 +5,8 @@ set BASE_DIR=..\..\..
 obc -src %BASE_DIR%\core\compiler\lib_src\ml.obs -tar lib -lib csv -dest %BASE_DIR%\core\lib\ml.obl
 copy /y %BASE_DIR%\core\lib\ml.obl %BASE_DIR%\core\release\deploy64\lib\ml.obl
 
-obc -src %BASE_DIR%\programs\tests\prgm257.obs -lib csv,ml
+obc -src %BASE_DIR%\programs\tests\prgm258.obs -lib csv,ml
 
 if [%1] NEQ [test] goto test
-	obr %BASE_DIR%\programs\tests\prgm257.obe
+	obr %BASE_DIR%\programs\tests\prgm258.obe
 :test
