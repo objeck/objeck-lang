ead4f3d050b6e719503393e32650e13c7e125864
Randy Hollines <objeck@gmail.com>
2023-11-13 03:02:33 -0800

    updated version

--- core/shared/version.h
+++ core/shared/version.h
@@ -38,7 +38,7 @@
 #define MAGIC_NUM_EXE 0xffbe // bitmask 'e'
 #define MAGIC_NUM_LIB 0xffb6 // bitmask 'k'
 
-#define VER_NUM 2023111
-#define VERSION_STRING L"2023.11.1"
+#define VER_NUM 2023112
+#define VERSION_STRING L"2023.11.2"
 
 #endif
