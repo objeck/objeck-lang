23ae709367fb473ce50b03ee2a9645442e0f1230
Randy Hollines <objeck@gmail.com>
2023-11-29 15:38:55 -0800

    tweaking #407

--- core/vm/posix_main.cpp
+++ core/vm/posix_main.cpp
@@ -113,7 +113,11 @@ int main(const int argc, const char* argv[])
   else {
     wstring usage;
     usage += L"Usage: obr <program>\n\n";
-    usage += L"Example: \"obr hello.obe\"\n\nVersion: ";
+
+    usage += L"Options:\n";
+    usage += L"\t--GC_THRESHOLD:\t[input] inital garbage collection threshold <number>(K|M|G)\n";
+
+    usage += L"\nExample: \"obr hello.obe\"\n\nVersion: ";
     usage += VERSION_STRING;
     
 #if defined(_WIN64) && defined(_WIN32)
