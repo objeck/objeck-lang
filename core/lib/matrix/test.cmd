39940a8f5d8f4637d467f3932141b8dd287346d7
Randy Hollines <objeck@gmail.com>
2023-12-06 09:03:41 -0800

    changing the matrix bundle to use eigen, and optimized matrix math library

---
+++ core/lib/matrix/test.cmd
@@ -0,0 +1,12 @@
+@echo off
+
+set BASE_DIR=..\..\..
+
+obc -src %BASE_DIR%\core\compiler\lib_src\ml.obs -tar lib -lib csv -dest %BASE_DIR%\core\lib\ml.obl
+copy /y %BASE_DIR%\core\lib\ml.obl %BASE_DIR%\core\release\deploy64\lib\ml.obl
+
+obc -src lr3.obs -lib csv,ml
+
+if [%1] NEQ [test] goto test
+	obr lr3.obe
+:test
