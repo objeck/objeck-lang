c523834dd0eb8a8aadb191cfb2733c048b22ed7a
Randy Hollines <objeck@gmail.com>
2023-12-07 12:14:36 -0800

    adding eigen libaray to ml bundle

--- core/lib/matrix/matrix.cpp
+++ core/lib/matrix/matrix.cpp
@@ -64,42 +64,45 @@ extern "C" {
 #ifdef _WIN32
   __declspec(dllexport)
 #endif
-  void ml_matrix_add_sm(VMContext& context) {
+  void ml_matrix_add_scalar_matrix(VMContext& context) {
     const double value = (double)APITools_GetFloatValue(context, 1);
     size_t* matrix_obj = APITools_GetObjectValue(context, 2);
     
+    // get pointer to the FloatMatrixRef value
     size_t* data_ptr = (size_t*)*matrix_obj;
     if(!data_ptr) {
       std::wcerr << L">>> Attempting to dereference a 'Nil' memory element <<<" << std::endl;
       return;
     }
     
+    // ensure 2d matrix
     const size_t array_dim = data_ptr[1];
     if(array_dim != 2) {
       APITools_SetIntValue(context, 0, 0);
       return;
     }
 
-    const size_t array_size = data_ptr[0];
-    const size_t array_cols = data_ptr[2];
-    const size_t array_rows = data_ptr[3];
-
     // copy input values
+    const size_t array_rows = data_ptr[2];
+    const size_t array_cols = data_ptr[3];
+
     FLOAT_VALUE* input_values = (FLOAT_VALUE*)(data_ptr + array_dim + 2);
     Eigen::MatrixXd matrix = Eigen::Map<Matrix2D>(input_values, array_rows, array_cols);
 
     // add value
     matrix.array() += value;
     
-    // create and copy output buffer
-    size_t* output_ptr = APITools_MakeFloatArray(context, array_size);
+    // create results matrix
+    const size_t array_size = data_ptr[0];
+    size_t* output_ptr = APITools_MakeFloatArray(context, array_size + array_dim + 2);
     FLOAT_VALUE* output_values = (FLOAT_VALUE*)(output_ptr + array_dim + 2);
 
-    // TODO:
+    // copy results to output matrix
     Eigen::Map<Matrix2D>(output_values, matrix.rows(), matrix.cols()) = matrix;
     size_t* result_obj = APITools_GetObjectValue(context, 0);
     result_obj[0] = (size_t)output_ptr;
 
+    // set return value
     APITools_SetObjectValue(context, 0, result_obj);
   }
 }
--- core/lib/matrix/test.cmd
+++ core/lib/matrix/test.cmd
@@ -4,14 +4,12 @@ set BASE_DIR=..\..\..
 set PATH=%PATH%;%BASE_DIR%\core\release\deploy64\bin
 set OBJECK_LIB_PATH=%BASE_DIR%\core\release\deploy64\lib
 
-devenv matrix.sln /rebuild "Debug|x64"
-copy /y Debug\x64\libobjk_ml.dll %OBJECK_LIB_PATH%\native
-
 obc -src %BASE_DIR%\core\compiler\lib_src\ml.obs -tar lib -lib csv -dest %BASE_DIR%\core\lib\ml.obl
 copy /y %BASE_DIR%\core\lib\ml.obl %OBJECK_LIB_PATH%\ml.obl
 
 obc -src test.obs -lib csv,ml -asm
 
-if [%1] NEQ [test] goto test
-	obr lr3.obe
-:test
+if [%1] NEQ [dll] goto end
+	devenv matrix.sln /rebuild "Debug|x64"
+	copy /y Debug\x64\libobjk_ml.dll %OBJECK_LIB_PATH%\native
+:end
