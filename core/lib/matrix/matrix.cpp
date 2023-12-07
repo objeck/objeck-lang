05b7fa118eeb4cf1d0ea35502b42257b34d4829c
Randy Hollines <objeck@gmail.com>
2023-12-07 12:20:04 -0800

    adding eigen libaray to ml bundle

--- core/lib/matrix/matrix.cpp
+++ core/lib/matrix/matrix.cpp
@@ -105,42 +105,49 @@ extern "C" {
     // set return value
     APITools_SetObjectValue(context, 0, result_obj);
   }
-}
 
-Eigen::MatrixXd& CreateMatrix(size_t array_dim, size_t* data_ptr)
-{
-  const size_t array_size = data_ptr[0];
-  const size_t array_rows = data_ptr[1];
-  const size_t array_cols = data_ptr[2];
+#ifdef _WIN32
+  __declspec(dllexport)
+#endif
+  void ml_matrix_sub_scalar_matrix(VMContext& context) {
+    const double value = (double)APITools_GetFloatValue(context, 1);
+    size_t* matrix_obj = APITools_GetObjectValue(context, 2);
 
-  Eigen::MatrixXd matrix(array_rows, array_cols);
+    // get pointer to the FloatMatrixRef value
+    size_t* data_ptr = (size_t*)*matrix_obj;
+    if (!data_ptr) {
+      std::wcerr << L">>> Attempting to dereference a 'Nil' memory element <<<" << std::endl;
+      return;
+    }
 
-  size_t k = 0;
-  FLOAT_VALUE* values = (FLOAT_VALUE*)(data_ptr + array_dim + 2);
-  for (size_t i = 0; i < array_rows; i++) {
-    for (size_t j = 0; j < array_cols; j++) {
-      matrix(i, j) = values[k];
-      ++k;
-    };
+    // ensure 2d matrix
+    const size_t array_dim = data_ptr[1];
+    if (array_dim != 2) {
+      APITools_SetIntValue(context, 0, 0);
+      return;
+    }
+
+    // copy input values
+    const size_t array_rows = data_ptr[2];
+    const size_t array_cols = data_ptr[3];
+
+    FLOAT_VALUE* input_values = (FLOAT_VALUE*)(data_ptr + array_dim + 2);
+    Eigen::MatrixXd matrix = Eigen::Map<Matrix2D>(input_values, array_rows, array_cols);
+
+    // add value
+    matrix.array() -= value;
+
+    // create results matrix
+    const size_t array_size = data_ptr[0];
+    size_t* output_ptr = APITools_MakeFloatArray(context, array_size + array_dim + 2);
+    FLOAT_VALUE* output_values = (FLOAT_VALUE*)(output_ptr + array_dim + 2);
+
+    // copy results to output matrix
+    Eigen::Map<Matrix2D>(output_values, matrix.rows(), matrix.cols()) = matrix;
+    size_t* result_obj = APITools_GetObjectValue(context, 0);
+    result_obj[0] = (size_t)output_ptr;
+
+    // set return value
+    APITools_SetObjectValue(context, 0, result_obj);
   }
-
-  return matrix;
-}
-
-bool CopyMatrixToPtr(Eigen::MatrixXd& matrix, size_t array_dim, size_t* data_ptr)
-{
-  const size_t array_size = data_ptr[0];
-  const size_t array_rows = data_ptr[1];
-  const size_t array_cols = data_ptr[2];
-
-  size_t k = 0;
-  FLOAT_VALUE* values = (FLOAT_VALUE*)(data_ptr + array_dim + 2);
-  for(size_t i = 0; i < array_rows; i++) {
-    for(size_t j = 0; j < array_cols; j++) {
-      values[k] = matrix(i, j);
-      ++k;
-    };
-  }
-
-  return false;
 }
