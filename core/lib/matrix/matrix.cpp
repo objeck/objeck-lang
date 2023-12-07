f4bbddd7bfb2cceef75fb7bc248561ee57c8a270
Randy Hollines <objeck@gmail.com>
2023-12-07 10:15:25 -0800

    adding eigen libaray to ml bundle, needs debugging

--- core/lib/matrix/matrix.cpp
+++ core/lib/matrix/matrix.cpp
@@ -34,6 +34,11 @@
 #include "../../shared/sys.h"
 #include <Eigen/Dense>
 
+typedef Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::ColMajor> Matrix2D;
+
+static Eigen::MatrixXd& CreateMatrix(size_t array_dim, size_t* data_ptr);
+static bool CopyMatrixToPtr(Eigen::MatrixXd& matrix, size_t array_dim, size_t* array_ptr);
+
 extern "C" {
   //
   // initialize library
@@ -60,28 +65,79 @@ extern "C" {
   __declspec(dllexport)
 #endif
   void ml_matrix_add_sm(VMContext& context) {
-    const double c = (double)APITools_GetFloatValue(context, 1);
-    size_t* m_obj = APITools_GetObjectValue(context, 2);
-    m_obj = (size_t*)*m_obj;
-
-    std::wcout << c << std::endl;
-
-    const size_t m_dims = m_obj[1];
-    if (m_dims == 2) {
-      const size_t m_dim_rows = m_obj[2];
-      const size_t m_dim_cols = m_obj[3];
-
-      Eigen::MatrixXd m(m_dim_rows, m_dim_cols);
-
-      double x = m(1,1);
-      double y = m(1, 2);
-      
-
-      std::wcout << x << L", " << y << std::endl;
-
+    const double value = (double)APITools_GetFloatValue(context, 1);
+    size_t* matrix_obj = APITools_GetObjectValue(context, 2);
+    
+    size_t* data_ptr = (size_t*)*matrix_obj;
+    if(!data_ptr) {
+      std::wcerr << L">>> Attempting to dereference a 'Nil' memory element <<<" << std::endl;
+      return;
+    }
+    
+    const size_t array_dim = data_ptr[1];
+    if(array_dim != 2) {
+      APITools_SetIntValue(context, 0, 0);
       return;
     }
 
-    APITools_SetIntValue(context, 0, 0);
+    const size_t array_size = data_ptr[0];
+    const size_t array_cols = data_ptr[2];
+    const size_t array_rows = data_ptr[3];
+
+    // copy input values
+    FLOAT_VALUE* input_values = (FLOAT_VALUE*)(data_ptr + array_dim + 2);
+    Eigen::MatrixXd matrix = Eigen::Map<Matrix2D>(input_values, array_rows, array_cols);
+
+    // add value
+    matrix.array() += value;
+    
+    // create and copy output buffer
+    size_t* output_ptr = APITools_MakeFloatArray(context, array_size);
+    FLOAT_VALUE* output_values = (FLOAT_VALUE*)(output_ptr + array_dim + 2);
+
+    // TODO:
+    Eigen::Map<Matrix2D>(output_values, matrix.rows(), matrix.cols()) = matrix;
+    size_t* result_obj = APITools_GetObjectValue(context, 0);
+    result_obj[0] = (size_t)output_ptr;
+
+    APITools_SetObjectValue(context, 0, result_obj);
   }
+}
+
+Eigen::MatrixXd& CreateMatrix(size_t array_dim, size_t* data_ptr)
+{
+  const size_t array_size = data_ptr[0];
+  const size_t array_rows = data_ptr[1];
+  const size_t array_cols = data_ptr[2];
+
+  Eigen::MatrixXd matrix(array_rows, array_cols);
+
+  size_t k = 0;
+  FLOAT_VALUE* values = (FLOAT_VALUE*)(data_ptr + array_dim + 2);
+  for (size_t i = 0; i < array_rows; i++) {
+    for (size_t j = 0; j < array_cols; j++) {
+      matrix(i, j) = values[k];
+      ++k;
+    };
+  }
+
+  return matrix;
+}
+
+bool CopyMatrixToPtr(Eigen::MatrixXd& matrix, size_t array_dim, size_t* data_ptr)
+{
+  const size_t array_size = data_ptr[0];
+  const size_t array_rows = data_ptr[1];
+  const size_t array_cols = data_ptr[2];
+
+  size_t k = 0;
+  FLOAT_VALUE* values = (FLOAT_VALUE*)(data_ptr + array_dim + 2);
+  for(size_t i = 0; i < array_rows; i++) {
+    for(size_t j = 0; j < array_cols; j++) {
+      values[k] = matrix(i, j);
+      ++k;
+    };
+  }
+
+  return false;
 }
--- core/lib/matrix/test.cmd
+++ core/lib/matrix/test.cmd
@@ -10,7 +10,7 @@ copy /y Debug\x64\libobjk_ml.dll %OBJECK_LIB_PATH%\native
 obc -src %BASE_DIR%\core\compiler\lib_src\ml.obs -tar lib -lib csv -dest %BASE_DIR%\core\lib\ml.obl
 copy /y %BASE_DIR%\core\lib\ml.obl %OBJECK_LIB_PATH%\ml.obl
 
-obc -src test.obs -lib csv,ml
+obc -src test.obs -lib csv,ml -asm
 
 if [%1] NEQ [test] goto test
 	obr lr3.obe
