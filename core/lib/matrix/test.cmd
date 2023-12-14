8182bd4226bcbd8e20404dfbc5550b168efce7e8
Randy Hollines <objeck@gmail.com>
2023-12-14 05:44:21 -0800

    ml linear regression

--- core/lib/matrix/test.cmd
+++ core/lib/matrix/test.cmd
@@ -1,13 +1,15 @@
 @echo off
 
-set BASE_DIR=..\..\..
+set BASE_DIR=C:\Users\objec\Documents\Code\objeck-lang
 set PATH=%PATH%;%BASE_DIR%\core\release\deploy64\bin
 set OBJECK_LIB_PATH=%BASE_DIR%\core\release\deploy64\lib
 
 obc -src %BASE_DIR%\core\compiler\lib_src\ml.obs -tar lib -lib csv -dest %BASE_DIR%\core\lib\ml.obl
 copy /y %BASE_DIR%\core\lib\ml.obl %OBJECK_LIB_PATH%\ml.obl
 
-obc -src test.obs -lib csv,ml -asm
+pushd %BASE_DIR%\programs\tests
+obc -src prgm263.obs -lib csv,ml,net,json & obr prgm263
+popd
 
 if [%1] NEQ [dll] goto end
 	devenv matrix.sln /rebuild "Debug|x64"
