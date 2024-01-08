8df9fb036c9eeea0cb0174cecf2bc22a128bcb1c
Randy Hollines <objeck@gmail.com>
2024-01-08 07:26:09 -0800

    incorporating python into codebase, more to come

--- programs/tests/prgm269.py
+++ programs/tests/prgm269.py
@@ -4,15 +4,22 @@ import json
 response = requests.get('http://worldtimeapi.org/api/ip')
 if response.status_code == 200 :
 	time_dist = json.loads(response.content)
-	date_time_strs = time_dist['datetime'].split('T')
+	date_time_parts = time_dist['datetime'].split('T')
 
-	date_str = date_time_strs[0]
-	print(date_str)
+	date_parts = date_time_parts[0].split(':')
+	if len(date_parts) == 3 :
+		year = time_parts[0]
+		mon = time_parts[1]
+		day = time_parts[2]
 
-	time_str = date_time_strs[1]
+	time_str = date_time_parts[1]
 	index = time_str.find('.')
 	if index > -1 :
-		print(time_str[0 : index])
+		time_parts = time_str[0 : index].split(':')
+		if len(time_parts) == 3 :
+			hrs = time_parts[0]
+			mins = time_parts[1]
+			secs = time_parts[2]
 
 # TODO: convert to a string to a date
 # datetime.strptime(datetime_str, '%m/%d/%y %H:%M:%S')
