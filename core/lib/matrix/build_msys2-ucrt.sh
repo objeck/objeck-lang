804e1519c31f044903d00fcd6a803561330abef4
Randy Hollines <objeck@gmail.com>
2024-02-10 11:06:25 -0800

    fixing msys2 builds

--- core/lib/matrix/build_msys2-ucrt.sh
+++ core/lib/matrix/build_msys2-ucrt.sh
@@ -2,5 +2,5 @@
 rm -rf *.o
 rm -rf *.dll
 
-g++ $(pkg-config --cflags eigen3) -O3 -D_MSYS2f -std=c++11 -Wall -fPIC -c *$1.cpp -Wno-unused-function -Wno-address
+g++ $(pkg-config --cflags eigen3) -O3 -D_MSYS2 -std=c++11 -Wall -fPIC -c *$1.cpp -Wno-unused-function -Wno-address
 g++ -O3 -shared -Wl,-soname,$1.so.1 -o $1.dll *.o -lssl -lcrypto
