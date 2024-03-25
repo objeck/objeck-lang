dbf07318199ac68e8ba73ff02f99d026db7a0486
Randy Hollines <objeck@gmail.com>
2024-03-25 01:31:26 -0700

    updates to dockerfile

--- docker/build.cmd
+++ docker/build.cmd
@@ -1,16 +1,19 @@
-REM cls && build "objeck:2024.3.0" "\\wsl.localhost\Ubuntu\home\objeck\Documents\Code\objeck-lang"
+REM cls && build objeck:2024.3.0 "\\wsl.localhost\Ubuntu\home\{user}\Documents\Code\objeck-lang" "/home/{user}/Documents/Code/objeck-lang"
 
 rmdir /s /q deploy
-del /y *.tar
+del /q *.tar
+
+wsl --cd %3/core/release -e git reset --hard origin/master
+wsl --cd %3/core/release -e ./deploy_posix.sh 64
 
 docker rm objeck_nightly
 docker image prune --force --all
 
 xcopy /e /q %2\core\release\deploy deploy\
-docker build -t %1 . 
-docker save -o tmp.tar %1
-rename tmp.tar %1.tar
+docker build -t objeck:%1 . 
+docker save -o tmp.tar objeck:%1
+rename tmp.tar objeck-%1.tar
 
-docker run -d --name objeck_nightly -it %1
-docker run --rm -it %1 /bin/bash
+docker run -d --name objeck_nightly -it objeck:%1
+docker run --rm -it objeck:%1 /bin/bash
 REM docker attach objeck_nightly
