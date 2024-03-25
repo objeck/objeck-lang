1420a30ed65da7cc15d3c6f6cc2833fe9b403163
Randy Hollines <objeck@gmail.com>
2024-03-25 02:21:05 -0700

    updated docker windows build

--- docker/build.cmd
+++ docker/build.cmd
@@ -1,7 +1,15 @@
-REM cls && build objeck:2024.3.0 "\\wsl.localhost\Ubuntu\home\{user}\Documents\Code\objeck-lang" "/home/{user}/Documents/Code/objeck-lang"
+REM cls && build 2024.3.0 "\\wsl.localhost\Ubuntu\home\{user}\Documents\Code\objeck-lang" /home/{user}/Documents/Code/objeck-lang
 
-rmdir /s /q deploy
-del /q *.tar
+@echo off
+
+set arg_count=0
+for %%x in (%*) do set /a arg_count += 1
+
+if not "%arg_count%"=="3" (
+	goto usage
+)
+
+set ZIP_BIN="\Program Files\7-Zip\7z.exe"
 
 wsl --cd %3/core/release -e git reset --hard origin/master
 wsl --cd %3/core/release -e ./deploy_posix.sh 64
@@ -13,7 +21,16 @@ xcopy /e /q %2\core\release\deploy deploy\
 docker build -t objeck:%1 . 
 docker save -o tmp.tar objeck:%1
 rename tmp.tar objeck-%1.tar
+%ZIP_BIN% a -ttar -so objeck-%1.tar | %ZIP_BIN% a -si objeck-%1.tgz
+
+rmdir /s /q deploy
+del /q *.tar
 
 docker run -d --name objeck_nightly -it objeck:%1
-docker run --rm -it objeck:%1 /bin/bash
-REM docker attach objeck_nightly
+REM docker run --rm -it objeck:%1 /bin/bash
+goto end
+
+:usage
+echo Usage: [version] [local_objeck_path] [linx_objeck_path]
+
+:end
