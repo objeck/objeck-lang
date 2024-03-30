462dceaa80d80d098737931a81d2be8d4943ff6b
Randy Hollines <objeck@gmail.com>
2024-03-30 13:47:33 -0700

    code clean up

---
+++ programs/frameworks/gemini/build.cmd
@@ -0,0 +1,10 @@
+@echo off
+
+set OBJECK_ROOT=C:\Users\objec\Documents\Code\objeck-lang
+obc -src %OBJECK_ROOT%\core\compiler\lib_src\net_common.obs,%OBJECK_ROOT%\core\compiler\lib_src\net.obs,%OBJECK_ROOT%\core\compiler\lib_src\net_secure.obs -lib json -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\net.obl
+
+if [%1] == [] goto end
+	obc -src %1 -lib net,json -dest %1
+	if [%2] == [] goto end
+		obr %1 %2 %3 %4
+:end
