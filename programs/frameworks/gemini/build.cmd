f2a0d01e786378f9ce2dd91f7abc35bfb09582f8
Randy Hollines <objeck@gmail.com>
2024-04-03 13:43:30 -0600

    getting ready to build Gemini api client library

--- programs/frameworks/gemini/build.cmd
+++ programs/frameworks/gemini/build.cmd
@@ -3,7 +3,7 @@
 set OBJECK_ROOT=C:\Users\objec\Documents\Code\objeck-lang
 REM obc -src %OBJECK_ROOT%\core\compiler\lib_src\net_common.obs,%OBJECK_ROOT%\core\compiler\lib_src\net.obs,%OBJECK_ROOT%\core\compiler\lib_src\net_secure.obs -lib json -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\net.obl
 
-del /y *.obe
+del /q *.obe
 
 if [%1] == [] goto end
 	obc -src %1 -lib net,json,encrypt -dest %1
