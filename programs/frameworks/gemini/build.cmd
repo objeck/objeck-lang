8302657141fe60a93406ae4c59502dff47a9ca05
Randy Hollines <objeck@gmail.com>
2024-04-03 03:22:40 -0600

    poc for gemini image support

--- programs/frameworks/gemini/build.cmd
+++ programs/frameworks/gemini/build.cmd
@@ -1,10 +1,12 @@
 @echo off
 
 set OBJECK_ROOT=C:\Users\objec\Documents\Code\objeck-lang
-obc -src %OBJECK_ROOT%\core\compiler\lib_src\net_common.obs,%OBJECK_ROOT%\core\compiler\lib_src\net.obs,%OBJECK_ROOT%\core\compiler\lib_src\net_secure.obs -lib json -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\net.obl
+REM obc -src %OBJECK_ROOT%\core\compiler\lib_src\net_common.obs,%OBJECK_ROOT%\core\compiler\lib_src\net.obs,%OBJECK_ROOT%\core\compiler\lib_src\net_secure.obs -lib json -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\net.obl
+
+del /y *.obe
 
 if [%1] == [] goto end
-	obc -src %1 -lib net,json -dest %1
+	obc -src %1 -lib net,json,encrypt -dest %1
 	if [%2] == [] goto end
-		obr %1 %2 %3 %4
+		obr %1 %2 %3 %4 %5
 :end
