0086279ea433c5d202a3a92d940101351034214a
Randy Hollines <objeck@gmail.com>
2024-04-05 21:41:57 -0600

    adding gemini library

--- programs/frameworks/gemini/build.cmd
+++ programs/frameworks/gemini/build.cmd
@@ -1,11 +1,11 @@
 @echo off
 
 set OBJECK_ROOT=C:\Users\objec\Documents\Code\objeck-lang
-REM obc -src %OBJECK_ROOT%\core\compiler\lib_src\net_common.obs,%OBJECK_ROOT%\core\compiler\lib_src\net.obs,%OBJECK_ROOT%\core\compiler\lib_src\net_secure.obs -lib json -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\net.obl
+obc -src %OBJECK_ROOT%\core\compiler\lib_src\gemini.obs -lib json,net,encrypt -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\gemini.obl
 
 del /q *.obe
 
 if [%1] == [] goto end
-	obc -src %1 -lib net,json,encrypt -dest %1
+	obc -src %1 -lib net,json,encrypt,gemini -dest %1
 	obr %1 %2 %3 %4 %5
 :end
