641e6e3aa603a78f6f31ee7e9a2ad8185551e1ee
Randy Hollines <objeck@gmail.com>
2024-04-08 12:53:00 -0600

    openai function calling support

--- programs/frameworks/openai/build.cmd
+++ programs/frameworks/openai/build.cmd
@@ -2,7 +2,7 @@
 
 set OBJECK_ROOT=C:\Users\objec\Documents\Code\objeck-lang
 REM obc -src %OBJECK_ROOT%\core\compiler\lib_src\net_common.obs,%OBJECK_ROOT%\core\compiler\lib_src\net.obs,%OBJECK_ROOT%\core\compiler\lib_src\net_secure.obs -lib json -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\net.obl
-obc -src %OBJECK_ROOT%\core\compiler\lib_src\openai.obs -lib json,net -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\openai.obl
+obc -src %OBJECK_ROOT%\core\compiler\lib_src\openai.obs -lib json,net,misc -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\openai.obl
 
 if [%1] == [] goto end
 	obc -src %1 -lib net,json,misc,openai -dest %1
