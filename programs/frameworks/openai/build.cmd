51b58a5455b9e2a398590b54c475e54d6b510e79
Randy Hollines <objeck@gmail.com>
2024-04-08 09:30:09 -0600

    adding openai chat test

--- programs/frameworks/openai/build.cmd
+++ programs/frameworks/openai/build.cmd
@@ -5,7 +5,7 @@ REM obc -src %OBJECK_ROOT%\core\compiler\lib_src\net_common.obs,%OBJECK_ROOT%\co
 obc -src %OBJECK_ROOT%\core\compiler\lib_src\openai.obs -lib json,net -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\openai.obl
 
 if [%1] == [] goto end
-	obc -src %1 -lib net,json,openai -dest %1
+	obc -src %1 -lib net,json,misc,openai -dest %1
 	if [%2] == [] goto end
 		obr %1 %2 %3 %4 %5
 :end
