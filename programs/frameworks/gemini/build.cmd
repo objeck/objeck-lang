ba15964ced886c0b203c947e7d60149182a3b648
Randy Hollines <objeck@gmail.com>
2024-04-14 18:03:32 -0600

    moved gemini corpus apis to gemini library

--- programs/frameworks/gemini/build.cmd
+++ programs/frameworks/gemini/build.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 set OBJECK_ROOT=C:\Users\objec\Documents\Code\objeck-lang
-obc -src %OBJECK_ROOT%\core\compiler\lib_src\net_common.obs,%OBJECK_ROOT%\core\compiler\lib_src\net.obs,%OBJECK_ROOT%\core\compiler\lib_src\net_secure.obs -lib json -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\net.obl
+REM obc -src %OBJECK_ROOT%\core\compiler\lib_src\net_common.obs,%OBJECK_ROOT%\core\compiler\lib_src\net.obs,%OBJECK_ROOT%\core\compiler\lib_src\net_secure.obs -lib json -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\net.obl
 obc -src %OBJECK_ROOT%\core\compiler\lib_src\gemini.obs -lib misc,json,net,encrypt -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\gemini.obl
 
 del /q *.obe
