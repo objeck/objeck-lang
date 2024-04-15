85bd7f52c37f47ce61b8030633e4a570f9828f03
Randy Hollines <objeck@gmail.com>
2024-04-15 13:47:19 -0600

    docing gemini apis

--- programs/frameworks/gemini/build.cmd
+++ programs/frameworks/gemini/build.cmd
@@ -1,4 +1,4 @@
-@echo off
+REM @echo off
 
 set OBJECK_ROOT=C:\Users\objec\Documents\Code\objeck-lang
 obc -src %OBJECK_ROOT%\core\compiler\lib_src\net_common.obs,%OBJECK_ROOT%\core\compiler\lib_src\net.obs,%OBJECK_ROOT%\core\compiler\lib_src\net_secure.obs -lib json -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\net.obl
