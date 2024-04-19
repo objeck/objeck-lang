f4f6654aaa134e636cb6cdafd3196b858267f559
Randy Hollines <objeck@gmail.com>
2024-04-18 18:05:23 -0600

    testing release

--- programs/frameworks/gemini/build.cmd
+++ programs/frameworks/gemini/build.cmd
@@ -2,6 +2,7 @@
 
 set OBJECK_ROOT=C:\Users\objec\Documents\Code\objeck-lang
 obc -src %OBJECK_ROOT%\core\compiler\lib_src\net_common.obs,%OBJECK_ROOT%\core\compiler\lib_src\net.obs,%OBJECK_ROOT%\core\compiler\lib_src\net_secure.obs -lib json -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\net.obl
+obc -src %OBJECK_ROOT%\core\compiler\lib_src\json.obs -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\json.obl
 obc -src %OBJECK_ROOT%\core\compiler\lib_src\gemini.obs -lib misc,json,net,encrypt -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\gemini.obl
 
 del /q *.obe
