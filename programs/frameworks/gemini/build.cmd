b4df3ffd509fb73cadd9e9d2811bb27cc36bca79
Randy Hollines <objeck@gmail.com>
2024-04-19 12:54:40 -0600

    testing embeddings

--- programs/frameworks/gemini/build.cmd
+++ programs/frameworks/gemini/build.cmd
@@ -1,4 +1,4 @@
-@echo off
+rem @echo off
 
 set OBJECK_ROOT=C:\Users\objec\Documents\Code\objeck-lang
 obc -src %OBJECK_ROOT%\core\compiler\lib_src\net_common.obs,%OBJECK_ROOT%\core\compiler\lib_src\net.obs,%OBJECK_ROOT%\core\compiler\lib_src\net_secure.obs -lib json -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\net.obl
@@ -8,6 +8,6 @@ obc -src %OBJECK_ROOT%\core\compiler\lib_src\gemini.obs -lib misc,json,net,encry
 del /q *.obe
 
 if [%1] == [] goto end
-	obc -src %1 -lib misc,net,json,encrypt,gemini -dest %1
+	obc -src %1 -lib csv,misc,ml,net,json,encrypt,gemini -dest %1
 	obr %1 %2 %3 %4 %5
 :end
