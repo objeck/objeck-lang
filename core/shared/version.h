edb6f6182aeef7ed30ba8659fd6abc1158b189bd
Randy Hollines <objeck@gmail.com>
2024-04-29 12:54:24 -0600

    bumped version

--- core/shared/version.h
+++ core/shared/version.h
@@ -38,7 +38,7 @@
 #define MAGIC_NUM_EXE 0xffbe // bitmask 'e'
 #define MAGIC_NUM_LIB 0xffb6 // bitmask 'k'
 
-#define VER_NUM 202441
-#define VERSION_STRING L"2024.4.1"
+#define VER_NUM 202450
+#define VERSION_STRING L"2024.5.0"
 
 #endif
