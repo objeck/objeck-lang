0ad3146399aac8bc377f328bd82dac657e7fa400
Randy Hollines <objeck@gmail.com>
2024-05-03 13:17:10 -0600

    updated amd64 flags for #496

--- programs/frameworks/openai/build.cmd
+++ programs/frameworks/openai/build.cmd
@@ -5,7 +5,9 @@ obc -src %OBJECK_ROOT%\core\compiler\lib_src\net_common.obs,%OBJECK_ROOT%\core\c
 obc -src %OBJECK_ROOT%\core\compiler\lib_src\openai.obs -lib json,net,misc -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\openai.obl
 
 if [%1] == [] goto end
-	obc -src %1 -lib net,json,misc,openai -dest %1
+	set a=%1:~0,-3%
+	echo %a%
+	obc -src %1 -lib net,json,misc,openai -dest 
 	if [%2] == [] goto end
 		obr %1 %2 %3 %4 %5
 :end
