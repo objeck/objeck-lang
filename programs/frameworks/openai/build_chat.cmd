69106eab9259bc6d5b8de1250b18d87c2322c961
Randy Hollines <objeck@gmail.com>
2024-05-06 19:32:47 -0600

    adding openai function callback example

--- programs/frameworks/openai/build_chat.cmd
+++ programs/frameworks/openai/build_chat.cmd
@@ -1,3 +1,8 @@
+set OBJECK_ROOT=C:\Users\objec\Documents\Code\objeck-lang
+
+obc -src %OBJECK_ROOT%\core\compiler\lib_src\openai.obs -lib json,net,misc -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\openai.obl
+
+
 obc -src openai_chat -lib openai,csv,net,json,misc
 obr openai_chat %1 %2
 
