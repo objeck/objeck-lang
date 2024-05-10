e9a623848b315f9adb6a805c851d3fe706b2a6ea
Randy Hollines <objeck@gmail.com>
2024-05-10 15:32:20 -0600

    prepping to add function call support to gemini api

--- programs/frameworks/gemini/build.cmd
+++ programs/frameworks/gemini/build.cmd
@@ -1,9 +1,9 @@
-rem @echo off
+@echo off
 
 set OBJECK_ROOT=C:\Users\objec\Documents\Code\objeck-lang
-obc -src %OBJECK_ROOT%\core\compiler\lib_src\net_common.obs,%OBJECK_ROOT%\core\compiler\lib_src\net.obs,%OBJECK_ROOT%\core\compiler\lib_src\net_secure.obs -lib json -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\net.obl
-obc -src %OBJECK_ROOT%\core\compiler\lib_src\json.obs -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\json.obl
-obc -src %OBJECK_ROOT%\core\compiler\lib_src\ml.obs -lib json,csv -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\ml.obl
+REM obc -src %OBJECK_ROOT%\core\compiler\lib_src\net_common.obs,%OBJECK_ROOT%\core\compiler\lib_src\net.obs,%OBJECK_ROOT%\core\compiler\lib_src\net_secure.obs -lib json -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\net.obl
+REM obc -src %OBJECK_ROOT%\core\compiler\lib_src\json.obs -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\json.obl
+REM obc -src %OBJECK_ROOT%\core\compiler\lib_src\ml.obs -lib json,csv -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\ml.obl
 obc -src %OBJECK_ROOT%\core\compiler\lib_src\gemini.obs -lib misc,json,net,encrypt -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\gemini.obl
 
 del /q *.obe
