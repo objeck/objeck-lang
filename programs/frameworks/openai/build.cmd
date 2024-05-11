48609808b01d1627b64d6c777468d7df25f02724
Randy Hollines <objeck@gmail.com>
2024-05-11 06:47:14 -0600

    refactoring and adding support for gemini function calling

--- programs/frameworks/openai/build.cmd
+++ programs/frameworks/openai/build.cmd
@@ -1,7 +1,13 @@
 @echo off
 
 set OBJECK_ROOT=C:\Users\objec\Documents\Code\objeck-lang
-obc -src %OBJECK_ROOT%\core\compiler\lib_src\net_common.obs,%OBJECK_ROOT%\core\compiler\lib_src\net.obs,%OBJECK_ROOT%\core\compiler\lib_src\net_secure.obs -lib json -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\net.obl
+SET OBJECK_LIB_SRC=%OBJECK_ROOT%\core\compiler\lib_src
+SET OBJECK_LIB_DST=%OBJECK_ROOT%\core\release\deploy64\lib
+
+obc -src %OBJECK_LIB_SRC%\json.obs -lib gen_collect -tar lib -opt s3 -dest %OBJECK_LIB_DST%\json.obl
+REM obc -src %OBJECK_LIB_SRC%\net_common.obs,%OBJECK_LIB_SRC%\net.obs,%OBJECK_LIB_SRC%\net_secure.obs -lib json -tar lib -dest %OBJECK_LIB_DST%\net.obl
+REM obc -src %OBJECK_LIB_SRC%\json.obs -tar lib -dest %OBJECK_LIB_DST%\json.obl
+REM obc -src %OBJECK_LIB_SRC%\ml.obs -lib json,csv -tar lib -dest %OBJECK_LIB_DST%\ml.obl
 obc -src %OBJECK_ROOT%\core\compiler\lib_src\openai.obs -lib json,net,misc -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\openai.obl
 
 if [%1] == [] goto end
