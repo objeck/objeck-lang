11746dfff28674bf3a0a0d0c548387eedbaaf01e
Randy Hollines <objeck@gmail.com>
2024-05-11 22:27:03 -0600

    wip - support for gemini function calling

--- programs/frameworks/gemini/build.cmd
+++ programs/frameworks/gemini/build.cmd
@@ -5,9 +5,9 @@ SET OBJECK_LIB_SRC=%OBJECK_ROOT%\core\compiler\lib_src
 SET OBJECK_LIB_DST=%OBJECK_ROOT%\core\release\deploy64\lib
 
 obc -src %OBJECK_LIB_SRC%\json.obs -lib gen_collect -tar lib -opt s3 -dest %OBJECK_LIB_DST%\json.obl
-REM obc -src %OBJECK_LIB_SRC%\net_common.obs,%OBJECK_LIB_SRC%\net.obs,%OBJECK_LIB_SRC%\net_secure.obs -lib json -tar lib -dest %OBJECK_LIB_DST%\net.obl
-REM obc -src %OBJECK_LIB_SRC%\json.obs -tar lib -dest %OBJECK_LIB_DST%\json.obl
-REM obc -src %OBJECK_LIB_SRC%\ml.obs -lib json,csv -tar lib -dest %OBJECK_LIB_DST%\ml.obl
+obc -src %OBJECK_LIB_SRC%\net_common.obs,%OBJECK_LIB_SRC%\net.obs,%OBJECK_LIB_SRC%\net_secure.obs -lib json -tar lib -dest %OBJECK_LIB_DST%\net.obl
+obc -src %OBJECK_LIB_SRC%\json.obs -tar lib -dest %OBJECK_LIB_DST%\json.obl
+obc -src %OBJECK_LIB_SRC%\ml.obs -lib json,csv -tar lib -dest %OBJECK_LIB_DST%\ml.obl
 obc -src %OBJECK_LIB_SRC%\gemini.obs -lib misc,json,net,encrypt -tar lib -dest %OBJECK_LIB_DST%\gemini.obl
 
 del /q *.obe
