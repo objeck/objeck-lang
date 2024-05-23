f91c63d0d58e9bb41c1c3815b41d8e666d0e97ba
Randy Hollines <objeck@gmail.com>
2024-05-23 17:44:46 -0600

    working on openai tuning support

---
+++ programs/frameworks/openai/build_tuning.cmd
@@ -0,0 +1,10 @@
+@echo off
+
+set OBJECK_ROOT=C:\Users\objec\Documents\Code\objeck-lang
+set OBJECK_LIB_PATH=%OBJECK_ROOT%\core\release\deploy64\lib
+
+obc -src %OBJECK_ROOT%\core\compiler\lib_src\openai.obs -lib json,net,misc -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\openai.obl
+
+del /q *.obe
+obc -src openai_tune -lib openai,csv,net,json,misc
+obr openai_tune %1 %2
