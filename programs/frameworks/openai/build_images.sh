53f637f33cb1854f88803e7dc36dc892da1950df
Randy Hollines <objeck@gmail.com>
2024-05-24 16:58:55 -0600

    mergering openai work

---
+++ programs/frameworks/openai/build_images.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+export OBJECK_ROOT=/Users/randyhollines/Documents/Code/objeck-lang
+export OBJECK_LIB_PATH=$OBJECK_ROOT/core/release/deploy/lib
+export PATH=$PATH:$OBJECK_ROOT/core/release/deploy/bin
+
+obc -src $OBJECK_ROOT/core/compiler/lib_src/openai.obs -lib json,net,misc -tar lib -dest $OBJECK_ROOT/core/release/deploy/lib/openai.obl
+
+rm -f *.obe
+obc -src openai_images -lib openai,csv,net,json,misc
+obr openai_images $1 $2
