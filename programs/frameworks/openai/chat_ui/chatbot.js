2ced04f5773f96468254f5059a34648acdb7d2a2
Randy Hollines <objeck@gmail.com>
2024-06-23 08:43:59 -0600

    chat web client demo

---
+++ programs/frameworks/openai/chat_ui/chatbot.js
@@ -0,0 +1,72 @@
+const chatInput = document.querySelector('.chat-input textarea');
+const sendChatBtn = document.querySelector('.chat-input button');
+const chatbox = document.querySelector(".chatbox");
+
+let userMessage;
+
+const createChatLi = (message, className) => {
+	const chatLi = document.createElement("li");
+	chatLi.classList.add("chat", className);
+	
+	let chatContent = className === "chat-outgoing" ? `<p>${message}</p>` : `<p>${message}</p>`;
+	chatLi.innerHTML = chatContent;
+
+	return chatLi;
+}
+
+const generateResponse = (incomingChatLi) => {
+	const messageElement = incomingChatLi.querySelector("p");
+	
+	url = "http://localhost:1123/completion";
+	const requestOptions = {
+        method: "POST",
+        body:  userMessage,
+    }
+
+	fetch(url, requestOptions)
+        .then(res => {
+            if (!res.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return res.json();
+        })
+        .then(data => {
+            messageElement.textContent = data
+        })
+        .catch((error) => {
+            messageElement.classList.add("error");
+            messageElement.textContent = "Oops! Something went wrong. Please try again!";
+        })
+        .finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
+};
+
+const handleChat = () => {
+	userMessage = chatInput.value.trim();
+	if(!userMessage) {
+		return;
+	}
+
+	chatbox.appendChild(createChatLi(userMessage, "chat-outgoing"));
+	chatbox.scrollTo(0, chatbox.scrollHeight);
+
+	setTimeout(() => {
+		const incomingChatLi = createChatLi("Thinking...", "chat-incoming")
+		chatbox.appendChild(incomingChatLi);
+		chatbox.scrollTo(0, chatbox.scrollHeight);
+		generateResponse(incomingChatLi);
+	}, 600);
+}
+
+sendChatBtn.addEventListener("click", handleChat);
+
+function cancel() {
+	let chatbotcomplete = document.querySelector(".chatBot");
+	if(chatbotcomplete.style.display != 'none') {
+		chatbotcomplete.style.display = "none";
+		
+		let lastMsg = document.createElement("p");
+		lastMsg.textContent = 'Thanks for using our Chatbot!';
+		lastMsg.classList.add('lastMessage');
+		document.body.appendChild(lastMsg)
+	}
+}
