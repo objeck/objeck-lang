71568cba665d70a9bc79662a2ecabf5d206cb9ce
Randy Hollines <objeck@gmail.com>
2024-07-30 05:11:18 -0600

    adding better type inference for generics in libraries

--- core/compiler/tree.cpp
+++ core/compiler/tree.cpp
@@ -355,10 +355,7 @@ std::wstring Method::EncodeType(Type* type, Class* klass, ParsedProgram* program
 
     // generics
     if(type->HasGenerics()) {
-      const std::vector<Type*> generic_types = type->GetGenerics();
-      for(size_t i = 0; i < generic_types.size(); ++i) {
-        name += L"|" + generic_types[i]->GetName();
-      }
+      name += EncodeGenerics(type);
     }
 
     // dimension
@@ -366,6 +363,22 @@ std::wstring Method::EncodeType(Type* type, Class* klass, ParsedProgram* program
       name += L'*';
     }
   }
+
+  return name;
+}
+
+std::wstring Method::EncodeGenerics(Type* type)
+{
+  std::wstring name = L"(";
+
+  const std::vector<Type*> generic_types = type->GetGenerics();
+  for(size_t i = 0; i < generic_types.size(); ++i) {
+    name += generic_types[i]->GetName();
+    if(i + 1 < generic_types.size()) {
+      name += L',';
+    }
+  }
+  name += L')';
 
   return name;
 }
@@ -439,10 +452,7 @@ std::wstring Method::EncodeType(Type* type) {
 
     // generics
     if(type->HasGenerics()) {
-      const std::vector<Type*> generic_types = type->GetGenerics();
-      for(size_t i = 0; i < generic_types.size(); ++i) {
-        name += L"|" + generic_types[i]->GetName();
-      }
+      name += EncodeGenerics(type);
     }
 
     // dimension
