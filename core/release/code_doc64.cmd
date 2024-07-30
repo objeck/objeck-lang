8e7dcfe826ec6582eee83ec5022ec8cac6fa9845
Randy Hollines <objeck@gmail.com>
2024-07-30 06:30:22 -0600

    adding better type inference for generics in libraries

--- core/release/code_doc64.cmd
+++ core/release/code_doc64.cmd
@@ -3,7 +3,7 @@ set ZIP_BIN="\Program Files\7-Zip"
 cd deploy64\bin
 rmdir /s /q ..\html 
 mkdir ..\html
-obc -src ..\..\..\lib\code_doc\doc_html.obs,..\..\..\lib\code_doc\doc_parser.obs -lib xml -dest ..\..\code_doc.obe -asm
+obc -src ..\..\..\lib\code_doc\doc_html.obs,..\..\..\lib\code_doc\doc_parser.obs -lib xml -dest ..\..\code_doc.obe
 obr ..\..\code_doc.obe ..\..\..\lib\code_doc\templates 2024.8.0 ..\..\..\compiler\lib_src\lang.obs ..\..\..\compiler\lib_src\math.obs ..\..\..\compiler\lib_src\regex.obs ..\..\..\compiler\lib_src\json_stream.obs ..\..\..\compiler\lib_src\json.obs ..\..\..\compiler\lib_src\xml.obs ..\..\..\compiler\lib_src\encrypt.obs ..\..\..\compiler\lib_src\odbc.obs ..\..\..\compiler\lib_src\odbc.obs ..\..\..\compiler\lib_src\csv.obs ..\..\..\compiler\lib_src\query.obs ..\..\..\compiler\lib_src\sdl2.obs ..\..\..\compiler\lib_src\sdl_game.obs ..\..\..\compiler\lib_src\gen_collect.obs ..\..\..\compiler\lib_src\net_common.obs ..\..\..\compiler\lib_src\net.obs ..\..\..\compiler\lib_src\net_secure.obs ..\..\..\compiler\lib_src\rss.obs ..\..\..\compiler\lib_src\misc.obs ..\..\..\compiler\lib_src\diags.obs ..\..\..\compiler\lib_src\ml.obs ..\..\..\compiler\lib_src\openai.obs ..\..\..\compiler\lib_src\gemini.obs ..\..\..\compiler\lib_src\ollama.obs
 rmdir /s /q ..\doc\api
 mkdir ..\doc\api
