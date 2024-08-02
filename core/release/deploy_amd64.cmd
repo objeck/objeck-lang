d8f1f47ee843d65360eb143ed11b4fb29bd6082a
Randy Hollines <objeck@gmail.com>
2024-08-02 06:03:15 -0600

    v2024.8.0

--- core/release/deploy_amd64.cmd
+++ core/release/deploy_amd64.cmd
@@ -88,9 +88,10 @@ mkdir deploy64\doc\syntax
 xcopy /e ..\..\docs\syntax\* deploy64\doc\syntax
 
 REM update and process readme
-pushd ..\..\programs\deploy\util\readme
-call build.cmd readme_builder readme.json
-popd && copy ..\..\docs\readme.html deploy64
+REM pushd ..\..\programs\deploy\util\readme
+REM call build.cmd readme_builder readme.json
+REM popd && 
+copy ..\..\docs\readme.html deploy64
 
 copy ..\..\docs\doc\readme.css deploy64\doc
 copy ..\..\LICENSE deploy64
