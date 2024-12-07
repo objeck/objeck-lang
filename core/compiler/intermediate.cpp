b662722417690d1654ca1d3e83c8db6acabde8d4
Randy Hollines <objeck@gmail.com>
2024-12-07 08:08:56 -0700

    adding udp support

--- core/compiler/intermediate.cpp
+++ core/compiler/intermediate.cpp
@@ -2195,42 +2195,29 @@ void IntermediateEmitter::EmitSystemDirective(SystemStatement* statement)
 
 
 
+    /*
+
+    SOCK_UDP_CONNECT,
+      SOCK_UDP_BIND,
+      SOCK_UDP_LISTEN,
+      SOCK_UDP_ACCEPT,
+      SOCK_UDP_IS_CONNECTED,
+      SOCK_UDP_CLOSE,
+      SOCK_UDP_IN_BYTE,
+      SOCK_UDP_IN_BYTE_ARY,
+      SOCK_UDP_IN_CHAR_ARY,
+      SOCK_UDP_OUT_BYTE,
+      SOCK_UDP_OUT_BYTE_ARY,
+      SOCK_UDP_OUT_CHAR_ARY,
+      SOCK_UDP_IN_STRING,
+      SOCK_UDP_OUT_STRING,
+      SOCK_UDP_HOST_NAME,
+      SOCK_UDP_RESOLVE_NAME,
+      SOCK_UDP_ERROR,
+  */
 
 
-  case instructions::SOCK_UDP_BIND:
-    imm_block->AddInstruction(IntermediateFactory::Instance()->MakeInstruction(statement, cur_line_num, LOAD_INST_MEM));
-    imm_block->AddInstruction(IntermediateFactory::Instance()->MakeInstruction(statement, cur_line_num, LOAD_INT_VAR, 0, LOCL));
-    imm_block->AddInstruction(IntermediateFactory::Instance()->MakeIntLitInstruction(statement, cur_line_num, instructions::SOCK_UDP_BIND));
-    imm_block->AddInstruction(IntermediateFactory::Instance()->MakeInstruction(statement, cur_line_num, TRAP, 3L));
-    break;
 
-  case instructions::SOCK_UDP_LISTEN:
-    imm_block->AddInstruction(IntermediateFactory::Instance()->MakeInstruction(statement, cur_line_num, LOAD_INST_MEM));
-    imm_block->AddInstruction(IntermediateFactory::Instance()->MakeInstruction(statement, cur_line_num, LOAD_INT_VAR, 0, LOCL));
-    imm_block->AddInstruction(IntermediateFactory::Instance()->MakeIntLitInstruction(statement, cur_line_num, instructions::SOCK_UDP_LISTEN));
-    imm_block->AddInstruction(IntermediateFactory::Instance()->MakeInstruction(statement, cur_line_num, TRAP, 3L));
-    break;
-
-  case instructions::SOCK_UDP_ACCEPT:
-    imm_block->AddInstruction(IntermediateFactory::Instance()->MakeInstruction(statement, cur_line_num, LOAD_INST_MEM));
-    imm_block->AddInstruction(IntermediateFactory::Instance()->MakeInstruction(statement, cur_line_num, LOAD_INT_VAR, 0, LOCL));
-    imm_block->AddInstruction(IntermediateFactory::Instance()->MakeIntLitInstruction(statement, cur_line_num, instructions::SOCK_UDP_ACCEPT));
-    imm_block->AddInstruction(IntermediateFactory::Instance()->MakeInstruction(statement, cur_line_num, TRAP, 3L));
-    break;
-
-  case instructions::SOCK_UDP_CLOSE:
-    imm_block->AddInstruction(IntermediateFactory::Instance()->MakeInstruction(statement, cur_line_num, LOAD_INST_MEM));
-    imm_block->AddInstruction(IntermediateFactory::Instance()->MakeInstruction(statement, cur_line_num, LOAD_INT_VAR, 0, LOCL));
-    imm_block->AddInstruction(IntermediateFactory::Instance()->MakeIntLitInstruction(statement, cur_line_num, instructions::SOCK_UDP_CLOSE));
-    imm_block->AddInstruction(IntermediateFactory::Instance()->MakeInstruction(statement, cur_line_num, TRAP, 3L));
-    break;
-
-  case instructions::SOCK_UDP_ERROR:
-    imm_block->AddInstruction(IntermediateFactory::Instance()->MakeInstruction(statement, cur_line_num, LOAD_INST_MEM));
-    imm_block->AddInstruction(IntermediateFactory::Instance()->MakeInstruction(statement, cur_line_num, LOAD_INT_VAR, 0, LOCL));
-    imm_block->AddInstruction(IntermediateFactory::Instance()->MakeIntLitInstruction(statement, cur_line_num, instructions::SOCK_UDP_ERROR));
-    imm_block->AddInstruction(IntermediateFactory::Instance()->MakeInstruction(statement, cur_line_num, TRAP, 3L));
-    break;
 
 
 
