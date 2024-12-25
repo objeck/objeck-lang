6e22e0c5f9028075f08e638438ff0270c4cffd5a
Randy Hollines <objeck@gmail.com>
2024-12-24 21:45:20 -0700

    updates to windows on arm build

--- core/compiler/parser.cpp
+++ core/compiler/parser.cpp
@@ -1,7 +1,7 @@
 /***************************************************************************
  * Language parser.
  *
- * Copyright (c) 2024, Randy Hollines
+ * Copyright (c) 2025, Randy Hollines
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
--- core/repl/editor.h
+++ core/repl/editor.h
@@ -1,7 +1,7 @@
 /***************************************************************************
  * REPL editor
  *
- * Copyright (c) 2023, Randy Hollines
+ * Copyright (c) 2025, Randy Hollines
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
