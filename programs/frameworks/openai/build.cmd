e966b52ed0c6a62fc01be74b62b13b9aaeea2aaa
Randy Hollines <objeck@gmail.com>
2025-01-11 20:05:14 -0700

    working on adding openai image/vision support

--- programs/frameworks/openai/build.cmd
+++ programs/frameworks/openai/build.cmd
@@ -1,14 +1,15 @@
 @echo off
 
-set OBJECK_ROOT=C:\Users\objec\Documents\Code\objeck-lang
+SET TARGET=deploy-arm64
+SET OBJECK_ROOT=C:\Users\objec\Documents\Code\objeck-lang
 SET OBJECK_LIB_SRC=%OBJECK_ROOT%\core\compiler\lib_src
-SET OBJECK_LIB_DST=%OBJECK_ROOT%\core\release\deploy64\lib
+SET OBJECK_LIB_DST=%OBJECK_ROOT%\core\release\%TARGET%\lib
 
 REM obc -src %OBJECK_LIB_SRC%\json.obs -lib gen_collect -tar lib -opt s3 -dest %OBJECK_LIB_DST%\json.obl
 REM obc -src %OBJECK_LIB_SRC%\net_common.obs,%OBJECK_LIB_SRC%\net.obs,%OBJECK_LIB_SRC%\net_secure.obs -lib json -tar lib -dest %OBJECK_LIB_DST%\net.obl
 REM obc -src %OBJECK_LIB_SRC%\json.obs -tar lib -dest %OBJECK_LIB_DST%\json.obl
 REM obc -src %OBJECK_LIB_SRC%\ml.obs -lib json,csv -tar lib -dest %OBJECK_LIB_DST%\ml.obl
-obc -src %OBJECK_ROOT%\core\compiler\lib_src\openai.obs -lib json,net,misc -tar lib -dest %OBJECK_ROOT%\core\release\deploy64\lib\openai.obl
+obc -src %OBJECK_ROOT%\core\compiler\lib_src\openai.obs -lib json,net,misc -tar lib -dest %OBJECK_ROOT%\core\release\%TARGET%\lib\openai.obl
 
 if [%1] == [] goto end
 	obc -src %1 -lib net,json,misc,openai
