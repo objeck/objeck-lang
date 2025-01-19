faada70bbe355b30b3b50128dac8a8d296923240
Randy Hollines <objeck@gmail.com>
2025-01-19 03:36:24 -0700

    working on websocket support

--- programs/tests/build.sh
+++ programs/tests/build.sh
@@ -2,8 +2,9 @@
 
 rm *.obe
 
-export PATH=$PATH:/Users/randyhollines/Documents/Code/objeck-lang/core/release/deploy/bin
-export OBJECK_LIB_PATH=/Users/randyhollines/Documents/Code/objeck-lang/core/release/deploy/lib
+OBJK_BASE=/Users/randyhollines/Documents/Code/objeck-lang
+export PATH=$PATH:$OBJK_BASE/core/release/deploy/bin
+export OBJECK_LIB_PATH=$OBJK_BASE/core/release/deploy/lib
 
 obc -src ../../core/compiler/lib_src/net.obs,../../core/compiler/lib_src/net_common.obs,../../core/compiler/lib_src/net_secure.obs -tar lib -lib json,cipher -dest ../../core/release/deploy/lib/net.obl
 
