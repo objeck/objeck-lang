29e34e24d3eaffcfc15db848554bd38dd7c1c045
Randy Hollines <objeck@gmail.com>
2025-01-18 17:49:20 -0700

    working on websocket support

--- programs/tests/build.sh
+++ programs/tests/build.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+rm *.obe
+
 export PATH=$PATH:/Users/randyhollines/Documents/Code/objeck-lang/core/release/deploy/bin
 export OBJECK_LIB_PATH=/Users/randyhollines/Documents/Code/objeck-lang/core/release/deploy/lib
 
-obc -src ../../core/compiler/lib_src/net.obs,../../core/compiler/lib_src/net_common.obs,../../core/compiler/lib_src/net_secure.obs -tar lib -lib json -dest ../../core/lib/net.obl
+obc -src ../../core/compiler/lib_src/net.obs,../../core/compiler/lib_src/net_common.obs,../../core/compiler/lib_src/net_secure.obs -tar lib -lib json,cipher -dest ../../core/release/deploy/lib/net.obl
 
 if [ ! -z "$1" ]; then
-	obc -src $1 -lib net,json
-	obr $1 $2 $3 $4 $5
+	obc -src $1 -lib net,json,cipher
+	obr $1
 fi
