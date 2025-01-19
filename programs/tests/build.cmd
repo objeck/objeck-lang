365194c62024d388db9d2ad2e08bd4c86cc6d7a0
Randy Hollines <objeck@gmail.com>
2025-01-19 12:09:08 -0700

    adding websocket support

--- programs/tests/build.cmd
+++ programs/tests/build.cmd
@@ -1,4 +1,4 @@
-rem @echo off
+@echo off
 setlocal
 
 del /q *.obe
@@ -7,7 +7,7 @@ set OBJK_BASE=\Users\objec\Documents\Code\objeck-lang
 set PATH=%PATH%:%OBJK_BASE%\core\release\deploy-x64\bin
 set OBJECK_LIB_PATH=%OBJK_BASE%\core\release\deploy-x64\lib
 
-obc -src %OBJK_BASE%\core\compiler\lib_src\cipher.obs -tar lib -dest %OBJK_BASE%\core\release\deploy-x64\lib\cipher.obl
+rem obc -src %OBJK_BASE%\core\compiler\lib_src\cipher.obs -tar lib -dest %OBJK_BASE%\core\release\deploy-x64\lib\cipher.obl
 obc -src %OBJK_BASE%\core\compiler\lib_src\net.obs,%OBJK_BASE%\core\compiler\lib_src\net_common.obs,%OBJK_BASE%\core\compiler\lib_src\net_secure.obs -tar lib -lib json,cipher -dest %OBJK_BASE%\core\release\deploy-x64\lib\net.obl
 
 if [%1] == [] goto end
