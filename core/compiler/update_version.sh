b3beccab0a0ab9cc839a88cb1d8548e584346467
Randy Hollines <objeck@gmail.com>
2025-01-19 18:59:18 -0700

    updated libs

--- core/compiler/update_version.sh
+++ core/compiler/update_version.sh
@@ -18,7 +18,7 @@ make -f make/Makefile.amd64
 ./obc -src lib_src/net_common.obs,lib_src/net.obs,lib_src/net_secure.obs -tar lib -lib json,gen_collect,cipher -opt s3 -dest ../lib/net.obl
 ./obc -src lib_src/json_rpc.obs -tar lib -lib json,net -opt s3 -dest ../lib/json_rpc.obl
 ./obc -src lib_src/misc.obs -lib gen_collect,net,json -tar lib -opt s3 -dest ../lib/misc.obl
-./obc -src lib_src/rss.obs -tar lib -lib xml,gen_collect,net -opt s3 -dest ../lib/rss.obl
+./obc -src lib_src/rss.obs -tar lib -lib xml,gen_collect,net,cipher -opt s3 -dest ../lib/rss.obl
 ./obc -src lib_src/query.obs -tar lib -lib net,regex,csv,xml,json,misc -opt s3 -dest ../lib/query.obl
 ./obc -src lib_src/cipher.obs -tar lib -opt s3 -dest ../lib/cipher.obl
 ./obc -src lib_src/odbc.obs -lib gen_collect -tar lib -opt s3 -dest ../lib/odbc.obl
