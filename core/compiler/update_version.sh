76289649105b84a434cffa11df7333e88af9ee72
Randy Hollines <objeck@gmail.com>
2025-02-15 23:55:45 -0700

    bumping version

--- core/compiler/update_version.sh
+++ core/compiler/update_version.sh
@@ -9,6 +9,7 @@ make -f make/Makefile.amd64
 
 ./obc -src lib_src/gen_collect.obs -lib ../lib/lang -tar lib -opt s3 -dest ../lib/gen_collect.obl -strict
 ./obc -src lib_src/json_stream.obs -lib gen_collect -tar lib -opt s3 -dest ../lib/json_stream.obl
+./obc -src lib_src/cipher.obs -tar lib -opt s3 -dest ../lib/cipher.obl
 ./obc -src lib_src/diags.obs -lib gen_collect -tar lib -opt s3 -dest ../lib/diags.obl
 ./obc -src lib_src/xml.obs -lib gen_collect -tar lib -opt s3 -dest ../lib/xml.obl
 ./obc -src lib_src/json.obs -lib gen_collect -tar lib -opt s3 -dest ../lib/json.obl
@@ -20,7 +21,6 @@ make -f make/Makefile.amd64
 ./obc -src lib_src/misc.obs -lib gen_collect,net,json -tar lib -opt s3 -dest ../lib/misc.obl
 ./obc -src lib_src/rss.obs -tar lib -lib xml,gen_collect,net,cipher -opt s3 -dest ../lib/rss.obl
 ./obc -src lib_src/query.obs -tar lib -lib net,regex,csv,xml,json,misc -opt s3 -dest ../lib/query.obl
-./obc -src lib_src/cipher.obs -tar lib -opt s3 -dest ../lib/cipher.obl
 ./obc -src lib_src/odbc.obs -lib gen_collect -tar lib -opt s3 -dest ../lib/odbc.obl
 ./obc -src lib_src/openai.obs -lib json,net,cipher,misc -tar lib -opt s3 -dest ../lib/openai.obl
 ./obc -src lib_src/gemini.obs -lib misc,json,net,cipher -tar lib -opt s3 -dest ../lib/gemini.obl
