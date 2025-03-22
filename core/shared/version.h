3963a03a03dbd1248f9af7ac2fab40b16addd7fd
Randy Hollines <objeck@gmail.com>
2025-03-22 17:07:05 -0600

    bumping version

--- core/shared/version.h
+++ core/shared/version.h
@@ -37,5 +37,5 @@
 #define MAGIC_NUM_EXE 0xffbe // bitmask 'e'
 #define MAGIC_NUM_LIB 0xffb6 // bitmask 'k'
 
-#define VER_NUM 202530
-#define VERSION_STRING L"2025.3.0"
+#define VER_NUM 202531
+#define VERSION_STRING L"2025.3.1"
