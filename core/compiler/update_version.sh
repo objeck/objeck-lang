0cd706cefcb4837c327b6bb1787bbaae4e4efdb9
Randy Hollines <objeck@gmail.com>
2025-04-23 07:44:44 -0600

    updated libs

--- core/compiler/update_version.sh
+++ core/compiler/update_version.sh
@@ -23,8 +23,8 @@ make -f make/Makefile.amd64
 ./obc -src lib_src/rss.obs -tar lib -lib xml,gen_collect,net,cipher -opt s3 -dest ../lib/rss.obl
 ./obc -src lib_src/query.obs -tar lib -lib net,regex,csv,xml,json,misc -opt s3 -dest ../lib/query.obl
 ./obc -src lib_src/odbc.obs -lib gen_collect -tar lib -opt s3 -dest ../lib/odbc.obl
-./obc -src lib_src/openai.obs -lib json,net,net_server,,cipher,misc -tar lib -opt s3 -dest ../lib/openai.obl
-./obc -src lib_src/gemini.obs -lib misc,json,net,net_server,,cipher -tar lib -opt s3 -dest ../lib/gemini.obl
+./obc -src lib_src/openai.obs -lib json,net,net_server,cipher,misc -tar lib -opt s3 -dest ../lib/openai.obl
+./obc -src lib_src/gemini.obs -lib misc,json,net,net_server,cipher -tar lib -opt s3 -dest ../lib/gemini.obl
 ./obc -src lib_src/ollama.obs -lib net,json,cipher,misc -tar lib -opt s3 -dest ../lib/ollama.obl
 ./obc -src lib_src/sdl2.obs -tar lib -dest ../lib/sdl2.obl
 ./obc -src lib_src/sdl_game.obs -lib gen_collect,json,sdl2 -tar lib -dest ../lib/sdl_game.obl
