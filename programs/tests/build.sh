0274a18763b7f6729fc3bd6b81b906adcf8c16db
Randy Hollines <objeck@gmail.com>
2025-04-23 13:51:16 -0600

    updates to the url parser

--- programs/tests/build.sh
+++ programs/tests/build.sh
@@ -2,13 +2,13 @@
 
 rm *.obe
 
-OBJK_BASE=/home/randy/Documents/Code/objeck-lang
+OBJK_BASE=../..
 export PATH=$PATH:$OBJK_BASE/core/release/deploy/bin
 export OBJECK_LIB_PATH=$OBJK_BASE/core/release/deploy/lib
 
-obc -src ../../core/compiler/lib_src/gen_collect.obs -tar lib -lib lang.obl -dest ../../core/release/deploy/lib/gen_collect.obl -strict
+obc -src ../../core/compiler/lib_src/net.obs,../../core/compiler/lib_src/net_common.obs,../../core/compiler/lib_src/net_secure.obs -tar lib -lib json,cipher -dest ../../core/release/deploy/lib/net.obl
 
-# obc -src ../../core/compiler/lib_src/net.obs,../../core/compiler/lib_src/net_common.obs,../../core/compiler/lib_src/net_secure.obs -tar lib -lib json,cipher -dest ../../core/release/deploy/lib/net.obl
+obc -src ../../core/compiler/lib_src/net_server.obs -tar lib -lib net,json,cipher -dest ../../core/release/deploy/lib/net_server.obl
 
 if [ ! -z "$1" ]; then
 	obc -src $1 -lib net,json,cipher
