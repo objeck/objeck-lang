74f0568271f15b62cc0a4f22cdfdc7916c2b9d16
Randy Hollines <objeck@gmail.com>
2025-04-30 07:02:59 -0600

    code clean up

--- core/shared/version.h
+++ core/shared/version.h
@@ -37,5 +37,5 @@
 #define MAGIC_NUM_EXE 0xffbe // bitmask 'e'
 #define MAGIC_NUM_LIB 0xffb6 // bitmask 'k'
 
-#define VER_NUM 202540
-#define VERSION_STRING L"2025.4.0"
+#define VER_NUM 202550
+#define VERSION_STRING L"2025.5.0"
