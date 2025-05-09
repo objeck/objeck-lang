080e1d5872f2fa785438b5e61c3db400342b1d7d
Randy Hollines <objeck@gmail.com>
2025-05-09 07:22:22 -0600

    openai example workflow

---
+++ programs/examples/job-search/build.cmd
@@ -0,0 +1,37 @@
+@echo off
+setlocal
+
+if not [%1]==[x64] if not [%1]==[arm64] (
+	echo Windows targets are: 'x64' and 'arm64'
+	goto end
+)
+
+if [%1] == [x64] (
+	set TARGET=deploy-x64
+)
+
+set ZIP_BIN="\Program Files\7-Zip"
+
+if [%1] == [arm64] (
+	set TARGET=deploy-arm64
+)
+
+del /q *.obe
+
+set OBJK_BASE=\Users\objec\Documents\Code\objeck-lang
+set PATH=%PATH%;%OBJK_BASE%\core\release\%TARGET%\bin
+set OBJECK_LIB_PATH=%OBJK_BASE%\core\release\%TARGET%\lib
+
+rem obc -src %OBJK_BASE%\core\compiler\lib_src\cipher.obs -tar lib -dest %OBJK_BASE%\core\release\%TARGET%\lib\cipher.obl
+
+
+obc -src %OBJK_BASE%\core\compiler\lib_src\net_server.obs -tar lib -lib net,json,gen_collect,cipher -opt s3 -dest %OBJK_BASE%\core\release\%TARGET%\lib\net_server.obl
+REM obc -src %OBJK_BASE%\core\compiler\lib_src\net.obs,%OBJK_BASE%\core\compiler\lib_src\net_common.obs,%OBJK_BASE%\core\compiler\lib_src\net_secure.obs -tar lib -lib json,cipher -dest %OBJK_BASE%\core\release\%TARGET%\lib\net.obl
+obc -src %OBJK_BASE%\core\compiler\lib_src\net.obs,%OBJK_BASE%\core\compiler\lib_src\net_common.obs,%OBJK_BASE%\core\compiler\lib_src\net_secure.obs -tar lib -lib json,cipher -dest %OBJK_BASE%\core\release\%TARGET%\lib\net.obl
+rem obc -src %OBJK_BASE%\core\compiler\lib_src\json_stream.obs -tar lib -dest %OBJK_BASE%\core\release\%TARGET%\lib\json_stream.obl
+
+if [%2] == [] goto end
+	obc -src %2 -lib @std,net_server
+	obr %2 %3 %4
+:end
+
