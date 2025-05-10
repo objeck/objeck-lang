e2a9ffda3eb68de7f90fea084dca1c5b7df04351
Randy Hollines <objeck@gmail.com>
2025-05-10 05:04:41 -0600

    adding openai responses support

--- programs/frameworks/openai/build.cmd
+++ programs/frameworks/openai/build.cmd
@@ -16,18 +16,21 @@ if [%1] == [arm64] (
 	set TARGET=deploy-arm64
 )
 
+del *.obe
+
 set OBJECK_ROOT=C:\Users\objec\Documents\Code\objeck-lang
 set OBJECK_LIB_SRC=%OBJECK_ROOT%\core\compiler\lib_src
 set OBJECK_LIB_DST=%OBJECK_ROOT%\core\release\%TARGET%\lib
 
-rem obc -src %OBJECK_LIB_SRC%\json.obs -lib gen_collect -tar lib -opt s3 -dest %OBJECK_LIB_DST%\json.obl
+obc -src %OBJECK_LIB_SRC%\json.obs -lib gen_collect -tar lib -opt s3 -dest %OBJECK_LIB_DST%\json.obl
 rem obc -src %OBJECK_LIB_SRC%\json.obs -tar lib -dest %OBJECK_LIB_DST%\json.obl
 rem obc -src %OBJECK_LIB_SRC%\ml.obs -lib json,csv -tar lib -dest %OBJECK_LIB_DST%\ml.obl
-obc -src %OBJECK_LIB_SRC%\net_common.obs,%OBJECK_LIB_SRC%\net.obs,%OBJECK_LIB_SRC%\net_secure.obs -lib json,cipher -tar lib -dest %OBJECK_LIB_DST%\net.obl
-obc -src %OBJECK_LIB_SRC%\net_server.obs -lib net,json,cipher -tar lib -dest %OBJECK_LIB_DST%\net_server.obl
+rem obc -src %OBJECK_LIB_SRC%\net_common.obs,%OBJECK_LIB_SRC%\net.obs,%OBJECK_LIB_SRC%\net_secure.obs -lib json,cipher -tar lib -dest %OBJECK_LIB_DST%\net.obl
+rem obc -src %OBJECK_LIB_SRC%\net_server.obs -lib net,json,cipher -tar lib -dest %OBJECK_LIB_DST%\net_server.obl
+
 obc -src %OBJECK_ROOT%\core\compiler\lib_src\openai.obs -lib json,cipher,net,net_server,misc -tar lib -dest %OBJECK_ROOT%\core\release\%TARGET%\lib\openai.obl
 
 if [%2] == [] goto end
-	obc -src %2 -lib net,json,cipher,misc,openai
+	obc -src %2 -lib @std,@ml
 	obr %2 %3 %4 %5 %6
 :end
