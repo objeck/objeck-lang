8b58b1b96c89d792956a0c335e0fb3b3b5a34c57
Randy Hollines <objeck@gmail.com>
2025-05-24 06:23:12 -0600

    wip

--- core/release/code_doc64_debug.cmd
+++ core/release/code_doc64_debug.cmd
@@ -19,7 +19,8 @@ mkdir ..\html
 obc -src ..\..\..\lib\code_doc\doc_html.obs,..\..\..\lib\code_doc\doc_parser.obs -lib xml -dest ..\..\code_doc.obe
 
 if [%2] NEQ [deploy] goto end
-	obr ..\..\code_doc.obe ..\..\..\lib\code_doc\templates 2025.5.0 ..\..\..\compiler\lib_src\lang.obs ..\..\..\compiler\lib_src\regex.obs ..\..\..\compiler\lib_src\gen_collect.obs
+	obr ..\..\code_doc.obe ..\..\..\lib\code_doc\templates 2025.5.0 ..\..\..\compiler\lib_src\lang.obs ..\..\..\compiler\lib_src\sdl2.obs ..\..\..\compiler\lib_src\gen_collect.obs
+REM obr ..\..\code_doc.obe ..\..\..\lib\code_doc\templates 2025.5.0 ..\..\..\compiler\lib_src\gen_collect.obs
 	rmdir /s /q ..\doc\api
 	mkdir ..\doc\api
 	copy .\..\..\..\lib\code_doc\mockups\style.css ..\doc\api
