744e1d039a6c568ef3cdefd5f74efdd30dd6272e
Randy Hollines <objeck@gmail.com>
2025-05-26 11:54:07 -0600

    adding response schema formatting support for gemini

--- programs/frameworks/gemini/build.cmd
+++ programs/frameworks/gemini/build.cmd
@@ -1,18 +1,36 @@
 @echo off
+setlocal
+
+if not [%1]==[x64] if not [%1]==[arm64] (
+	echo Windows targets are: 'x64' and 'arm64'
+	goto end
+)
+
+if [%1] == [x64] (
+	set TARGET=deploy-x64
+)
+
+set ZIP_BIN="\Program Files\7-Zip"
+
+if [%1] == [arm64] (
+	set TARGET=deploy-arm64
+)
+
+del *.obe
 
 set OBJECK_ROOT=C:\Users\objec\Documents\Code\objeck-lang
-SET OBJECK_LIB_SRC=%OBJECK_ROOT%\core\compiler\lib_src
-SET OBJECK_LIB_DST=%OBJECK_ROOT%\core\release\deploy-arm64\lib
+set OBJECK_LIB_SRC=%OBJECK_ROOT%\core\compiler\lib_src
+set OBJECK_LIB_DST=%OBJECK_ROOT%\core\release\%TARGET%\lib
 
-REM obc -src %OBJECK_LIB_SRC%\json.obs -lib gen_collect -tar lib -opt s3 -dest %OBJECK_LIB_DST%\json.obl
-REM obc -src %OBJECK_LIB_SRC%\net_common.obs,%OBJECK_LIB_SRC%\net.obs,%OBJECK_LIB_SRC%\net_secure.obs -lib json -tar lib -dest %OBJECK_LIB_DST%\net.obl
-REM obc -src %OBJECK_LIB_SRC%\json.obs -tar lib -dest %OBJECK_LIB_DST%\json.obl
-REM obc -src %OBJECK_LIB_SRC%\ml.obs -lib json,csv -tar lib -dest %OBJECK_LIB_DST%\ml.obl
-obc -src %OBJECK_LIB_SRC%\gemini.obs -lib misc,json,net,cipher -tar lib -dest %OBJECK_LIB_DST%\gemini.obl
+obc -src %OBJECK_LIB_SRC%\json.obs -lib gen_collect -tar lib -opt s3 -dest %OBJECK_LIB_DST%\json.obl
+rem obc -src %OBJECK_LIB_SRC%\json.obs -tar lib -dest %OBJECK_LIB_DST%\json.obl
+rem obc -src %OBJECK_LIB_SRC%\ml.obs -lib json,csv -tar lib -dest %OBJECK_LIB_DST%\ml.obl
+rem obc -src %OBJECK_LIB_SRC%\net_common.obs,%OBJECK_LIB_SRC%\net.obs,%OBJECK_LIB_SRC%\net_secure.obs -lib json,cipher -tar lib -dest %OBJECK_LIB_DST%\net.obl
+rem obc -src %OBJECK_LIB_SRC%\net_server.obs -lib net,json,cipher -tar lib -dest %OBJECK_LIB_DST%\net_server.obl
 
-del /q *.obe
+obc -src %OBJECK_LIB_SRC%\gemini.obs -lib misc,json,net,net_server,cipher -tar lib -dest %OBJECK_LIB_DST%\gemini.obl
 
-if [%1] == [] goto end
-	obc -src %1 -lib csv,misc,ml,net,json,cipher,gemini -dest %1
-	obr %1 %2 %3 %4 %5
+if [%2] == [] goto end
+	obc -src %2 -lib @std,@ml
+	obr %2 %3 %4 %5 %6
 :end
