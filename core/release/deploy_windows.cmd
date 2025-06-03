14221cc6ecc05326f9fb30fc5a95394acc2aee69
Randy Hollines <objeck@gmail.com>
2025-06-03 06:56:51 -0600

    v2025.6.1

--- core/release/deploy_windows.cmd
+++ core/release/deploy_windows.cmd
@@ -196,6 +196,9 @@ mkdir %TARGET%\doc
 mkdir %TARGET%\doc\syntax
 xcopy /e ..\..\docs\syntax\* %TARGET%\doc\syntax
 
+mkdir %TARGET%\style 
+xcopy /e ..\lib\code_doc\templates\resources\* %TARGET%\style 
+
 REM update and process readme
 pushd ..\..\programs\deploy\util\readme
 REM call build.cmd readme_builder readme.json
