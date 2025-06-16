ef5aebfdc993a1fe74290e4aeacb6243bbbb09d1
Randy Hollines <objeck@gmail.com>
2025-06-16 09:30:56 -0600

    tweaking to code doc

--- core/release/code_doc64_debug.cmd
+++ core/release/code_doc64_debug.cmd
@@ -19,13 +19,12 @@ mkdir ..\html
 obc -src ..\..\..\lib\code_doc\doc_html.obs,..\..\..\lib\code_doc\doc_parser.obs -lib cipher,json,xml,misc,net -dest ..\..\code_doc.obe
 
 if [%2] NEQ [deploy] goto end
-	obr ..\..\code_doc.obe ..\..\..\lib\code_doc\templates 2025.5.0 ..\..\..\compiler\lib_src\lang.obs ..\..\..\compiler\lib_src\sdl2.obs ..\..\..\compiler\lib_src\gen_collect.obs ..\..\..\compiler\lib_src\json.obs
+obr ..\..\code_doc.obe ..\..\..\lib\code_doc\templates 2025.6.3 ..\..\..\compiler\lib_src\openai.obs
 	rmdir /s /q ..\doc\api
 	mkdir ..\doc\api
-	copy .\..\..\..\lib\code_doc\templates\style.css ..\doc\api
 	xcopy /e ..\html\* ..\doc\api
-	mkdir ..\doc\api\resources
-	copy ..\..\..\lib\code_doc\templates\resources\* ..\doc\api\resources
+	mkdir ..\doc\api\style
+	copy ..\..\..\lib\code_doc\templates\resources\* ..\doc\api\style
 	rmdir /s /q ..\html
 	cd ..\doc
 	%ZIP_BIN%\7z.exe a -r -tzip api.zip api\*
