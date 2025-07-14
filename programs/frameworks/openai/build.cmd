a5d8c89e57ffb80d2ddc4aee524bff8f51d01c1d
Randy Hollines <objeck@gmail.com>
2025-07-13 23:16:50 -0600

    working on openai realtime api support

--- programs/frameworks/openai/build.cmd
+++ programs/frameworks/openai/build.cmd
@@ -22,15 +22,15 @@ set OBJECK_ROOT=C:\Users\objec\Documents\Code\objeck-lang
 set OBJECK_LIB_SRC=%OBJECK_ROOT%\core\compiler\lib_src
 set OBJECK_LIB_DST=%OBJECK_ROOT%\core\release\%TARGET%\lib
 
-obc -src %OBJECK_LIB_SRC%\json.obs -lib gen_collect -tar lib -opt s3 -dest %OBJECK_LIB_DST%\json.obl
+rem obc -src %OBJECK_LIB_SRC%\json.obs -lib gen_collect -tar lib -opt s3 -dest %OBJECK_LIB_DST%\json.obl
 rem obc -src %OBJECK_LIB_SRC%\json.obs -tar lib -dest %OBJECK_LIB_DST%\json.obl
 rem obc -src %OBJECK_LIB_SRC%\ml.obs -lib json,csv -tar lib -dest %OBJECK_LIB_DST%\ml.obl
 rem obc -src %OBJECK_LIB_SRC%\net_common.obs,%OBJECK_LIB_SRC%\net.obs,%OBJECK_LIB_SRC%\net_secure.obs -lib json,cipher -tar lib -dest %OBJECK_LIB_DST%\net.obl
 rem obc -src %OBJECK_LIB_SRC%\net_server.obs -lib net,json,cipher -tar lib -dest %OBJECK_LIB_DST%\net_server.obl
-
+obc -src %OBJECK_LIB_SRC%\\sdl2.obs -lib collect.obl -tar lib -dest %OBJECK_LIB_DST%\sdl2.obl
 obc -src %OBJECK_ROOT%\core\compiler\lib_src\openai.obs -lib json,cipher,net,net_server,misc -tar lib -dest %OBJECK_ROOT%\core\release\%TARGET%\lib\openai.obl
 
 if [%2] == [] goto end
-	obc -src %2 -lib @std,@ml
+	obc -src %2 -lib json,cipher,net,net_server,misc,sdl2,sdl_game,openai
 	obr %2 %3 %4 %5 %6
 :end
