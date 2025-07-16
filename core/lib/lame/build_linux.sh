ebd9211149b001c7fe6f7da1a167ae5d71494125
Randy Hollines <objeck@gmail.com>
2025-07-16 09:35:53 -0700

    updating linux build to support lame

--- core/lib/lame/build_linux.sh
+++ core/lib/lame/build_linux.sh
@@ -4,4 +4,4 @@ rm -rf *.o
 rm -rf *.so
 
 g++ -O3 -std=c++11 -Wall -fPIC -c *$1.cpp -Wno-unused-function -Wno-deprecated-declarations
-g++ -O3 -shared -Wl,-soname,$1.so.1 -o $1.so *.o -lssl -lcrypto
+g++ -O3 -shared -Wl,-soname,$1.so.1 -o $1.so *.o -lssl -lmp3lame
