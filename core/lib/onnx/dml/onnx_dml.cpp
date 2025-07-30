b038751e8cd6418e5068a3c829261868bf13aea0
Randy Hollines <objeck@gmail.com>
2025-07-30 07:26:33 -0600

    refactoring for extensibility

--- core/lib/onnx/dml/onnx_dml.cpp
+++ core/lib/onnx/dml/onnx_dml.cpp
@@ -1,33 +1,8 @@
-#include <iostream>
-#include <fstream>
-#include <random>
-#include <filesystem>
-
-#include <opencv2/opencv.hpp>
-#include <onnxruntime_cxx_api.h>
-
-#include "../../../vm/lib_api.h"
+#include "../onnx_common.h"
 
 #ifdef _WIN32
 namespace fs = std::filesystem;
 #endif
-
-// Preprocess image for ResNet input
-cv::Mat preprocess(const cv::Mat& img, int resize_height, int resize_width) {
-   cv::Mat resized;
-   cv::resize(img, resized, cv::Size(resize_width, resize_height)); // ResNet input size
-   resized.convertTo(resized, CV_32F, 1.0 / 255);
-
-   // Normalize (mean/std from ImageNet)
-   cv::Mat channels[3];
-   cv::split(resized, channels);
-   for(int i = 0; i < 3; ++i) {
-      channels[i] = (channels[i] - 0.485f) / 0.229f;
-   }
-   cv::merge(channels, 3, resized);
-
-   return resized;
-}
 
 extern "C" {
    //
@@ -101,20 +76,9 @@ extern "C" {
          auto start = std::chrono::high_resolution_clock::now();
          */
 
-         // Preprocess image
-         cv::Mat input = preprocess(img, resize_height, resize_width);
-
-         // Convert HWC -> CHW
-         std::vector<float> input_tensor_values;
-         input_tensor_values.reserve(3 * resize_height * resize_width);
-         for(int c = 0; c < 3; ++c) {
-            for(int y = 0; y < resize_height; ++y) {
-               for(int x = 0; x < resize_width; ++x) {
-                  input_tensor_values.push_back(input.at<cv::Vec3f>(y, x)[c]);
-               }
-            }
-         }
-
+         // Preprocess image and convert HWC -> CHW
+         std::vector<float> input_tensor_values = preprocess(img, resize_height, resize_width);
+         
          // Create input tensor
          std::array<int64_t, 4> input_shape = { 1, 3, resize_height, resize_width };
          Ort::MemoryInfo mem_info = Ort::MemoryInfo::CreateCpu(OrtDeviceAllocator, OrtMemTypeCPU);
@@ -181,28 +145,12 @@ extern "C" {
 #ifdef _WIN32
    __declspec(dllexport)
 #endif
-  void onnx_get_provider_names(VMContext& context) {
+     void onnx_get_provider_names(VMContext& context) {
      // Get output parameter
      size_t* output_holder = APITools_GetArray(context, 0);
 
-     // Get execution provider names
-     std::vector<std::wstring> execution_provider_names;
-
-     auto execution_providers = Ort::GetAvailableProviders();
-     for(size_t i = 0; i < execution_providers.size(); ++i) {
-       auto execution_provider = execution_providers[i];
-       execution_provider_names.push_back(BytesToUnicode(execution_provider));
-     }
-
-     // Copy results
-     size_t* output_string_array = APITools_MakeIntArray(context, execution_provider_names.size());
-     size_t* output_string_array_buffer = output_string_array + 3;
-     for(size_t i = 0; i < execution_provider_names.size(); ++i) {
-       output_string_array_buffer[i] = (size_t)APITools_CreateStringObject(context, execution_provider_names[i]);
-     }
-
-     // Set output holder
-     output_holder[0] = (size_t)output_string_array;
+     // get provider names and set output holder
+     output_holder[0] = (size_t)get_provider_names(context);
    }
 }
 
