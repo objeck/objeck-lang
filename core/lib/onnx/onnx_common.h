6e54298d1844c46535e6aad121bd69b9b76bc243
Randy Hollines <objeck@gmail.com>
2025-07-30 07:26:33 -0600

    refactoring for extensibility

---
+++ core/lib/onnx/onnx_common.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <random>
+#include <filesystem>
+
+#include <opencv2/opencv.hpp>
+#include <onnxruntime_cxx_api.h>
+
+#include "../../vm/lib_api.h"
+
+// Preprocess image for ResNet input (normalize + HWC to CHW)
+std::vector<float> preprocess(const cv::Mat& img, int resize_height, int resize_width) {
+  cv::Mat resized;
+  cv::resize(img, resized, cv::Size(resize_width, resize_height));
+  resized.convertTo(resized, CV_32F, 1.0 / 255.0);
+
+  // Split channels
+  std::vector<cv::Mat> channels(3);
+  cv::split(resized, channels);
+
+  // Normalize using ImageNet mean/std for each channel
+  const float mean[3] = { 0.485f, 0.456f, 0.406f };
+  const float std[3] = { 0.229f, 0.224f, 0.225f };
+  for(int i = 0; i < 3; ++i) {
+    channels[i] = (channels[i] - mean[i]) / std[i];
+  }
+
+  // Convert to CHW format and flatten
+  std::vector<float> input_tensor_values;
+  for(const auto& channel : channels) {
+    input_tensor_values.insert(input_tensor_values.end(), (float*)channel.datastart, (float*)channel.dataend);
+  }
+
+  return input_tensor_values;
+}
+
+size_t* get_provider_names(VMContext& context) {
+  // Get execution provider names
+  std::vector<std::wstring> execution_provider_names;
+
+  auto execution_providers = Ort::GetAvailableProviders();
+  for(size_t i = 0; i < execution_providers.size(); ++i) {
+    auto execution_provider = execution_providers[i];
+    execution_provider_names.push_back(BytesToUnicode(execution_provider));
+  }
+
+  // Copy results
+  size_t* output_string_array = APITools_MakeIntArray(context, execution_provider_names.size());
+  size_t* output_string_array_buffer = output_string_array + 3;
+  for(size_t i = 0; i < execution_provider_names.size(); ++i) {
+    output_string_array_buffer[i] = (size_t)APITools_CreateStringObject(context, execution_provider_names[i]);
+  }
+
+  return output_string_array;
+}
