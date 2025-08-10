d423092568d06d8afe0c667f71ad5c625d9f414b
Randy Hollines <objeck@gmail.com>
2025-08-10 13:25:27 -0600

    adding linux build support for onnx

--- core/lib/onnx/eq/cuda/build_linux.sh
+++ core/lib/onnx/eq/cuda/build_linux.sh
@@ -3,5 +3,5 @@
 rm -rf *.o
 rm -rf *.so
 
-g++ -O3 -std=c++11 -Wall -fPIC -c *$1.cpp -Wno-unused-function -Wno-deprecated-declarations
+g++ -O3 -std=c++17 -Wall -fPIC -I./lib/include -L./lib/lib -c `pkg-config --cflags --libs opencv4` -Wl,-rpath=$(pwd)/onnxruntime-linux-x64-1.19.0/lib -c *$1.cpp -Wno-unused-function -Wno-deprecated-declarations
 g++ -O3 -shared -Wl,-soname,$1.so.1 -o $1.so *.o -lssl -lcrypto
