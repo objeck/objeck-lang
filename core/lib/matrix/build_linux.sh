e9646d50050d1c5fb29128f2120b97a4e96c7971
Randy Hollines <objeck@gmail.com>
2025-08-10 13:43:37 -0600

    updating libs

--- core/lib/matrix/build_linux.sh
+++ core/lib/matrix/build_linux.sh
@@ -3,5 +3,5 @@
 rm -rf *.o
 rm -rf *.so
 
-g++ $(pkg-config --cflags eigen3) -O3 -std=c++11 -Wall -fPIC -c *$1.cpp -Wno-unused-function -Wno-deprecated-declarations
+g++ $(pkg-config --cflags eigen3) -O3 -std=c++11 -Wall -fPIC -c *$1.cpp -Wno-unused-function -Wno-deprecated-declarations -Wno-maybe-uninitialized
 g++ -O3 -shared -Wl,-soname,$1.so.1 -o $1.so *.o -lssl -lcrypto
