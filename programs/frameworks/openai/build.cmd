30ca6352d3a3aafb14fa0faec5d2c5a646135de2
Randy Hollines <objeck@gmail.com>
2025-09-02 16:02:47 -0600

    working on mcp server lib

--- programs/frameworks/openai/build.cmd
+++ programs/frameworks/openai/build.cmd
@@ -28,8 +28,9 @@ rem obc -src %OBJECK_LIB_SRC%\json.obs -tar lib -dest %OBJECK_LIB_DST%\json.obl
 rem obc -src %OBJECK_LIB_SRC%\ml.obs -lib json,csv -tar lib -dest %OBJECK_LIB_DST%\ml.obl
 rem obc -src %OBJECK_LIB_SRC%\net_common.obs,%OBJECK_LIB_SRC%\net.obs,%OBJECK_LIB_SRC%\net_secure.obs -lib json,cipher -tar lib -dest %OBJECK_LIB_DST%\net.obl
 
+obc -src %OBJECK_LIB_SRC%\json.obs -lib gen_collect -tar lib -opt s3 -dest %OBJECK_LIB_DST%\json.obl
 obc -src %OBJECK_LIB_SRC%\net_server.obs -lib net,json,cipher -tar lib -dest %OBJECK_LIB_DST%\net_server.obl
-obc -src %OBJECK_ROOT%\core\compiler\lib_src\openai.obs -lib json,cipher,net,net_server,misc -tar lib -dest %OBJECK_ROOT%\core\release\%TARGET%\lib\openai.obl
+obc -src %OBJECK_LIB_SRC%\openai.obs -lib json,cipher,net,net_server,misc -tar lib -dest %OBJECK_LIB_DST%\\openai.obl
 
 rem obc -src %OBJECK_LIB_SRC%\\sdl2.obs -lib collect.obl -tar lib -dest %OBJECK_LIB_DST%\sdl2.obl
 rem copy /y %OBJECK_SDL2_DEBUG_ROOT%\libobjk_sdl.dll %OBJECK_LIB_DST%\native\libobjk_sdl.dll
