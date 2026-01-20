7b8c18476a93615a442fb2784e8e12e3c1776c42
Randy Hollines <objeck@gmail.com>
2026-01-19 23:27:29 -0700

    code refactor with ai tools

--- core/release/update_version.ps1
+++ core/release/update_version.ps1
@@ -1,6 +1,6 @@
 # strings
-$year_end = "2025"
-$month_end = "9"
+$year_end = "2026"
+$month_end = "2"
 $version = "0"
 
 $version = "$year_end.$month_end.$version"
--- core/shared/version.h
+++ core/shared/version.h
@@ -37,5 +37,5 @@
 #define MAGIC_NUM_EXE 0xffbe // bitmask 'e'
 #define MAGIC_NUM_LIB 0xffb6 // bitmask 'k'
 
-#define VER_NUM 202590
-#define VERSION_STRING L"2025.9.0"
+#define VER_NUM 202620
+#define VERSION_STRING L"2026.2.0"
