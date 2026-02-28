762a0da90528dd5a05f02813c3455f742633cbaa
objeck <objeck@users.noreply.github.com>
2026-02-27 17:33:16 -0700

    updating version

--- core/shared/version.h
+++ core/shared/version.h
@@ -37,5 +37,5 @@
 #define MAGIC_NUM_EXE 0xffbe // bitmask 'e'
 #define MAGIC_NUM_LIB 0xffb6 // bitmask 'k'
 
-#define VER_NUM 202621
-#define VERSION_STRING L"2026.2.1"
+#define VER_NUM 202630
+#define VERSION_STRING L"2026.3.0"
